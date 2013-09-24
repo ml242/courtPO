2d66b113f366a2cc17b3d72c7c753ea558f39d45
Morgan Neiman <quackhouse@me.com>
2013-09-24 18:14:07 -0400

    added calculator menu

--- w01/d02/morgan_neiman/calculator.rb
+++ w01/d02/morgan_neiman/calculator.rb
@@ -10,5 +10,9 @@ if firstIn != "n" && firstIn != "y"
   puts "I don't get it. Shutting down."
 end
 
-while firstIn == "y"
+while firstIn == "y" #if user enters yes, begin calc program loop
 
+puts "What operation would you like to perform?"
+puts "Your options are: Add, Subtract, Multiply, Divide"
+puts "Please enter your choice:"
+choice = gets.chomp!.downcase
