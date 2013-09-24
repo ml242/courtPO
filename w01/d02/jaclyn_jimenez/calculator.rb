887d9c3f9ece407e7129fab49c04c7c2cb44155b
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-09-24 18:26:24 -0400

    created basic menu for calculator

---
+++ w01/d02/jaclyn_jimenez/calculator.rb
@@ -0,0 +1,7 @@
+puts "Welcome to the Calculator!"
+
+puts "What would you like to do? \n \t * Add \n \t * Subtract \n \t * Multiply \n \t * Divide"
+
+input = gets.chomp.downcase
+
+puts "What number(s) would you like to perform this operation on?"
