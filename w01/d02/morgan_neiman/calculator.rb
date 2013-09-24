9ab92261873f0e1ea5159fab94f52012cd39e855
Morgan Neiman <quackhouse@me.com>
2013-09-24 19:50:19 -0400

    trying out not using a method

--- w01/d02/morgan_neiman/calculator.rb
+++ w01/d02/morgan_neiman/calculator.rb
@@ -1,10 +1,11 @@
-def getNumbers #method to make an array out of user's numbers
-  puts "Enter the numbers you'd like me to use, separated by spaces."
-  numIn = gets.chomp #gets numbers
-  numbers = numIn.split(" ") #separates numbers into individual strings
-  workingArray = [] #create new array
-  numbers.each { |num| workingArray << num.to_i } #adds each number to the end of workingArray
-end
+workingArray = [] #makes workingArray empty array
+
+  #def getNumbers #method to make an array out of user's numbers
+  #  puts "Enter the numbers you'd like me to use, separated by spaces."
+  #  numIn = gets.chomp #gets numbers
+  #  numbers = numIn.split(" ") #separates numbers into individual strings
+  #  numbers.each { |num| workingArray << num.to_i } #adds each number to the end of workingArray
+ # end
 
 puts "Welcome to Calculator."
 puts "Would you like to calculate something?"
@@ -20,26 +21,23 @@ end
 
 while firstIn == "y" #if user enters yes, begin calc program loop
 
-workingArray = [] #makes workingArray empty array
-
 puts "What operation would you like to perform?"
 puts "Your options are: Add, Subtract, Multiply, Divide"
 puts "Please enter your choice:"
 choice = gets.chomp!.downcase
+puts "Enter the numbers you'd like me to use, separated by spaces."
+numIn = gets.chomp #gets numbers
+numbers = numIn.split(" ") #separates numbers into individual strings
+numbers.each { |num| workingArray << num.to_i } #adds each number to the end of workingArray
 
 if choice == "add"
   getNumbers
+  puts workingArray
   sum = 0
-  while numInUse != workingArray.last
-    workingArray.each do |x|
-      sum = sum + x
-      numInUse = x
-    end
+  numInUse = nil
+  workingArray[0]...workingArray.last do |x|
+    sum = sum + x
   end
-
-  #workingArray[0]...workingArray.last do |x|
-   # sum = sum + x
-  #end
   puts "Your sum is #{sum}"
 end
 
