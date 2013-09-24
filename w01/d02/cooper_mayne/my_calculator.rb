d58c35e9260dfd0f328bb25d32d9834e7488f06c
Cooper Mayne <coopermayne@gmail.com>
2013-09-24 19:43:36 -0400

    finished menu

---
+++ w01/d02/cooper_mayne/my_calculator.rb
@@ -0,0 +1,14 @@
+puts "welcome to CalcZor!\n"
+
+puts "THESE ARE THE COMMANDS CALCZOR UNDERSTANDS!"
+puts "  add"
+puts "  sub"
+puts "  div"
+puts "  mul"
+puts "  pow"
+puts "\n\n WARNING: CalcZor is not all powerful... it can only process two number and one operation at a time..."
+puts "\nYour input should look like... \"5 add 3\" or \"3 power 2\"\n"
+
+while true
+
+end
