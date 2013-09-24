f46c350a4b9cd6c36fe8a63abfc7067fe680ce70
Morgan Neiman <quackhouse@me.com>
2013-09-24 18:28:07 -0400

    defined getnumbers method

--- w01/d02/morgan_neiman/calculator.rb
+++ w01/d02/morgan_neiman/calculator.rb
@@ -1,3 +1,12 @@
+def getarray(numIn) #method to make an array out of user's numbers
+  puts "Enter the numbers you'd like me to use"
+  numIn = gets.chomp #gets numbers
+  numbers = numIn.split(" ") #separates numbers into individual strings
+  workingArray = [] #create new array
+  numbers.each { |num| workingArray << num.to_i } #adds each number to the end of workingArray
+end
+
+
 puts "Welcome to Calculator."
 puts "Would you like to calculate something?"
 puts "y/n"
@@ -11,6 +20,8 @@ if firstIn != "n" && firstIn != "y"
 end
 
 while firstIn == "y" #if user enters yes, begin calc program loop
+
+workingArray = [] #makes workingArray empty array
 
 puts "What operation would you like to perform?"
 puts "Your options are: Add, Subtract, Multiply, Divide"
