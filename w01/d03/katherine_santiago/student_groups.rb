4a51ef75354da20638e8f7e850b40cf33d9f9f9d
Katherine Santiago <santiago.kat@gmail.com>
2013-09-25 18:37:45 -0400

    added random group sample for one group

--- w01/d03/katherine_santiago/student_groups.rb
+++ w01/d03/katherine_santiago/student_groups.rb
@@ -30,12 +30,20 @@ puts "Please enter a number between 2 and 11."
 group_size_str = gets.chomp
 group_size = group_size_str.to_i
 
-num_of_groups = 22 / group_size
+num_of_groups = 23 / group_size
 puts "There will be #{num_of_groups} groups in all."
-remainder = 22 % group_size
+remainder = 23 % group_size
 puts "The #{remainder} remainder will be in the last group."
 
+student_groups = students.sample(group_size)
+puts student_groups
+
+
+
 # divide class into random groups of that size
+
+# {student}.sample.unique
+
     # divide class size by desired group size (user input)
         # - sample of a split?
     # divide class size by desired group size to
