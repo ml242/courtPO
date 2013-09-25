601f969bb078bfbc43e79a59a163cb5eb1a669cc
Cooper Mayne <coopermayne@gmail.com>
2013-09-25 17:35:22 -0400

    starting find_group assignment

---
+++ w01/d03/cooper_mayne/find_groups.rb
@@ -0,0 +1,34 @@
+CLASS_MEMBERS = {
+  { name: "Alex Hint" },
+  #{ name: "Alicia Lauerman" },
+  { name: "Amy Ruan" },
+  { name: "Ana Giraldo-Wingler" },
+  { name: "Cooper Mayne" },
+  { name: "Diego Palma" },
+  { name: "Edward Shin" },
+  { name: "Enoch Riese" },
+  { name: "Harrison Powers " },
+  { name: "Jaclyn Jimenez" },
+  { name: "James Rothpearl" },
+  { name: "Jimmy Davis" },
+  #{ name: "Jonathan Leung" },
+  { name: "Jorge Colindres" },
+  { name: "Joshua Oynick" },
+  { name: "Katherine Santiago" },
+  { name: "Matt Lucas" },
+  { name: "Matthew Korporaal" },
+  { name: "Morgan Neiman" },
+  { name: "Natasha Green" },
+  #{ name: "Phillip Lamplugh" },
+  { name: "Quin Cogdell " },
+  { name: "Tom Metzger" },
+  { name: "Travis Vanderhoop" },
+  { name: "Will Smith" },
+  { name: "Yaritza Rodriguez"}
+}
+
+def make_groups( people_hash, group_size )
+
+end
+
+make_groups( CLASS_MEMBERS, 3)
