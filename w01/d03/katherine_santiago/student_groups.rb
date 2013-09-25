f485120ab36a6155a367e6b265dfde24cb60d3b4
Katherine Santiago <santiago.kat@gmail.com>
2013-09-25 18:02:19 -0400

    started student_groups app

---
+++ w01/d03/katherine_santiago/student_groups.rb
@@ -0,0 +1,42 @@
+students = [
+"Alex Hint",
+"Amy Ruan",
+"Ana Giraldo-Wingler",
+"Cooper Mayne",
+"Diego Palma",
+"Edward Shin",
+"Enoch Riese",
+"Harrison Powers ",
+"Jaclyn Jimenez",
+"James Rothpearl",
+"Jimmy Davis",
+"Jorge Colindres",
+"Joshua Oynick",
+"Katherine Santiago",
+"Matt Lucas",
+"Matthew Korporaal",
+"Morgan Neiman",
+"Natasha Green",
+"Quin Cogdell ",
+"Tom Metzger",
+"Travis Vanderhoop",
+"Will Smith",
+"Yaritza Rodriguez"
+]
+
+# ask for desired group size
+puts "How many people should be in each group?"
+puts "Please enter a number between 2 and 11."
+group_size = gets.chomp.to_i
+
+
+
+
+# divide class into random groups of that size
+    # divide class size by desired group size (user input)
+        # - sample of a split?
+    # divide class size by desired group size to
+        # find out the remainder using % (modulo)
+        # find number of groups
+    # create random samplings / arrays of that size
+    # add the remainder to the last group
