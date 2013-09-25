ba91557f69187af8bb0be10e259d673682a54cb5
Tom Metzger <metzgert@gmail.com>
2013-09-24 22:38:23 -0400

    outlined calculator requirements

---
+++ w01/d02/tom_metzger/calculator.rb
@@ -0,0 +1,50 @@
+=begin
+requirements of calculator:
+
+user choose desired operation
+user input values
+user views result
+
+user given menu of operations
+user able to choose operation from menu
+user able to enter numbers to be used by operation
+quit option should be one of the options in the menu
+
+PHASE 2
+add advanced arithmatic
+
+PHASE 3
+????
+
+=end
+
+#enter number
+puts "Welcome to your calculator!"
+
+#operation -- if arithmetic then prompt for another number and if exponentation or
+
+a for addition
+s for subtraction
+m for multiplication
+d for division
+#e for exponentation
+p to raise to the nth power
+r to take the nth root
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
