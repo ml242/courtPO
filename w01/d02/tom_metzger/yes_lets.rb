d25753d264f310260a17a8894e8e9752753bdb93
Tom Metzger <metzgert@gmail.com>
2013-09-24 21:43:22 -0400

    added _tm to my yes_lets file name and added homework version of yes_lets

--- w01/d02/tom_metzger/yes_lets.rb
+++ w01/d02/tom_metzger/yes_lets.rb
@@ -1,30 +1,44 @@
+# while something_is_true do
+
+# end
+
+# counter = 0
+# something = 10
+
+# while counter < something do
+#  puts "cool stuff"
+#  # counter += 1
+#  counter = counter + 1
+# end
+
+# Yes Let's!
 awesome_things = []
 
-counter = 0
+puts "Do you wanna play Yes Let's?"
+# this is displaying `y/n` to prompt the user to
+# enter a single character: y or n
+print "y/n "
 
-puts "Do you want to play Yes Let's?"
-print "y/n"
+input = gets.downcase.chomp!
 
-input = gets.chomp!
-
-while input == "y" && counter <= 5 do
+while input == 'y' do
   puts "ok we're playing..."
 
-  puts "What do you wnat to do?!?"
+  puts "What do you wanna do?!"
   awesome_thing = gets.chomp!
+
 
   awesome_things << awesome_thing
 
-  puts "Do you STILL want to play Yes Let's??, currently your story says" # add more here
+  puts "Do you STILL wanna play Yes Let's??"
   input = gets.chomp!
-  counter += 1
 end
 
 # this print a string with a carriage return of the array
-# puts awesome_things.inspect
+puts awesome_things.inspect
 # this print a string without a carriage return of the array
-#print awesome_things.inspect
+print awesome_things.inspect
 # print a string of the inspected string of the array
-#p awesome_things.inspect
+p awesome_things.inspect
 # printing and inspecting
-#p awesome_things
+p awesome_things
