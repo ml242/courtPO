0b532c456039290c59b52f3e8226502998e3cda0
JC <jorge.e.colindres@gmail.com>
2013-09-25 18:37:10 -0400

    created group_names.rb

---
+++ w01/d03/jorge_colindres/group_names.rb
@@ -0,0 +1,9 @@
+# Create a list of all the students
+
+# Ask user the desired group size
+
+# Select students randomly and put them into groups of the size set above
+
+# If there's a remainder student put them in the last group
+
+# Set a funny team name for each group
