7fc99fe06db846113135ecb02efe7bcad41e6be2
Ana <agiraldow@gmail.com>
2013-09-25 19:21:34 -0400

    added group shuffling and slicing

--- w01/d03/ana_giraldo-wingler/student_groups.rb
+++ w01/d03/ana_giraldo-wingler/student_groups.rb
@@ -31,26 +31,9 @@ group_size = group_size_str.to_i
 #shuffle 'students' array and slice into groups of group_size variable
 rand = students.shuffle.each_slice(group_size).to_a
 
-puts "#{rand}"
+puts rand
 
-#chop shuffled array into smaller arrays of group_size
-
-# puts poop
-
-# students.shuffle.slice(group_size)
-# puts rand
-
-# #divide class size by desired group size to find # of groups
-# group_num = 23 / group_size
-# puts "OK. That makes #{group_num} groups."
 
 # #find out the remainder using modulo (%)
 # remainder = 23 % group_size
-
-# #create random arrays using group size
-# student_groups = students.sample(group_size)
-# #split sample?
-# puts student_groups
-
-
 #add remainder to last group
