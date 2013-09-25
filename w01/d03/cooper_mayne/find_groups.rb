cd1790444cfd3a7fa7ec108b83dc567f9fb2f66c
Cooper Mayne <coopermayne@gmail.com>
2013-09-25 18:28:29 -0400

    got it working... needs refinement

--- w01/d03/cooper_mayne/find_groups.rb
+++ w01/d03/cooper_mayne/find_groups.rb
@@ -1,4 +1,4 @@
-CLASS_MEMBERS = {
+CLASS_MEMBERS = [
   { name: "Alex Hint" },
   #{ name: "Alicia Lauerman" },
   { name: "Amy Ruan" },
@@ -25,10 +25,29 @@ CLASS_MEMBERS = {
   { name: "Travis Vanderhoop" },
   { name: "Will Smith" },
   { name: "Yaritza Rodriguez"}
-}
+]
 
-def make_groups( people_hash, group_size )
+def random_name
+  random_names = %w(bunnyFaces mouseTails Funkypheasants PinkPanthers Jimmies Calsters Whipperbottoms TankTopsOnly)
+  random_names.sample
+end
+def make_groups( people_ungrouped, group_size )
 
+  groups = []
+
+  i=0
+  while people_ungrouped.size > group_size-1 do
+    group_members = people_ungrouped.shift( group_size )
+    groups.push( { name: random_name, members: group_members, id: i } )
+    i += 1
+  end
+
+  people_ungrouped.each_with_index do |person, index|
+    groups[index][:members].push( person )
+  end
+  return groups
 end
 
-make_groups( CLASS_MEMBERS, 3)
+groups = make_groups( CLASS_MEMBERS, 3)
+groups.each {|group| puts group; puts}
+
