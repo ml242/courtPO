5353cde66c8bd198421c1e30c07001fe57121ae4
Morgan Neiman <quackhouse@me.com>
2013-09-24 21:08:59 -0400

    commented out, added some checks for nonnumerical characters

--- w01/d02/morgan_neiman/calculator.rb
+++ w01/d02/morgan_neiman/calculator.rb
@@ -1,90 +1,113 @@
-
-
-  #def getNumbers #method to make an array out of user's numbers
-  #  puts "Enter the numbers you'd like me to use, separated by spaces."
-  #  numIn = gets.chomp #gets numbers
-  #  numbers = numIn.split(" ") #separates numbers into individual strings
-  #  numbers.each { |num| workingArray << num.to_i } #adds each number to the end of workingArray
- # end
-
 puts "Welcome to Calculator."
 puts "Would you like to calculate something?"
 puts "y/n"
 firstIn = gets.chomp!.downcase
 
 if firstIn == "n"
+# If user answers "n", end program here.
   puts "That's too bad. Bye!"
 end
 if firstIn != "n" && firstIn != "y"
+# If user types anything but "y" or "n", end program here.
   puts "I don't get it. Shutting down."
 end
 
-while firstIn == "y" #if user enters yes, begin calc program loop
+while firstIn == "y"
+#if user enters yes, begin calc program loop.
 
-workingArray = [] #makes workingArray empty array
+workingArray = [] # Makes workingArray into an empty array.
 
 puts "What operation would you like to perform?"
 puts "Your options are: Add, Subtract, Multiply, Divide, Exponent, Square Root"
 puts "Please enter your choice:"
 choice = gets.chomp!.downcase
+# User enters choice between calculator functions.
 
 if choice == "exponent"
+# If user chooses exponent
   puts "Please enter the initial value (the one we'll be raising to a power)."
   starting_num = gets.chomp!
-  puts "Please enter the power you'd like to raise it to."
-  power = gets.chomp!
-  exp = starting_num ** power
-  puts "Your exponent is #{exp}"
+  # Gets first value, to be raised to a power.
 
-elsif choice == "Square Root"
+  if starting_num != "0" && starting_num.to_i == 0
+  # Checks for non-numerical characters.
+    puts "Hey, that's not a number."
+  else
+    starting_num = starting_num.to_i
+    # Converts this value into an integer.
+    puts "Please enter the power you'd like to raise it to."
+    # Gets power to raise starting_num to.
+    power = gets.chomp!
+    if power != "0" && power.to_i == 0
+      puts "Hey, that's not a number."
+    else
+      power = power.to_i
+      exp = starting_num ** power
+      puts "Your exponent is #{exp}"
+    end
+  end
+
+elsif choice == "square root"
   puts "Please enter the number you'd like to find the square root of."
   to_square = gets.chomp!
-  sqr = to_square ** (1/2)
-  puts "Your square root is #{sqr}"
+  if to_square != "0" && to_square.to_i == 0
+      puts "Hey, that's not a number."
+  else
+    to_square = to_square.to_i
+    sqr = to_square ** (0.5)
+    puts "Your square root is #{sqr}"
+  end
 
 else
-puts "Enter the numbers you'd like me to use, separated by spaces."
-numIn = gets.chomp #gets numbers
-numbers = numIn.split(" ") #separates numbers into individual strings
-numbers.each { |num| workingArray << num.to_i } #adds each number to the end of workingArray
+puts "Enter the numbers you'd like me to use, separated by spaces. Put in as many as you'd like!"
+numIn = gets.chomp
+# Gets numbers.
+numbers = numIn.split(" ")
+# Separates numbers into individual strings.
+numbers.each { |num| workingArray << num.to_i }
+# Adds each number to the end of workingArray.
 
 if choice == "add"
- # getNumbers
- sum = 0
-  puts workingArray
+  sum = 0
+  # Creates variable sum.
   workingArray.each {|num| sum += num}
-  puts "Your sum is #{sum}"
+  # Sum increases by each item in array.
+    puts "Your sum is #{sum}"
 end
 
 if choice == "subtract"
-  dif = 0 + workingArray[0] + workingArray[0] #there are two of these to counteract the next step subtracting the first value
+  dif = 0 + workingArray[0] + workingArray[0]
+  # There are two of these to counteract the next step subtracting the first value.
   workingArray.each {|num| dif -= num}
+  # Dif decreases by each item in array.
   puts "Your difference is #{dif}"
 end
 
 if choice == "multiply"
-  prod = 1
-  workingArray.each {|num| prod *= num}
+  prod = 1 #begin prod at 1
+  workingArray.each {|num| prod *= num} #prod multiplies by each item in array
   puts "Your product is #{prod}"
 end
 
 if choice == "divide"
-  quot = 1 * workingArray[0] * workingArray[0] #two to counteract first division by first val in array
-  workingArray.each {|num| quot /= num }
+  quot = 1 * workingArray[0] * workingArray[0]
+  #multiplied two times by first item in array to counteract first division by first val in array
+  workingArray.each {|num| quot /= num } #quot divides by each item in array
   puts "Your quotient is #{quot}"
 end
 
-puts "Would you like to calculate something else?"
+puts "Would you like to calculate something else?" #option to break out of while loop
 puts "y/n"
-firstIn = gets.chomp.downcase
+firstIn = gets.chomp.downcase #unless firstIn == y, will break
 
-if firstIn == "n"
+if firstIn == "n" #ends program
   puts "k bye!"
 end
 
-if firstIn != "n" && firstIn != "y"
+if firstIn != "n" && firstIn != "y" #ends program
   puts "I don't get it. Shutting down."
 end
+
 end
 
 end
