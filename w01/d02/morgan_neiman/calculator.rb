008b88fe387e3090e354b2e03e830180d7f3d22a
Morgan Neiman <quackhouse@me.com>
2013-09-24 20:41:17 -0400

    added exponent and sqr root

--- w01/d02/morgan_neiman/calculator.rb
+++ w01/d02/morgan_neiman/calculator.rb
@@ -24,9 +24,25 @@ while firstIn == "y" #if user enters yes, begin calc program loop
 workingArray = [] #makes workingArray empty array
 
 puts "What operation would you like to perform?"
-puts "Your options are: Add, Subtract, Multiply, Divide"
+puts "Your options are: Add, Subtract, Multiply, Divide, Exponent, Square Root"
 puts "Please enter your choice:"
 choice = gets.chomp!.downcase
+
+if choice == "exponent"
+  puts "Please enter the initial value (the one we'll be raising to a power)."
+  starting_num = gets.chomp!
+  puts "Please enter the power you'd like to raise it to."
+  power = gets.chomp!
+  exp = starting_num ** power
+  puts "Your exponent is #{exp}"
+
+elsif choice == "Square Root"
+  puts "Please enter the number you'd like to find the square root of."
+  to_square = gets.chomp!
+  sqr = to_square ** (1/2)
+  puts "Your square root is #{sqr}"
+
+else
 puts "Enter the numbers you'd like me to use, separated by spaces."
 numIn = gets.chomp #gets numbers
 numbers = numIn.split(" ") #separates numbers into individual strings
@@ -36,25 +52,25 @@ if choice == "add"
  # getNumbers
  sum = 0
   puts workingArray
-  workingArray.each {|x| sum += x}
+  workingArray.each {|num| sum += num}
   puts "Your sum is #{sum}"
 end
 
 if choice == "subtract"
   dif = 0 + workingArray[0] + workingArray[0] #there are two of these to counteract the next step subtracting the first value
-  workingArray.each {|num| dif - num}
+  workingArray.each {|num| dif -= num}
   puts "Your difference is #{dif}"
 end
 
 if choice == "multiply"
   prod = 1
-  workingArray.each {|num| prod * num}
+  workingArray.each {|num| prod *= num}
   puts "Your product is #{prod}"
 end
 
 if choice == "divide"
   quot = 1 * workingArray[0] * workingArray[0] #two to counteract first division by first val in array
-  workingArray.each {|num| quot / num }
+  workingArray.each {|num| quot /= num }
   puts "Your quotient is #{quot}"
 end
 
@@ -68,6 +84,7 @@ end
 
 if firstIn != "n" && firstIn != "y"
   puts "I don't get it. Shutting down."
+end
 end
 
 end
