708aa4b6a9f907dd3a5a7098b27236492c7105ac
Harrison Powers <fijimunkii@gmail.com>
2013-09-25 17:39:55 -0400

    team names

---
+++ w01/d03/harrison_powers/grouper.rb
@@ -0,0 +1,42 @@
+STUDENTS = [
+	"Alex Hint",
+	"Amy Ruan",
+	"Ana Giraldo-Wingler",
+	"Cooper Mayne",
+	"Diego Palma",
+	"Edward Shin",
+	"Enoch Riese",
+	"Harrison Powers ",
+	"Jaclyn Jimenez",
+	"James Rothpearl",
+	"Jimmy Davis",
+	"Jorge Colindres",
+	"Joshua Oynick",
+	"Katherine Santiago",
+	"Matt Lucas",
+	"Matthew Korporaal",
+	"Morgan Neiman",
+	"Natasha Green",
+	"Quin Cogdell ",
+	"Tom Metzger",
+	"Travis Vanderhoop",
+	"Will Smith",
+	"Yaritza Rodriguez"
+]
+TEAM_NAMES = [
+	"The Inconceivables",
+	"The Hash Rockets",
+	"The Enumerati",
+	"The Super Sliders",
+	"The Master Shakes",
+	"The Crazy Eights",
+	"The Darth Vanderhoops",
+	"The Redundancy Refactorers",
+	"The Pedestals of Ridiculousness",
+	"The Buzzed Banshees",
+	"The Killer Klowns (from outer space)"
+]
+
+def grouper(size_of_group = 3)
+
+end
