53f44237df5c9cd4577aa4c06c2017ce228d6443
Tom Metzger <metzgert@gmail.com>
2013-09-25 00:53:00 -0400

    made lots of changes to get it to work and should have committed more

--- w01/d02/tom_metzger/calculator.rb
+++ w01/d02/tom_metzger/calculator.rb
@@ -43,18 +43,42 @@ action_descriptions = {
 #"c" => "Are you sure you want to clear the memory?"
 }
 
+
+=begin
+working on a way to simplify the code and properly handle nth roots
+
+def calc(num1, num2)
+  num1
+end
+
+
+def root(n)
+
+end
+=end
+
+
 operations = {
-  "a" => +,
-  "s" => -,
-  "m" => *,
-  "d" => //,
+  "a" => :+,
+  "s" => :-,
+  "m" => :*,
+  "d" => :/,
   #"e" => "exponentation",
-  "p" => **,
-  "r" => ** 1/,
+  "p" => :**,
+  #{}"r" => :** 1/,
   #"c" => operation = nil
 }
 
-
+operations_s = {
+  "a" => "+",
+  "s" => "-",
+  "m" => "*",
+  "d" => "/",
+  #"e" => "exponentation",
+  "p" => "**",
+  #{}"r" => :** 1/,
+  #"c" => operation = nil
+}
 
 #METHODS
 
@@ -67,7 +91,8 @@ puts "Welcome to your calculator!"
 
 #enter number
 puts "Please enter a number:"
-primary_num = gets.chomp!     #revisit this line to see if I need ! or should get rid of it
+first_num = gets.chomp!     #revisit this line to see if I need ! or should get rid of it
+primary_num = first_num.to_f
 
 #if this is run in another place besides the terminal a puts primary_num should be placed here
 
@@ -78,47 +103,56 @@ puts "\nFrom the preceeding menu, please select the operation you would like to 
 operation = gets.chomp!
 
 
+## Need to evaluate input value with statement like 'while x NOT IN array of accpetable inputs ask for proper input'
+
 #input the other value required for the calculation
 puts action_descriptions[operation]
 
-secondary_num = gets.chomp!
+second_num = gets.chomp!
+secondary_num = second_num.to_f
 
-#
-operations.puts {|math| puts primary_num math secondary_num}
-puts "Is this the expression you want evaluated?"
+#I was trying to get fancy
+puts ""
+puts "Is this the expression that you would like evaluated?"
+puts "#{primary_num} " + operations_s[operation] + " #{secondary_num} ="
+puts ""
 puts "Input y to evaluate and n to clear and retry."
 
 execute = gets.chomp!
 
-while execute != "y" || "n" do
+until execute == "y" || execute == "n"
   puts "Please input y or n."
   execute = gets.chomp!
 end
 
 if execute == "y"
-  result = primary_num operations[operation] secondary_num
-  puts result
+    if operation == "a"
+      puts primary_num + secondary_num
+    elsif operation == "s"
+      puts primary_num - secondary_num
+    elsif operation == "m"
+      puts primary_num * secondary_num
+    elsif operation == "d"
+      puts primary_num / secondary_num
+    elsif operation == "p"
+      puts primary_num ** secondary_num
+    else
+      puts primary_num ** (1/secondary_num)
+    end
 else
   puts "Your calculation has been cancelled."
 end
 
-
-
-# a for addition
-# s for subtraction
-# m for multiplication
-# d for division
-# #e for exponentation
-# p to raise to the nth power
-# r to take the nth root
-# c to clear the memory
+#I tried to avoid using case or if statements for this because it is long and ugly but its getting too late and I can't figure out how to get the operations hash to call the symbols/methods correctly
 
 
 
 
+#MY CONCLUSIONS AND SUMMARY NOTES
 
-
-
+#this behaves weirdly with imaginary numbers, etc and those answers would need to be properly coded
+#I also need to add the Math module to get all of the math functions but I would like to figure out how to put these into a hash and then get the correct syntax with this from a method that I create or in some other way but I'll work on this tomorrow.
+#I also want to add a clear memory function (e.g. reset) and I want to print the expression that will be evaluated and have the user confirm it
 
 
 
