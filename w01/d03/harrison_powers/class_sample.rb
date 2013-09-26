1a24c940b030c625e67222d5074d3b81253296e0
Harrison Powers <fijimunkii@gmail.com>
2013-09-26 11:45:50 -0400

    add funny name option

--- w01/d03/harrison_powers/class_sample.rb
+++ w01/d03/harrison_powers/class_sample.rb
@@ -21,7 +21,31 @@ end
 
 students = load_json_file( 'class_sample_db.json' )
 
-your_turn = ["your turn", "your next", "batters up", "do it", "good luck"]
+sayings = ["your turn", "your next", "batters up", "do it", "good luck"]
+
+nicknames = [
+  "Shoeshine",
+  "Wiggle-bum",
+  "Left Eye",
+  "Sirius",
+  "Letter-Writing",
+  "Squirrel Cheeks",
+  "Happy Days",
+  "Gangsta Walk",
+  "High Socks",
+  "House of La-Code-Sha",
+  "My Humps",
+  "Give It Up",
+  "Bumblefart",
+  "Slightly Askew",
+  "Numbnuts",
+  "Straight Pubes",
+  "Weird Nostrils",
+  "Shiny Teeth",
+  "Clacky Nails",
+  "Loony Bin",
+  "Funny Face"
+  ]
 
 puts "This is the class sampler"
 puts "press enter to get a random student"
@@ -34,7 +58,8 @@ while user_input != "q"
 	students = Hash[students.sort_by{|k,v| v}]
 	random_student = students.keys[0]
 	puts random_student
-	%x(say #{your_turn.sample}, #{random_student})
+	%x(say #{sayings.sample}, #{random_student}) if user_input != "f"
+	%x(say #{sayings.sample}, #{random_student.first} #{nicknames.sample} #{random_student.last}) if user_input = "f"
 	students[random_student] += 1
 	user_input = gets.chomp
 	pp students
