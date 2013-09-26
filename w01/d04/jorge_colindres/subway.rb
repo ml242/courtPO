d953fd03e7686575edc8ec5208b4b73c8052fe06
JC <jorge.e.colindres@gmail.com>
2013-09-26 18:18:23 -0400

    Added the user prompts

--- w01/d04/jorge_colindres/subway.rb
+++ w01/d04/jorge_colindres/subway.rb
@@ -1,3 +1,22 @@
+
+def ask(question)
+  puts question
+  gets.chomp
+end
+
+
+def riding_n_line
+
+end
+
+def riding_l_line
+
+end
+
+def riding_six_line
+
+end
+
 # Define the N Line with stops at Times SQ, 34, 28, 23, Union SQ, 8
 # Define the L Line with stops at 8, 6, Union SQ, 3, 1
 # Define the 6 line with stops at Grand Central, 33, 28, 23, Union SQ, Astor PL
@@ -17,7 +36,7 @@ subway_system = {
     "3",
     "1"
   ],
-  :six_line [
+  :six_line => [
     "Grand Central",
     "33",
     "28",
@@ -31,8 +50,35 @@ subway_system = {
 
 # Ask user what line they're taking
 
+CHOOSE_LINES_PROMPT = "
+  WELCOME TO THE SUBWAY SYSTEM!
+
+  What line do you want to take?
+    > Enter 1 for the N Line
+    > Enter 2 for the L Line
+    > Enter 3 for the 6 Line
+    > Enter Q to leave the Subway
+"
+
+selected_subway_line = ask("CHOOSE_LINES_PROMPT")
+
+if selected_subway_line == "1"
+ puts "You're riding the N Line"
+elsif selected_subway_line == "2"
+  puts "You're riding the L Line"
+elsif selected_subway_line == "3"
+  puts "You're riding the 6 Line"
+elsif selected_subway_line.upcase == "Q"
+  puts "Good Bye!"
+else
+  puts "error"
+end
+
+# if subway_system.has_key?
+
 # Ask user where they'r going
 
 # Ask user where they want to stop
 
 # Puts total number of stops
+
