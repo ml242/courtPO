0c888db743435903a8a92920166cf6057561c823
Tom Metzger <metzgert@gmail.com>
2013-09-25 22:01:00 -0400

    created outline of program and added hash of class

---
+++ w01/d03/tom_metzger/classmate_groups.rb
@@ -0,0 +1,81 @@
+#VARIABLES
+HASH_STUDENTS = {
+  :alex_hint => {:name => "Alex Hint", :email => "alex.hint.ah@gmail.com"},
+  :amy_Ruan => {:name => "Amy Ruan", :email => "nyc.amy@gmail.com"},
+  :ana_giraaldo_wingler => {:name => "Ana Giraldo-Wingler", :email => "agiraldow@gmail.com"},
+  :cooper_mayne => {:name => "Cooper Mayne", :email => "coopermayne@gmail.com"},
+  :diego_palma => {:name => "Diego Palma", :email => "dppalma@gmail.com"},
+  :edward_shin => {:name => "Edward Shin", :email => "eshin0910@gmail.com"},
+  :enoch_riese => {:name => "Enoch Riese", :email => "enoch.riese@gmail.com"},
+  :harrison_powers => {:name => "Harrison Powers ", :email => "harrisonpowers@gmail.com"},
+  :jaclyn_jimenez => {:name => "Jaclyn Jimenez", :email => "jaclynejimenez@gmail.com"},
+  :james_rothpearl => {:name => "James Rothpearl", :email => "jrothpearl@gmail.com"},
+  :jimmy_davis => {:name => "Jimmy Davis", :email => "jamesmdavis5@gmail.com"},
+  :jorge_colindres => {:name => "Jorge Colindres", :email => "jorge.e.colindres@gmail.com"},
+  :joshua_oynick => {:name => "Joshua Oynick", :email => "oynickj@gmail.com"},
+  :katherin_santiago => {:name => "Katherine Santiago", :email => "santiago.kat@gmail.com"},
+  :matt_lucas => {:name => "Matt Lucas", :email => "mattlucas@gmail.com"},
+  :matthew_korporaal => {:name => "Matthew Korporaal", :email => "matthew.korporaal@gmail.com"},
+  :morgan_neiman => {:name => "Morgan Neiman", :email => "quackhouse@me.com"},
+  :natasha_green => {:name => "Natasha Green", :email => "greennatasha@aol.com"},
+  :quin_cogdell => {:name => "Quin Cogdell ", :email => "quincogdell@gmail.com"},
+  :tom_metzger => {:name => "Tom Metzger", :email => "metzgert@gmail.com"},
+  :travis_vanderhoop => {:name => "Travis Vanderhoop", :email => "vanderhoop@me.com"},
+  :will_smith => {:name => "Will Smith", :email => "will.smith@preplaysports.com"},
+  :yaritza_rodriguez => {:name => "Yaritza Rodriguez", :email => "saturn540@gmail.com"}
+}
+
+
+#METHODS
+def ask(question)
+  puts(question)
+  answer = gets.chomp!
+end
+
+
+
+
+#CODE
+
+#user can select size of group
+    #TO DO---  or choose number of groups
+
+group_size = ask("How many people do you want per group?")
+
+    #TO DO---- is anyone missing today (y/n)
+        # ask for names one by one using a while loop
+        # remake list by deleting these students from the list
+
+
+
+
+#students should be randomly chosen from the list
+    #add without replacement, or without replacement for x cycles
+
+
+#if there is a remainder student put them in the last group
+    #if someone is not there then temporarily remove them from the groupings
+
+
+#add a funny team name for each team
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
