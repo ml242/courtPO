c595b20d21df662502834e07da70783ca24e4779
Edward Shin <Eshin0910@gmail.com>
2013-09-26 18:19:32 -0400

    testing part 1

--- w01/d04/edward_shin/mta.rb
+++ w01/d04/edward_shin/mta.rb
@@ -7,7 +7,11 @@ n_train = {
 	"Union Sqaure -N" => 5, 
 	"8th street -N" => 6
 }
+#questions
 
+train = "What train did you take today?"
+train_enter = "What stop did you get on?"
+train_exit = "Which stop did you get off?"
 
 #list of methods
 def ask(question)
@@ -16,7 +20,9 @@ def ask(question)
 end
 
 def n_method(n_train)
-	puts "I took the n train BITCH"
+	entrance = ask(train_enter)
+	train_ex = ask(train_exit)
+	puts "#{n_train[entrance]} was the entrance and #{n_train[train_ex] was the exit}"
 end
 
 
@@ -25,7 +31,7 @@ end
 
 
 #ask user which train stop they got on
-input = ask("What train did you take today?")
+input = ask(train)
 
 
 if input == "N train"
