d5ceb9341f9b524ff96ad8a8e7ca35c4ffe5fa41
JC <jorge.e.colindres@gmail.com>
2013-09-26 18:09:01 -0400

    created subway.rb and added subway_system hash

---
+++ w01/d04/jorge_colindres/subway.rb
@@ -0,0 +1,38 @@
+# Define the N Line with stops at Times SQ, 34, 28, 23, Union SQ, 8
+# Define the L Line with stops at 8, 6, Union SQ, 3, 1
+# Define the 6 line with stops at Grand Central, 33, 28, 23, Union SQ, Astor PL
+subway_system = {
+  :n_line => [
+    "Times Square",
+    "34",
+    "28",
+    "23",
+    "Union Square",
+    "8"
+  ],
+  :l_line => [
+    "8",
+    "6",
+    "Union Square",
+    "3",
+    "1"
+  ],
+  :six_line [
+    "Grand Central",
+    "33",
+    "28",
+    "23",
+    "Union Square",
+    "Astor Place"
+  ]
+}
+
+
+
+# Ask user what line they're taking
+
+# Ask user where they'r going
+
+# Ask user where they want to stop
+
+# Puts total number of stops
