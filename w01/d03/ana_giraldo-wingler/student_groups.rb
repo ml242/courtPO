b5c4df80da086eb9cecaa2257f455c881e0eeb1d
Ana <agiraldow@gmail.com>
2013-09-25 21:08:18 -0400

    added array of high-larious group names

--- w01/d03/ana_giraldo-wingler/student_groups.rb
+++ w01/d03/ana_giraldo-wingler/student_groups.rb
@@ -1,28 +1,36 @@
 students = [
-"Alex Hint",
-"Amy Ruan",
-"Ana Giraldo-Wingler",
-"Cooper Mayne",
-"Diego Palma",
-"Edward Shin",
-"Enoch Riese",
-"Harrison Powers ",
-"Jaclyn Jimenez",
-"James Rothpearl",
-"Jimmy Davis",
-"Jorge Colindres",
-"Joshua Oynick",
-"Katherine Santiago",
-"Matt Lucas",
-"Matthew Korporaal",
-"Morgan Neiman",
-"Natasha Green",
-"Quin Cogdell",
-"Tom Metzger",
-"Travis Vanderhoop",
-"Will Smith",
-"Yaritza Rodriguez"
-]
+    "Alex Hint",
+    "Amy Ruan",
+    "Ana Giraldo-Wingler",
+    "Cooper Mayne",
+    "Diego Palma",
+    "Edward Shin",
+    "Enoch Riese",
+    "Harrison Powers ",
+    "Jaclyn Jimenez",
+    "James Rothpearl",
+    "Jimmy Davis",
+    "Jorge Colindres",
+    "Joshua Oynick",
+    "Katherine Santiago",
+    "Matt Lucas",
+    "Matthew Korporaal",
+    "Morgan Neiman",
+    "Natasha Green",
+    "Quin Cogdell",
+    "Tom Metzger",
+    "Travis Vanderhoop",
+    "Will Smith",
+    "Yaritza Rodriguez"
+    ]
+
+group_name =  [
+    "The Cheese Smellies",
+    "The Twinkle Toes",
+    "The Weiner Blasters",
+    "The Fantastic Farts",
+    "The Downtrodden"
+    ]
 
 #ask user desired group size
 puts "How many people per group? Choose a number between 2 and 11."
