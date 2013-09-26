eb576b750f29648e108747ab21cd253c9dd4b39d
Ana <agiraldow@gmail.com>
2013-09-25 21:04:57 -0400

    got the program to put anyone in a lone group into the next last group

--- w01/d03/ana_giraldo-wingler/student_groups.rb
+++ w01/d03/ana_giraldo-wingler/student_groups.rb
@@ -17,23 +17,33 @@ students = [
 "Matthew Korporaal",
 "Morgan Neiman",
 "Natasha Green",
-"Quin Cogdell ",
+"Quin Cogdell",
 "Tom Metzger",
 "Travis Vanderhoop",
 "Will Smith",
 "Yaritza Rodriguez"
 ]
+
 #ask user desired group size
 puts "How many people per group? Choose a number between 2 and 11."
 group_size_str = gets.chomp
 group_size = group_size_str.to_i
 
 #shuffle 'students' array and slice into groups of group_size variable
-rand = students.shuffle.each_slice(group_size).to_a
+groups = students.shuffle.each_slice(group_size).to_a
 
-puts rand
+# check if last group only has one person in it
+if groups.last.length == 1
+  #add the last group to the second-to-last
+  remainder = groups[-2] + groups[-1]
+  #get rid of the last group
+  groups.pop
+  puts "#{groups} #{remainder}"
+  #pop them into last array
+else
+  puts "#{groups}"
+end
 
-
-# #find out the remainder using modulo (%)
+# find out the remainder using modulo (%)
 # remainder = 23 % group_size
 #add remainder to last group
