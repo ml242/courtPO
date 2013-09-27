8a721fb2564e3cf6626f0660a65b6c6782491586
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-09-27 00:05:40 -0400

    Trying to fix this bug

--- w01/d04/jaclyn_jimenez/mta.rb
+++ w01/d04/jaclyn_jimenez/mta.rb
@@ -1,23 +1,24 @@
+require 'pry'
+
 def ask(question)
   puts question
   gets.chomp.downcase
 end
 
-def number_of_stops(start, stop) #this method calculates number of stops for stops on the same line.
-  ans = start - stop
+def number_of_stops(start, final_stop)#this method calculates number of stops for stops on the same line.
+binding.pry
+  ans = start - final_stop
   ans.abs
 end
 
-def stops_before_transfer(line_on, line_off) #this method calculates number of stops with transfers.
-  common_stop = line_on & line_off
-  common_stop.join
-  stops_before_transfer_line_on = line_on.index("common_stop")  #number of stops from line_on
-  stops_before_transfer_line_off = line_off.index("common_stop") #number of stops from line_on
-  before_transfer = line_on - stops_before_transfer_line_on
-  after_transfer = line_off - stops_before_transfer_line_off
-  total_stops = before_transfer + after_transfer
-  total_stops
+def number_of_stops_multiple_transfers(line_name, other_line_name, stop_index) #this method calculates number of stops on diffent subway lines
+  common_stop_name = line_name & other_line_name
+  common_stop_name.join                                                #turns array of 'common stop' into a string
+  common_stop_index = line_name.index(common_stop_name)                     #index of common stop
+  transfer_stops_num = number_of_stops(common_stop_index, stop_index)  #number of transfer stops
+  transfer_stops_num
 end
+
 
 mta = {
   "n_line" => ["times square", "34th", "28th_n", "23rd_n", "union square", "8th_n"],
@@ -25,34 +26,24 @@ mta = {
   "six_line" => ["grand central", "33rd", "28th_6", "23rd_6", "union square", "astor place"]
 }
 
-#a.index("string") finds index ..
-
-# array = ['a', 'b', 'c']
-# hash = Hash[array.map.with_index.to_a]    # => {"a"=>0, "b"=>1, "c"=>2}
-# hash['b'] # => 1
-
 line_on = ask("What line are you taking to start?")
 stop_on = ask("What stop are you getting on at?")
 
 line_off = ask("What line are you getting off?")
 stop_off = ask("What stop are you getting off?")
 
-start = mta[line_on].index(stop_on) #this will be the index of the stop.
-stop = mta[line_off].index(stop_off) #this will be the index of a stop.
+start = mta[line_on].index(stop_on)           #this is the index of the first stop.
+final_stop = mta[line_off].index(stop_off)    #this is the index of the second stop.
+binding.pry
 
 if line_on == line_off
-stops_num = number_of_stops(start, stop)
-puts "#{stop_on} is #{stops_num} away from #{stop_off}"
+  binding.pry
+  stops_num = number_of_stops(start, final_stop)
+  puts "#{stop_on} is #{stops_num} away from #{stop_off}"
 else
-  stops_num = number_of_stops_with_transfers(start, stop)
+  binding.pry
+  first_ride = number_of_stops_multiple_transfers(mta[line_on], mta[line_off], start)
+  second_ride = number_of_stops_multiple_transfers(mta[line_off], mta[line_on], final_stop)
+  stops_num = first ride + second_ride
   puts "#{stop_on} is #{stops_num} away from #{stop_off}"
 end
-
-# different lines...
-#looking for it to count from stop 1 to union square, then from union square to second stop.
-#if line1 does not equal line2
-#do intersect method to see what they have in common
-# number of stops (line1 from common_line) and number of stops ()
-
-
-
