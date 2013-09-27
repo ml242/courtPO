13bdd2be5520ba7678ee64abb4cc56abafb0141a
Enoch Riese <enoch.riese@gmail.com>
2013-09-26 21:17:14 -0400

    stored subway lines

---
+++ w01/d04/enoch_riese/subways.rb
@@ -0,0 +1,5 @@
+subways = {
+  :n_train => [:eighth_n, :union_square, :twenty3_n, :twenty8_n, :thirty4_n :times_square_n]
+  :six_train => [:astor_6, :union_square, :twenty3_6, :twenty8_6, :thirty3_6, :grand_central_6]
+  :l_train => [:first_l, :third_l, :union_square, :sixth_l, :eighth_l]
+}
