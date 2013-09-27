01915c149e9b6e124961f3c13d289974abed730e
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-09-27 01:33:22 -0400

    Still can't fix this bug.

--- w01/d04/jaclyn_jimenez/mta.rb
+++ w01/d04/jaclyn_jimenez/mta.rb
@@ -1,17 +1,16 @@
-require 'pry'
+mta = {
+  "n_line" => ["times square", "34th", "28th_n", "23rd_n", "union square", "8th_n"],
+  "l_line" => ["8th_l", "6th", "union square", "3rd", "1st"],
+  "six_line" => ["grand central", "33rd", "28th_6", "23rd_6", "union square", "astor place"]
+}
 
-def ask(question)
-  puts question
-  gets.chomp.downcase
-end
 
-def number_of_stops(start, final_stop)#this method calculates number of stops for stops on the same line.
-binding.pry
-  ans = start - final_stop
+def number_of_stops(first_stop, final_stop)   #this method calculates number of stops for stops on the same line.
+  ans = first_stop - final_stop
   ans.abs
 end
 
-def number_of_stops_multiple_transfers(line_name, other_line_name, stop_index) #this method calculates number of stops on diffent subway lines
+def stops_for_multiple_transfers(line_name, other_line_name, stop_index) #this method calculates number of stops on diffent subway lines
   common_stop_name = line_name & other_line_name
   common_stop_name.join                                                #turns array of 'common stop' into a string
   common_stop_index = line_name.index(common_stop_name)                     #index of common stop
@@ -19,31 +18,26 @@ def number_of_stops_multiple_transfers(line_name, other_line_name, stop_index) #
   transfer_stops_num
 end
 
+def ask(question)
+  puts question
+  gets.chomp!
+end
 
-mta = {
-  "n_line" => ["times square", "34th", "28th_n", "23rd_n", "union square", "8th_n"],
-  "l_line" => ["8th_l", "6th", "union square", "3rd", "1st"],
-  "six_line" => ["grand central", "33rd", "28th_6", "23rd_6", "union square", "astor place"]
-}
+line_on = ask("What line are you starting from?")
+stop_on = ask("What stop are you starting from?")
 
-line_on = ask("What line are you taking to start?")
-stop_on = ask("What stop are you getting on at?")
+line_off = ask("What line are you getting off")
+stop_off = ask("What stop is your final destination?")
 
-line_off = ask("What line are you getting off?")
-stop_off = ask("What stop are you getting off?")
-
-start = mta[line_on].index(stop_on)           #this is the index of the first stop.
+first_stop = mta[line_on].index(stop_on)           #this is the index of the first stop.
 final_stop = mta[line_off].index(stop_off)    #this is the index of the second stop.
-binding.pry
 
-if line_on == line_off
-  binding.pry
-  stops_num = number_of_stops(start, final_stop)
-  puts "#{stop_on} is #{stops_num} away from #{stop_off}"
+if line_on == line_off          #if the station line names are the same
+  stops_num = number_of_stops(first_stop, final_stop)    # passes index of stops into method
+  puts "#{first_stop} is #{stops_num} away from #{final_stop}"
 else
-  binding.pry
-  first_ride = number_of_stops_multiple_transfers(mta[line_on], mta[line_off], start)
-  second_ride = number_of_stops_multiple_transfers(mta[line_off], mta[line_on], final_stop)
-  stops_num = first ride + second_ride
-  puts "#{stop_on} is #{stops_num} away from #{stop_off}"
+  first_ride = stops_for_multiple_transfers(mta[line_on], mta[line_off], first_stop)
+  second_ride = stops_for_multiple_transfers(mta[line_off], mta[line_on], final_stop)
+  stops_num2 = first ride + second_ride
+  puts "#{stop_on} is #{stops_num2} away from #{stop_off}"
 end
