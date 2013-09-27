b0d0fc76ede7ba5df05f13c06a08bace3501a068
Amy Ruan <nyc.amy@gmail.com>
2013-09-26 23:14:57 -0400

    update #2

--- w01/d04/amy_ruan/train.rb
+++ w01/d04/amy_ruan/train.rb
@@ -5,9 +5,9 @@ L Line: 1st Ave, 3rd Ave, Union Square, 6th Ave, 8th Ave
 6 Line: Astor Place, Union Square, 28th Street, 33rd Street, Grand Central
 "
 
-Line_N = ["8th street", "union square", "23rd street", "28th street", "34th street", "times square"]
-Line_L = ["1st ave", "3rd ave", "union square", "6th ave", "8th ave"]
-Line_6 =["astor place", "union square", "28th street", "33rd street","grand central"]
+Line_N = ["8th street_N", "union square", "23rd street_N", "28th street_N", "34th street_N", "times square_N"]
+Line_L = ["1st ave_L", "3rd ave_L", "union square", "6th ave_L", "8th ave_L"]
+Line_6 =["astor place_6", "union square", "28th street_6", "33rd street_6","grand central_6"]
 
 def ask(question)
   puts(question)
@@ -18,56 +18,62 @@ def line_locator(line)
   "Line" + "_" + line
 end
 
+def stop_locator(stop, line)
+  if stop != "union square"
+    stop = stop + "_" + line
+  else
+    stop
+  end
+end
+
 def line_hash(line)
   line.slice! "Line"
   line
   line.to_sym
 end
 
+def intersection_locator(first_array, second_array)
+  intersection = first_array & second_array
+  intersection.join
+end
 
 #First you need to know what line you are starting from
 first_line = ask("What is the name of the train line you starting from?").upcase
 first_line_id = line_locator(first_line)
-print first_line_id
 first_line_hash = line_hash(first_line_id)
 
 # Ask what stop you are starting from
 first_stop = ask("Please enter starting station name").downcase
-
+first_stop_id = stop_locator(first_stop, first_line)
 
 #Repeat for destination stop
 last_line = ask("What is the name of the train line for your destination?").upcase
 last_line_id = line_locator(last_line)
-print last_line_id
 last_line_hash = line_hash(last_line_id)
 
 last_stop = ask("Please enter destination station name").downcase
+last_stop_id = stop_locator(last_stop, last_line)
 
-# figure out how many stops between first_stop and last_stop
-# case first_line_id
-#   when "Line_N"
-#     print Line_N.find_index(last_stop) - Line_N.find_index(first_stop)
-#   when "Line_L"
-#     print Line_L.find_index(last_stop)-Line_L.find_index(first_stop)
-#   when "Line_6"
-#     print Line_6.find_index(last_stop)-Line_6.find_index(first_stop)
-# end
-
-
-#make hash of all the train lines
 #Make hash of the train lines
 map = Hash.new
 map = {:_N => Line_N, :_L => Line_L, :_6 => Line_6}
 # print map
 
 # figure out how many steps between first_stop and last stop
-if first_line_hash = last_line_hash
-  single_line = map[first_line_hash].find_index(last_stop) - map[first_line_hash].find_index(first_stop)
-  puts "#{single_line} more until your destination."
+if first_line_hash == last_line_hash
+  single_line = map[first_line_hash].find_index(last_stop_id) - map[first_line_hash].find_index(first_stop_id)
+#absolute value in case you go backwards
+  single_line = single_line.abs
+  puts "#{single_line} more stops until your destination."
 else
-
+  #find where the lines intersect
+  intersection = intersection_locator(map[first_line_hash],map[last_line_hash])
+  #find out how many stops from origin until intersection
+  first_multiple_line = map[first_line_hash].find_index(intersection) - map[first_line_hash].find_index(first_stop_id)
+  #find out how many stops from intersection until destination
+  second_multiple_line = map[last_line_hash].find_index(last_stop_id) - map[last_line_hash].find_index(intersection)
+  #absolute values until you go backwards sum up stops
+  total_line = first_multiple_line.abs + second_multiple_line.abs
+  puts "#{total_line} more stops until your destination"
 end
-
-
-#figure out how to locate the stops
 
