8aeaf201c174f67a97689725792955e2bde22a04
Quin Cogdell <quincogdell@gmail.com>
2013-09-29 17:25:32 -0400

    working count_apartments_available

---
+++ w01/d05/quin_cogdell/building.rb
@@ -0,0 +1,45 @@
+# num floors for building should be directly related to the keys in the apartments hash
+#currently these are not connected.
+#should floors just be the count of the hash keys?
+
+class Building
+  attr_accessor :address, :has_doorman, :is_walkup, :num_floors, :apartments
+
+  def initialize
+    @apartments = {}
+  end
+
+
+  def count_renters
+
+
+  end
+
+def count_apartments_available
+  available_apartments = 0
+  @apartments.each do |floor, apartments|
+    @apartments[floor].each do |apartment|
+      #binding.pry
+      if apartment.is_occupied? != false
+        available_apartments += 1
+      end
+    end
+  end
+  available_apartments
+end
+
+# added a method to count the total apartments in the building
+# was practice for looping through hashes
+  def count_total_apartments
+    total_apartments = 0
+    @apartments.each { |floor, apartments| total_apartments += @apartments[floor].count }
+    total_apartments #why do i have to include this to get it to return total apartments as a number?
+
+  end
+
+
+end
+
+
+#a = b2.apartments[:floor_two].each {|apartment| apartment.is_occupied? }
+
---
+++ w01/d05/quin_cogdell/person.rb
@@ -0,0 +1,6 @@
+# how do i connect the person to the apartment
+
+class Person
+  attr_accessor :name, :age, :gender, :apartment
+
+end
