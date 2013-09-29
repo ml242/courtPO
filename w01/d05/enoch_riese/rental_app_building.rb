a23418790b40fd52f71ed4af58dfe3b55a18feb1
Enoch Riese <enoch.riese@gmail.com>
2013-09-29 01:38:16 -0400

    building can count its apartments

--- w01/d05/enoch_riese/rental_app_building.rb
+++ w01/d05/enoch_riese/rental_app_building.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Building
 
   attr_reader :address
@@ -13,7 +15,12 @@ class Building
     @has_doorman = has_doorman
     @is_walkup = is_walkup
     @num_floors = num_floors
-    @apartments = count_apartments
+    @apartments = {}
+    x = 1
+    num_floors.times do
+      @apartments[x] = []
+      x+=1
+    end
     @count_people = count_people
     @count_appartments_available = count_appartments_available
   end
@@ -22,13 +29,9 @@ class Building
     []
   end
   def count_apartments
-    @apartments = {}
-    x = 1
-    num_floors.times do
-      @apartments[x] = ["apartments"]
-      x+=1
-    end
-    return @apartments
+    binding.pry
+    apartments_list = @apartments.values.flatten
+    apartments_list.compact.length
   end
 
   def count_appartments_available
@@ -44,13 +47,16 @@ class Apartment
   attr_accessor :is_occupied
   attr_accessor :price
 
-  def initialize(sqft, num_bedrooms, num_baths, price)
+  def initialize(sqft, num_bedrooms, num_baths, price, building, floor)
     @sqft = sqft
     @num_bedrooms = num_bedrooms
     @num_baths = num_baths
     @renters = []
     @is_occupied = is_occupied?
     @price = price
+    @building = building
+    @floor = floor
+    building.apartments[@floor] << self
   end
 
   def is_occupied?
@@ -63,17 +69,30 @@ class Person
   attr_reader :age
   attr_accessor :apartment
 
-  def initialize(name, age)
+  @database = {}
+  @id = 0
+
+  def self.store(person)
+    @id += 1
+    @database[@id] = person
+  end
+
+  def self.find(id)
+    return @database[id]
+  end
+
+  def initialize(name, age, apartment)
     @name = name
     @age = age
-    # @apartment = apartment
+    @apartment = apartment
+    @apartment.renters << self
+    # self.store(name)
   end
 end
 
 b1 = Building.new
 b1.intialize("140 Franklin St", true, false, 6)
-b1a1 = Apartment.new(3000, 3, 2, 5000)
-p1 = Person.new("James", 22)
-b1a1.renters << p1
-b1.apartments[1] = [b1a1]
-puts b1.apartments
+b1a1 = Apartment.new(3000, 3, 2, 5000, b1, 1)
+b1a2 = Apartment.new(2000, 2, 1, 3000, b1, 1)
+p1 = Person.new("James", 22, b1a1)
+puts b1.count_apartments
