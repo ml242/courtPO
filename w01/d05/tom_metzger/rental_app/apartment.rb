ec8b0816ec015d1d60b6c14cb3ebe5aa3f315fa6
Tom Metzger <metzgert@gmail.com>
2013-09-29 12:21:28 -0400

    created basic structure for apartment class

--- w01/d05/tom_metzger/rental_app/apartment.rb
+++ w01/d05/tom_metzger/rental_app/apartment.rb
@@ -3,6 +3,26 @@ require_relative "building"
 require_relative "person"
 
 
+class Apartment
+  attr_accessor :sqft
+  attr_accessor :num_bedrooms
+  attr_accessor :num_baths
+  attr_accessor :price
+  #apartments??????????????
+
+#only include the values to be given at initialize
+  def initialize(square_footage, bedrooms, bathrooms, price)
+    @sqft         = square_footage
+    @num_bedrooms = bedrooms
+    @num_baths    = bathrooms
+    @price        = price
+  end
+
+  def is_occupied?()
+    #allows user to query if apartment is occupied or not
+  end
+
+end
 
 
 
