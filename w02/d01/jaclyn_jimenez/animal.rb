59d722c5d7753a0159b376a0fbc1033502f44cb3
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-09-30 17:04:54 -0400

    added attritbutes to animal class

--- w02/d01/jaclyn_jimenez/animal.rb
+++ w02/d01/jaclyn_jimenez/animal.rb
@@ -1,3 +1,11 @@
+# Later: An animal can have multiple toys
+
 class Animal
+  attr_accessor :name :age :sex :species
+  def initialize(name, age, sex, :species)
+    @name = name
+    @age = age
+    @sex = sex
+  end
 
 end
