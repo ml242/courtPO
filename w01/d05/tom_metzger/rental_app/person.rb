ee2e63790955e0ce044baaba82c19901a46a370b
Tom Metzger <metzgert@gmail.com>
2013-09-29 23:40:00 -0400

    set up basic structure, created buildings (but getting error), created apartments, created people, need to create methods

--- w01/d05/tom_metzger/rental_app/person.rb
+++ w01/d05/tom_metzger/rental_app/person.rb
@@ -4,28 +4,25 @@ require_relative "building"
 
 
 class Person
-  attr_accessor :name
-  attr_accessor :age
-  attr_accessor :gender
-  #APARTMENT ??????????????
+  attr_accessor :name, :age, :gender, :apartment
 
 #only include the values to be given at initialize
   def initialize(name, age, gender, apartment)
     @name= name
     @age = age
     @gender= gender
-    #Apartment = apartment
-  end
-
-  def is_occupied?()
-    #allows user to query if apartment is occupied or not
+    @apartment = [apartment]
   end
 
 end
 
 
 
-
+person1 = Person.new("Bob",   40, "male",   ["666 Park Ave", :A1])
+person1 = Person.new("Sue",   38, "female", ["666 Park Ave", :A1])
+person1 = Person.new("Joe",   25, "male",   ["666 Park Ave", :B1])
+person1 = Person.new("Al",    45, "male",   ["666 Park Ave", :C1])
+person1 = Person.new("Jane",  42, "female", ["666 Park Ave", :C1])
 
 
 
