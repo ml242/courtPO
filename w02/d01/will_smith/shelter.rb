66339e37ad9050565c14e86b582e76bbcc74d9a5
Will Smith <will.smith@preplaysports.com>
2013-09-30 21:31:31 -0400

    Phase 2

--- w02/d01/will_smith/shelter.rb
+++ w02/d01/will_smith/shelter.rb
@@ -6,15 +6,31 @@ class Shelter
     @people = {}
     @animals = {}
   end
-
+# display the animals
   def display_animals
     @animals.each_value do |v|
       p v
     end
   end
+  #display the people
   def display_people
     @people.each_value do |v|
       p v
     end
   end
+  def add_person
+    add_person = Person.new(name, age)
+  end
+  def add_animal
+    add_animal = Animal.new(name, age, gender, species)
+  end
+
 end
+
+p1 = Person.new('Ace', 30)
+p2 = Person.new('Dan', 40)
+p3 = Person.new('Melissa', 32)
+
+a1 = Animal.new('Wolfie', 6, 'male', 'dog')
+a2 = Animal.new('Violet', 2, 'female', 'dog')
+a3 = Animal.new('Tula', 7, 'female', 'dog')
