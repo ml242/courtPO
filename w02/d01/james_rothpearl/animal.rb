51f0f66e19ac72a9c8b43c5614a40fb20c837608
James Rothpearl <jrothpearl@gmail.com>
2013-09-30 23:30:52 -0400

    added week02 d01 hw

---
+++ w02/d01/james_rothpearl/animal.rb
@@ -0,0 +1,10 @@
+# Animal:
+
+# An animal should have a name.
+# An animal should have an age.
+# An animal should have a gender.
+# An animal should have a species.
+# An animal can have multiple toys.
+
+class Animal
+  attr_accessor :name, :age, :gender, :species, :toys
