295a156f2809cdede119e6ed501ccb4534356e40
Tom Metzger <metzgert@gmail.com>
2013-10-01 01:39:45 -0400

    finished user input, program works but can't read names out of 'database', almost finished it. Trouble shoot tomorrow.

--- w02/d01/tom_metzger/animal_shelter.rb
+++ w02/d01/tom_metzger/animal_shelter.rb
@@ -12,72 +12,163 @@ class Shelter
     @animals      = []
     @people       = []
   end
-
-  def add_person(shelter_name, name, num_of_children, age) #, current_pets
-    #prompt user for inputs for Person.new
-     shelter_name.people << Person.new(name, num_of_children, age) #, current_pets
-    # call Person.new
-  end
-
-  def add_animal(name, age, gender, species)
-    #prompt user for inputs for Animal.new
-    # call Animal.new
-  end
-
-  def display_people(shelter_name)
-    puts shelter_name.people
-  end
-
-  def display_animals(shelter_name)
-    puts shelter_name.animals
-  end
-
-  def adopt(person, shelter)
-    person.pets << shelter.animals.pop
-  end
-
-  def surrender(person, shelter)
-    shelter.animals << person.pets.pop
-  end
-
 end
+
+def ask(question)
+  puts question
+  answer = gets.chomp!
+end
+
+def main_menu(initial_menu)
+  initial_menu.each {|item| puts item}
+end
+
+def display_animals(shelter_name)
+  #shelter_name = ask("For what shelter would you like to view the list of animals?")
+  # puts shelter_name.to_s.animals
+  puts shelter_name.animals
+end
+
+def display_people(shelter_name)
+  #shelter_name = ask("For what shelter would you like to view the list of people?")
+  puts shelter_name.people
+end
+
+def add_animal(shelter_name, name, age, gender, species)
+  #prompt user for inputs for Animal.new
+  shelter_name.animals << Animal.new(name, age, gender, species)
+  # call Animal.new
+end
+
+def new_animal_record(shelter_name)
+  #shelter_name  = ask("What shelter is this animal being surrendered to?")
+  name          = ask("What is the animal's name?")
+  age           = ask("What is the animal's age?")
+  gender        = ask("Is the animal male or female?")
+  species       = ask("What species is the animal")
+  add_animal(shelter_name, name, age, gender, species)
+  puts "#{name}, a #{age} year old #{gender} #{species} has been successfully added."
+end
+
+def add_person(shelter_name, name, num_of_children, age) #, current_pets
+  #prompt user for inputs for Person.new
+   shelter_name.people << Person.new(name, num_of_children, age) #, current_pets
+  # call Person.new
+end
+
+def new_person_record(shelter_name)
+  #shelter_name    = ask("What shelter is this person visiting?")
+  name            = ask("What is the person's name?")
+  num_of_children = ask("How many childen does the person have?")
+  age             = ask("What is the person's age?")
+  add_person(shelter_name, name, num_of_children, age)
+  puts "You have successfully added #{name.capitalize}, a #{age} year old with #{num_of_children} children."
+end
+
+def adopt(shelter_name)
+  input_name    = ask("What is your name?")
+  # shelter_name  = ask("What shelter are you looking to adopt from?")
+    if shelter_name.people.include?(input_name)
+      person.pets << shelter_name.animals.pop
+    else
+      puts "Please add this person to database before proceeding with the adoption process."
+      new_person_record(shelter_name)
+    end
+end
+
+def surrender(shelter_name)
+  input_name    = ask("What is your name")
+  # shelter_name  = ask("At what shelter will you surrender your animal?")
+    if shelter_name.people.include?(input_name)
+      shelter.animals << input_name.pets.pop
+    else
+      puts "Please add this person to database before proceeding with the surrender process."
+      new_person_record(shelter_name)
+    end
+end
+
+
 
 # VARIABLES-------------------------------------------
 
+happi_tails = Shelter.new("Happi Tails")
+
+initial_menu = [
+  "1:  display all animals",
+  "2:  display all persons",
+  "3:  create an animal",
+  "4:  create an person",
+  "5:  facilitate person adopts an animal",
+  "6:  facilitate person surrendering an animal up for adoption",
+  "q:  quit",
+  ""]
+
+
+# add_person_menu = {
+
+# }
+
+# add_animal_menu = [
+#     "1:  To what shelter is the animal being surendered?",
+#     "2:  What is the animal's name?",
+#     "3:  What is the animal's age?",
+#     "4:  Is the animal male or female?",
+#     "5:  What species is the animal"]
 
 
 #CODE-------------------------------------------------
 
-happi_tails = Shelter.new("Happi Tails")
+# add_person(happi_tails, "tom", 2, 35)
+# add_person(happi_tails, "joe", 0, 22)
+# add_person(happi_tails, "jen", 4, 44)
+# add_person(happi_tails, "sally", 0, 30)
+
+# add_animal(happi_tails, "bubbles",  2,  "female", "cat")
+# add_animal(happi_tails, "hank",     7,  "male",   "dog")
+# add_animal(happi_tails, "frog",     4,  "male",   "cat")
+# add_animal(happi_tails, "tweets",   11, "female", "bird")
+# add_animal(happi_tails, "rocky",    1,  "male",   "turtle")
+# add_animal(happi_tails, "horace",   13, "male",   "horse")
+# add_animal(happi_tails, "snickers", 6,  "female",   "cat")
+
+# adopt(p3, happi_tails)
+# adopt(p4, happi_tails)
+
+# surrender(p3, happi_tails)
 
 
-p1 = Person.new("tom", 2, 35)
-p2 = Person.new("joe", 0, 22)
-p3 = Person.new("jen", 4, 44)
-p4 = Person.new("sally", 0, 30)
-happi_tails.people.push(p1, p2, p3, p4)
+puts "Welcome to Happi Tails, please select from the following menu:"
+choice = ""
+#until ["1", "2", "3", "4", "5", "6"].include? choice
+until choice == "q"
+  main_menu(initial_menu)
+  choice = ask("Please select the number of the item from the menu to proceed:")
+
+  # display all animals
+  if choice == "1"
+    display_animals(happi_tails)
+  elsif choice == "2"
+    display_people(happi_tails)
+  elsif choice == "3"
+    new_animal_record(happi_tails)
+  elsif choice == "4"
+    new_person_record(happi_tails)
+  elsif choice == "5"
+    adopt(happi_tails)
+  elsif choice == "6"
+    surrender(happi_tails)
+  elsif choice == "q"
+    puts "Goodbye."
+  else
+    puts "Please make a valid selection from the menu."
+  end
+end
 
 
 
 
-a1 = Animal.new("bubbles",  2,  "female", "cat")
-a2 = Animal.new("hank",     7,  "male",   "dog")
-a3 = Animal.new("frog",     4,  "male",   "cat")
-a4 = Animal.new("tweets",   11, "female", "bird")
-a5 = Animal.new("rocky",    1,  "male",   "turtle")
-
-a6 = Animal.new("horace",   13, "male",   "horse")
-a7 = Animal.new("snickers", 6,  "female",   "cat")
-happi_tails.animals.push(a1, a2, a3, a4, a5)
-
-
-p3.pets << a6
-p4.pest << a7
 
 
 
-adopt(p3, happi_tails)
-
-surrender(p3, happi_tails)
 
 
