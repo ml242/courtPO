101e58da7cfc5fd72d32e5f9a30c9b5b9f3b559f
Harrison Powers <fijimunkii@gmail.com>
2013-10-02 08:14:40 -0400

    links to stops

--- w02/d02/harrison_powers/sinatra-mta/app.rb
+++ w02/d02/harrison_powers/sinatra-mta/app.rb
@@ -5,6 +5,17 @@ require_relative 'train_lines'
 
 get '/plan' do
 	unordered_list = '<ul>'
-	MTA.each do |line, stops| unordered_list += "<li>#{line}</li>" end
+	MTA.each do |line, stops|
+		unordered_list += "<li><a href=\"/plan/#{line}\">#{line}</a></li>"
+	end
+	unordered_list += '</ul>'
+end
+
+get '/plan/:line' do
+	line = params[:line].to_sym
+	unordered_list = '<ul>'
+	MTA[line].each do |stop|
+		unordered_list += "<li>#{stop}</li>"
+	end
 	unordered_list += '</ul>'
 end
