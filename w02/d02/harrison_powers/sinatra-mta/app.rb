57109a276c3abedb16b5f8bbfb09f6e7637d235c
Harrison Powers <fijimunkii@gmail.com>
2013-10-02 08:39:58 -0400

    start the journey

--- w02/d02/harrison_powers/sinatra-mta/app.rb
+++ w02/d02/harrison_powers/sinatra-mta/app.rb
@@ -19,3 +19,22 @@ get '/plan/:line' do
 	end
 	unordered_list += '</ul>'
 end
+
+get '/journey' do
+
+	# /journey?start_train=g_train&stop_1=nassau&end_train=l_train&stop_2=bedford
+	# {:start_train => "g_train", :stop_1 => "nassau", :end_train => "l_train" :stop_2 => "bedford"}
+
+	start_train = params[:start_train].to_sym
+	stop_1 = params[:stop_1].to_sym
+	end_train = params[:end_train].to_sym
+	stop_2 = params[:stop_2].to_sym
+
+	###################################
+	#
+	# Need to include mta app logic
+	# then just call the method
+	#
+	###################################
+
+end
