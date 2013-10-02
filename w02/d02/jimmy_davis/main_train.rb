b1283358addba5e99a4dad11d49f81564d5371a4
James Davis <jamesmdavis5@gmail.com>
2013-10-01 20:59:18 -0400

    created files for 3 trains, main, and train stylesheet, added html and css styles

---
+++ w02/d02/jimmy_davis/main_train.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+# Your ruby code will need to include a constant that points to a hash called MTA
+# MTA = { :l_train => ['bedford', '1st ave'], :g_train => ['greenpoint', 'nassau'], :n_train => ['canal', '8th'], }
+# the keys are train lines. the values are arrays of station name strings
+
+MTA = {
+  :l_train => ['bedford', '1st ave'],
+  :g_train => ['greenpoint', 'nassau'],
+  :n_train => ['canal', '8th'], }
