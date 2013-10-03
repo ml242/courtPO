a774d5707ebcb12316b609b9113e71b4e0c7301b
Will Smith <will.smith@preplaysports.com>
2013-10-03 00:11:24 -0400

    Completed HW

--- w02/d03/will_smith/distillery/main.rb
+++ w02/d03/will_smith/distillery/main.rb
@@ -1,18 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
-
+# require 'pry'
+# require 'pry-nav'
 require_relative("beverage")
 require_relative("marketing_department")
 require_relative("distillery")
 require_relative("person")
 
-# bev1 = Beverage.new("Stella")
-# p bev1
-
-# marketing_department = MarketingDepartment.new
-# p marketing_department
-# p marketing_department.generate_clever_name
-
+marketing_department = MarketingDepartment.new
+p marketing_department
+p marketing_department.generate_clever_name
 dist1 = Distillery.new
 p dist1
 dist1.brew_beverage
@@ -21,26 +18,45 @@ dist1.brew_beverage
 dist1.brew_beverage
 dist1.brew_beverage
 dist1.brew_beverage
-# # p dist1.beverage_count
-# # dist1.sell_beverage
-# # p dist1.beverage_count
+p dist1.beverage_count
+
+dist1.sell_beverage
+p dist1.beverage_count
 
 bev = dist1.sell_beverage
+bev1 = dist1.sell_beverage
+bev2 = dist1.sell_beverage
+bev3 = dist1.sell_beverage
+bev4 = dist1.sell_beverage
+bev5 = dist1.sell_beverage
+bev6 = dist1.sell_beverage
+bev7 = dist1.sell_beverage
+bev8 = dist1.sell_beverage
+bev9 = dist1.sell_beverage
+bev10 = dist1.sell_beverage
 
 person = Person.new("Jonathan")
 p person
 person.buy_beverage(bev)
-p person
-person.drink_beverage
-p person
+person.buy_beverage(bev1)
+person.buy_beverage(bev2)
+person.buy_beverage(bev3)
+person.buy_beverage(bev4)
+person.buy_beverage(bev5)
+person.buy_beverage(bev6)
+person.buy_beverage(bev7)
+person.buy_beverage(bev8)
+person.buy_beverage(bev9)
+person.buy_beverage(bev10)
+
 
 get "/beverage_stock" do
   @count = dist1.beverage_count
   erb :stock
 end
 
-get "/drink_beverage" do
-    @drunk = person.drink_beverage
+get "/buy_beverage" do
+    @drunk = person.buy_beverage(bev)
     erb :drinks
 end
 
