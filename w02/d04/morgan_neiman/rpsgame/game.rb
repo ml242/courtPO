2d163ae4fa0d2a754d0bcc61890b564b6236b67f
Morgan Neiman <quackhouse@me.com>
2013-10-03 18:52:28 -0400

    made multiplayer work

--- w02/d04/morgan_neiman/rpsgame/game.rb
+++ w02/d04/morgan_neiman/rpsgame/game.rb
@@ -3,21 +3,21 @@ def game(input1, input2)
     outcome = "It's a tie!"
   elsif input1 == "rock"
     if input2 == "paper"
-      outcome = "Paper covers rock. You lose!"
+      outcome = "Paper covers rock. Player 1 loses!"
     elsif input2 == "scissors"
-      outcome = "Rock smashes scissors. You win!"
+      outcome = "Rock smashes scissors. Player 1 wins!"
     end
   elsif input1 == "paper"
     if input2 == "scissors"
-      outcome = "Scissors cuts paper. You lose!"
+      outcome = "Scissors cuts paper. Player 1 loses!"
     elsif input2 == "rock"
-      outcome = "Paper covers rock. You win!"
+      outcome = "Paper covers rock. Player 1 wins!"
     end
   elsif input1 == "scissors"
     if input2 == "rock"
-      outcome = "Rock smashes scissors. You lose!"
+      outcome = "Rock smashes scissors. Player 1 loses!"
     elsif input2 == "paper"
-      outcome = "Scissors cut paper. You win!"
+      outcome = "Scissors cut paper. Player 1 winss!"
     end
   end
   return outcome
--- w02/d04/morgan_neiman/rpsgame/main.rb
+++ w02/d04/morgan_neiman/rpsgame/main.rb
@@ -10,10 +10,25 @@ end
 
 get '/result' do
   @user_choice = params[:choice]
+  @second_choice = params[:choice2]
   if @user_choice != nil
     @user_choice.downcase!
   end
-  @comp_choice = play_options.sample
-  @outcome = game(@user_choice, @comp_choice)
+  if @second_choice == nil
+  @second_choice = play_options.sample
+  @player2 = "The computer"
+  else
+    @second_choice.downcase!
+    @player2 = "Player 2"
+  end
+
+  @outcome = game(@user_choice, @second_choice)
+  if @outcome == nil
+    @outcome = "You chose a fake thing. You suck."
+  end
   erb :result
+end
+
+get '/multi_player' do
+  erb :multi_player
 end
