88f57c66779419c8fada142d8cfba8c532bd0308
Katherine Santiago <santiago.kat@gmail.com>
2013-10-03 20:14:51 -0400

    added arrays for winners and losers statements

--- w02/d04/katherine_santiago/rock_paper_scissors/main.rb
+++ w02/d04/katherine_santiago/rock_paper_scissors/main.rb
@@ -10,5 +10,8 @@ end
 get '/game' do
     @choice = params[:choice]
     @comp = ["rock", "paper", "scissors"].shuffle.sample
+    @winner = ["You rock!", "Yayer!", "Woot!", "You're a winner!"].shuffle.sample
+    @loser = ["Ewwww", "No way, Jose!", "Awww, you suck!", "Sigh.", ":(", "You stink at this."].shuffle.sample
+    @tie = ["No bueno", "boring...", "snore"].shuffle.sample
     erb :game
 end
