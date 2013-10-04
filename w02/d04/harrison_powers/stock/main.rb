62a1805db8160bebfa85e05a23ece6d08b2d2d3e
Harrison Powers <fijimunkii@gmail.com>
2013-10-04 11:22:51 -0400

    ticker time

--- w02/d04/harrison_powers/stock/main.rb
+++ w02/d04/harrison_powers/stock/main.rb
@@ -3,11 +3,65 @@ require 'sinatra/reloader' if development?
 require 'yahoofinance'
 require 'active_support/all'
 
+TOP_50 = [
+	"AAPL",
+	"AIG",
+	"CA",
+	"NLY",
+	"ORCL",
+	"ROST",
+	"SNDK",
+	"SNV",
+	"TJX",
+	"WEN",
+	"YHOO",
+	"FITB",
+	"TXN",
+	"CSCO",
+	"GT",
+	"HBAN",
+	"IBM",
+	"JPM",
+	"KEY",
+	"KRFT",
+	"LVS",
+	"SBUX",
+	"SCHW",
+	"TIVO",
+	"COP",
+	"DE",
+	"FOXA",
+	"WFC",
+	"MU",
+	"PBR",
+	"QIHU",
+	"SPF",
+	"TIBX",
+	"BA",
+	"CMCSA",
+	"DIS",
+	"DTV",
+	"EXC",
+	"HCP",
+	"KMI",
+	"KR",
+	"LUV",
+	"NKE",
+	"SE",
+	"SLB",
+	"V",
+	"OPK",
+	"WCRX",
+	"LLY",
+	"MDLZ"
+]
+
 get '/' do
+	@top_50 = ''
+  TOP_50.each do |x| @top_50 << "#{x} : <strong>#{YahooFinance::get_quotes(YahooFinance::StandardQuote, x)[x].lastTrade}</strong>  |  " end
   if params[:symbol].present?
     @symbol = params[:symbol].upcase
     @quote = YahooFinance::get_quotes(YahooFinance::StandardQuote, @symbol)[@symbol].lastTrade
   end
-
   erb :quote
 end
