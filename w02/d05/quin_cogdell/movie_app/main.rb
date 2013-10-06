3982c10a77da001e5d6d080f4e603aa6a9283103
Quin Cogdell <quincogdell@gmail.com>
2013-10-06 10:07:49 -0400

    search results return to list of hashes

--- w02/d05/quin_cogdell/movie_app/main.rb
+++ w02/d05/quin_cogdell/movie_app/main.rb
@@ -3,21 +3,47 @@ require 'sinatra/reloader' if development?
 require 'pry'
 require 'httparty'
 require 'json'
+require 'rainbow'
+require 'cgi'
 
-url = "http://www.omdbapi.com/?t=Rambo"
-#parsed_result = JSON.parse(response)
+# http://www.omdbapi.com/
+# s (NEW!)   string (optional)   title of a movie to search for
+# i  string (optional)   a valid IMDb movie id
+# t  string (optional)   title of a movie to return
+# y  year (optional)   year of the movie
+# r  JSON, XML   response data type (JSON default)
+# plot   short, full   short or extended plot (short default)
+# callback   name (optional)   JSONP callback name
+# tomatoes   true (optional)   adds rotten tomatoes data
+
+
+
+
+
 
 get '/' do
-  response = HTTParty.get("http://graph.facebook.com/quincogdell")
-  first_name = response['first_name']
-  first_name
-  #erb :index
+  erb :index
 end
 
 get '/movies/search' do
+  # t = params[:query]
+  erb :search
+end
+
+
+get '/movies/results' do
+  query = CGI::escape(params[:query])
+  url = "http://www.omdbapi.com/?"
+  s = 's=' + query
+  url += s
   response = HTTParty.get(url)
   parsed_result = JSON.parse(response)
-  @results = parsed_result.to_s
+  puts parsed_result.to_s.color(:red)
+  @results = parsed_result['Search']
+  #binding.pry
   erb :results
 end
+
+
+
 
