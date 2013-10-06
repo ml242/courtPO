cbd097306d24b01a039b4e1f82a1e1a5bece239f
Will Smith <will.smith@preplaysports.com>
2013-10-06 12:08:53 -0400

    movies with more than one word searchable

--- w02/d05/will_smith/movie_app/main.rb
+++ w02/d05/will_smith/movie_app/main.rb
@@ -13,10 +13,20 @@ get '/movies' do
     erb :movies
 end
 
+# When a user submits the search form, redirect them to localhost:4567/movies/search and display the list of returned movie titles
 get "/movies/search" do
-  movie_title = params[:movie]
-  url = "http://www.omdbapi.com/?i=&t=#{movie_title}"
-  response = HTTParty.get(url)
-  p response.to_s
+    movie_title = params[:movie]
+         if movie_title.split(' ').length > 1
+            movie_title_array = movie_title.split(' ')
+            movie_title = ""
+            movie_title_array.each do |word|
+              movie_title += word + "+"
+        end
+        else
+        movie_title = params[:movie]
+        end
+    url = "http://www.omdbapi.com/?i=&t=#{movie_title}"
+    response = HTTParty.get(url)
+    p response.to_s
 end
 
