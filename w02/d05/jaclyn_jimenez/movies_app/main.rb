1414dbbf17e223c217c48def53349ba78ce7342f
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-10-06 14:23:10 -0400

    program functions with multiple word search entries

--- w02/d05/jaclyn_jimenez/movies_app/main.rb
+++ w02/d05/jaclyn_jimenez/movies_app/main.rb
@@ -2,7 +2,10 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'httparty'
 require 'json'
+require 'cgi'
 require 'pry'
+
+#later: do if title.nil? display page that says no results were found.
 
 movie_titles = []
 
@@ -16,13 +19,13 @@ get '/movies/search' do
 end
 
 post '/movies/search' do
-  @title = params["title"]
-  url = "http://www.omdbapi.com/?s=" + @title
+  @encoded_title = CGI::escape(params["title"])
+  url = "http://www.omdbapi.com/?s=" + @encoded_title
   response = HTTParty.get(url)
   parsed_result = JSON.parse(response)
   search_results = parsed_result["Search"]
+  movie_titles.clear
   search_results.each do |hash|
-    binding.pry
     @movie_title = hash["Title"]
     movie_titles.push(@movie_title.to_s)
   end
