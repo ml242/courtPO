1e25d68a0505dbee6f3d7ca92f941d3775f839a7
James Davis <jamesmdavis5@gmail.com>
2013-10-06 17:34:28 -0400

    movie title and year are printing out, but always the same movie everytime, working to fix now

--- w02/d05/jimmy_davis/main.rb
+++ w02/d05/jimmy_davis/main.rb
@@ -5,3 +5,51 @@ require 'sinatra/reloader' if development?
 require 'pry'
 require 'rainbow'
 require 'json'
+require 'httparty'
+require 'CGI'
+
+get '/' do
+  erb :index
+end
+
+# from matt:
+# get '/:movie_title' do
+#   movie_title = params[:movie_title]
+#   movie_title = CGI::escape(movie_title)
+#   url = ("http://www.omdbapi.com/?s=")
+#   result = HTTParty.get(url+movie_title)
+#   parsed_result = JSON.parse(result.body)
+#   results = parsed_result
+#   movie_list1 = []
+#   results["Search"].each do |movie|
+#     movie_list1 << movie["Title"]
+#     @movie_list1 = movie_list1
+#   end
+#     erb :movie
+# end
+
+
+get '/:movie_title' do
+  movie_title = params[:movie_title]
+  movie_title = CGI::escape(movie_title)
+  url = "http://www.omdbapi.com/?s=#{movie_title}"
+  response = HTTParty.get(url)
+  parsed_result = JSON.parse(response.body)
+  # movie_list = []
+  # title = parsed_result["Search"][0]["Title"]
+  # year = parsed_result["Search"][0]["Year"]
+  result = parsed_result["Search"][0]
+  title = result["Title"]
+  year = result["Year"]
+  @title = title
+  @year = year
+  erb :movie
+end
+
+# get 'movie/:imdbid' do
+#   imdid = params[:imdbID]
+#   url = url = "http://www.omdbapi.com/?s=#{movie_title}"
+#   response = HTTparty.get(url)
+#   parsed_result = JSON.parse(response.body)
+#   @imdbid = "www.imdb.com/title/#{imdbid}"
+# end
