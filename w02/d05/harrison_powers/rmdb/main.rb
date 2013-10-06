ca02a121454682641333d0733815d185094ccc0f
Harrison Powers <fijimunkii@gmail.com>
2013-10-05 21:39:55 -0400

    rmdb complete

--- w02/d05/harrison_powers/rmdb/main.rb
+++ w02/d05/harrison_powers/rmdb/main.rb
@@ -2,17 +2,24 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'json'
 require 'httparty'
+require 'pry'
 
 get '/' do
+	@title = 'RMDB'
 	erb :index
 end
 
 post '/movies/search' do
-
+	@title = "Search for #{params[:movie_search]}"
+	search_request = params[:movie_search].split(' ').join('+')
+	omdb_result = HTTParty.get("http://www.omdbapi.com/?i=&t=#{search_request}")
+	@parsed_result = JSON.parse(omdb_result)
 	erb :search_result
 end
 
 get '/movies/:imdbID' do
-
+	imdbID = params[:imdbID]
+	omdb_result = HTTParty.get("http://www.omdbapi.com/?i=#{imdbID}&plot=full&tomatoes=true")
+	@parsed_result = JSON.parse(omdb_result)
 	erb :movie_info
 end
