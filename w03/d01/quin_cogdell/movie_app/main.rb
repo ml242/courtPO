0661c208c278e15c283d8134c89595657e074a1f
Quin Cogdell <quincogdell@gmail.com>
2013-10-07 17:44:31 -0400

    same as app from w02_d5

---
+++ w03/d01/quin_cogdell/movie_app/main.rb
@@ -0,0 +1,63 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+# require 'httparty'
+# require 'json'
+require 'rainbow'
+require 'cgi'
+require 'pry'
+require 'pg'
+
+require_relative 'movie_app_methods'
+
+get '/' do
+  erb :search
+end
+
+get '/movies/search' do
+  erb :search
+end
+
+get '/movies/results' do
+  query = CGI::escape(params[:query])
+  url = "http://www.omdbapi.com/?"
+  s = 's=' + query
+  url += s
+  api_call_search(url)
+
+  erb :results
+end
+
+get '/movies/:imdbID' do
+  url = "http://www.omdbapi.com/?"
+  imdbID = params[:imdbID]
+  i = 'i=' + imdbID
+  url += i
+  api_call_imdbID(url)
+  erb :movie
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
