4ae8a3669a2c35646425bade3d453fe44e0fd311
Cooper Mayne <coopermayne@gmail.com>
2013-10-07 00:41:53 -0400

    happy face

--- w02/d05/cooper_mayne/imdb/main.rb
+++ w02/d05/cooper_mayne/imdb/main.rb
@@ -4,24 +4,23 @@ require 'httparty'
 require 'json'
 require 'pry'
 
-get "/" do
-  #search form
+get "/" do  # SEARCH FORM 
   erb :index
 end
 
 get "/movies/search" do
-  #display all the titles that match their search
-  #make them into links which go to full info on that movie
+  # DISPLAY ALL THE TITLES THAT MATCH THEIR SEARCH
+  # MAKE THEM INTO LINKS WHICH GO TO FULL INFO ON THAT MOVIE
 
-  @query = params[:title]
-  query = @query.gsub(" ","+")
+  query = params[:title].gsub(" ","+")
   response = HTTParty.get("http://www.omdbapi.com/?s=#{query}")
   r = JSON.parse(response)
-  unless r.has_key? "Search"
+
+  unless r.has_key? "Search" # IN CASE THERE ARE NO RESULTS! 
     erb :nores 
   else
-  @results =  r["Search"]
-  erb :search
+    @results =  r["Search"]
+    erb :search
   end
 end
 
