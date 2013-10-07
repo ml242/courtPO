5c3644cda9fb16ae0a05a6492379b8c3f50969e5
Tom Metzger <metzgert@gmail.com>
2013-10-07 12:18:53 -0400

    tried homework again... still failed

--- w02/d05/tom_metzger/omdb_homework/main.rb
+++ w02/d05/tom_metzger/omdb_homework/main.rb
@@ -13,7 +13,7 @@ require 'httparty'
 
 
 get '/' do
-  erb :movies
+  erb :index
 end
 
 
@@ -25,17 +25,67 @@ get '/movies' do
   # binding.pry
   # @parsed.each {|movie| @titles << movie["Title"]}
   # @parsed.each {|movie| @id << movie["imdbID"]}
-  erb :movies
+  erb :imdbID
 end
 
 
 
-get '/search' do
-  @movie = params[:i]
-  if @movie["Poster"] = String
-    @poster =
-  # erb :results
-end
+# get '/search' do
+#   @movie = params[:i]
+#     @poster =
+#   # erb :results
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+# require 'sinatra'
+# require 'sinatra/reloader' if development?
+# require 'pry'
+
+# require 'json'
+# require 'httparty'
+
+# @titles = []
+# @id = []
+
+# @attributes = ["Title", "Year", "Rated", "Released", "Runtime", "Genre", "Director", "Writer", "Actors", "Plot"]
+
+
+
+# get '/' do
+#   erb :movies
+# end
+
+
+# get '/movies' do
+#   title = params[:query]
+#   result = HTTParty.get("http://www.omdbapi.com/?s=#{title}")
+#   parsed_result = JSON.parse(result.body)
+#   @parsed = parsed_result["Search"]
+#   # binding.pry
+#   # @parsed.each {|movie| @titles << movie["Title"]}
+#   # @parsed.each {|movie| @id << movie["imdbID"]}
+#   erb :movies
+# end
+
+
+
+# get '/search' do
+#   @movie = params[:i]
+#   if @movie["Poster"] = String
+#     @poster =
+#   # erb :results
+# end
 
 
 
