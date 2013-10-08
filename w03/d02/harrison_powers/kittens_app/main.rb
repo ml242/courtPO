645f9f015e4b1f8174e736b67f2e24030804930b
Harrison Powers <fijimunkii@gmail.com>
2013-10-08 18:48:14 -0400

    this is way easier with erbs...

--- w03/d02/harrison_powers/kittens_app/main.rb
+++ w03/d02/harrison_powers/kittens_app/main.rb
@@ -2,18 +2,28 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'pry'
 require 'pg'
+require 'json'
 
 get '/' do
   #nothing
 end
 
 post '/kittens' do
+  name = params[:name]
+  age = params[:age].to_i
+  is_cute = params[:is_cute]
+  image_url = params[:image_url]
   db_connect = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
-  sql = "INSERT INTO kittens (name, age, is_cute, image) VALUES ('#{name}', #{age}, #{is_cute}, '#{image}')"
+  sql = "INSERT INTO kittens (name, age, is_cute, image_url) VALUES ('#{name}', #{age}, #{is_cute}, '#{image_url}')"
   db_connect.exec(sql)
-  sql = "SELECT * FROM kittens WITH name = '#{name}', age = #{age}, is_cute = #{is_cute}, image = '#{image}'"
+  sql = "SELECT * FROM kittens ORDER BY id DESC LIMIT 1"
   results = db_connect.exec(sql)
   @kitten = results.entries
+  kitten_string = ''
+  @kitten[0].each do |x, y|
+    kitten_string << "#{x} => #{y}, "
+  end
+  kitten_string
 end
 
 get '/kittens' do
@@ -27,7 +37,7 @@ end
 get '/kittens/:id' do
   id = params[:id]
   db_connect = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
-  sql = "SELECT * FROM kittens WITH id = #{id}"
+  sql = "SELECT * FROM kittens WHERE id = #{id}"
   results = db_connect.exec(sql)
   db_connect.close
   @kitten = results.entries
