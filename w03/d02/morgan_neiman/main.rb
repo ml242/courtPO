3fc777b96c28d54990fa95949d1a8529b93fcdb7
Morgan Neiman <quackhouse@me.com>
2013-10-08 18:27:23 -0400

    ok made it work except for alternate text for images that dont load

--- w03/d02/morgan_neiman/main.rb
+++ w03/d02/morgan_neiman/main.rb
@@ -23,20 +23,59 @@ get '/kittens' do
   db_conn = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
   sql = "SELECT * FROM kittens"
   response = db_conn.exec(sql)
+  @photoarray = []
+  @kittyarray = []
   response.entries.each do |entry|
     name = entry["name"]
     age = entry["age"]
-    kitten_string = "This kitten's name is #{}, they are #{} old, "
+    if entry["is_cute"] == "f"
+      cute = "they are not cute"
+    else
+      cute = "they are cute"
+    end
+    @photoarray << entry["image_url"]
+    kitten_string = "This kitten's name is #{name}, they are #{age} years old, and #{cute}."
+    @kittyarray << kitten_string
   end
   db_conn.close
+  erb :kittens
+end
+
+get '/kittens/delete_kitty' do
+  erb :delete
+end
+
+
+post '/kittens/delete' do
+  id = params[:id].to_i
+  db_conn = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
+  sql = "DELETE FROM kittens WHERE id = #{id}"
+  db_conn.exec(sql)
+  db_conn.close
+  redirect to '/kittens'
 end
 
 get '/kittens/:id' do
-  id = params[:id]
+  id = params[:id].to_i
   db_conn = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
   sql = "SELECT * FROM kittens WHERE id = #{id}"
   response = db_conn.exec(sql)
-  response.entries.to_s
+  @photoarray = []
+  @kittyarray = []
+  response.entries.each do |entry|
+    name = entry["name"]
+    age = entry["age"]
+    if entry["is_cute"] == "f"
+      cute = "they are not cute"
+    else
+      cute = "they are cute"
+    end
+    @photoarray << entry["image_url"]
+    kitten_string = "This kitten's name is #{name}, they are #{age} years old, and #{cute}."
+    @kittyarray << kitten_string
+  end
   db_conn.close
+  erb :kittens
 end
+
 
