c4a11e8cef529f429054c5a6c4137f580d668874
Harrison Powers <fijimunkii@gmail.com>
2013-10-08 18:16:07 -0400

    phase 1

--- w03/d02/harrison_powers/kittens_app/main.rb
+++ w03/d02/harrison_powers/kittens_app/main.rb
@@ -4,4 +4,31 @@ require 'pry'
 require 'pg'
 
 get '/' do
+  #nothing
+end
+
+post '/kittens' do
+  db_connect = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
+  sql = "INSERT INTO kittens (name, age, is_cute, image) VALUES ('#{name}', #{age}, #{is_cute}, '#{image}')"
+  db_connect.exec(sql)
+  sql = "SELECT * FROM kittens WITH name = '#{name}', age = #{age}, is_cute = #{is_cute}, image = '#{image}'"
+  results = db_connect.exec(sql)
+  @kitten = results.entries
+end
+
+get '/kittens' do
+  db_connect = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
+  sql = 'SELECT * FROM kittens'
+  results = db_connect.exec(sql)
+  db_connect.close
+  @kittens = results.entries
+end
+
+get '/kittens/:id' do
+  id = params[:id]
+  db_connect = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
+  sql = "SELECT * FROM kittens WITH id = #{id}"
+  results = db_connect.exec(sql)
+  db_connect.close
+  @kitten = results.entries
 end
