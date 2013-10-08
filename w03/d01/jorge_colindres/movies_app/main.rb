f8181dcd4dd7df0a5a06810ab3ba1b5652eb6447
JC <jorge.e.colindres@gmail.com>
2013-10-07 21:47:10 -0400

    succesfully posting to db

--- w03/d01/jorge_colindres/movies_app/main.rb
+++ w03/d01/jorge_colindres/movies_app/main.rb
@@ -3,6 +3,7 @@ require 'sinatra/reloader' if development?
 require 'httparty'
 require 'json'
 require 'cgi'
+require 'pg'
 
 require 'pry'
 require 'pry-nav'
@@ -11,6 +12,10 @@ require 'pry-remote'
 
 get '/' do
   erb :index
+end
+
+get '/favorites' do
+  "works"
 end
 
 get '/search' do
@@ -45,7 +50,8 @@ get '/:imdbID' do
     :director => movie_parsed_result['Director'],
     :writer => movie_parsed_result['Writer'],
     :actors => movie_parsed_result['Actors'],
-    :imdb_rating => movie_parsed_result['imdbRating']
+    :imdb_rating => movie_parsed_result['imdbRating'],
+    :imdb_id => movie_parsed_result['imdbID']
   }
   if @movie[:poster] == "N/A"
     @movie[:poster] = "http://placehold.it/330x450&text=No Poster"
@@ -53,4 +59,29 @@ get '/:imdbID' do
   end
   erb :index
 end
+
+post '/favorites' do
+  fav_movieID = params[:imdb_id]
+  response = HTTParty.get("http://www.omdbapi.com/?i=#{fav_movieID}")
+  fav_movie_parsed_result = JSON.parse(response)
+  title = fav_movie_parsed_result['Title']
+  poster = fav_movie_parsed_result['Poster']
+  poster_alt = fav_movie_parsed_result['Title']
+  genre = fav_movie_parsed_result['Genre']
+  runtime = fav_movie_parsed_result['Runtime']
+  rated = fav_movie_parsed_result['Rated']
+  plot = fav_movie_parsed_result['Plot']
+  director = fav_movie_parsed_result['Director']
+  writer = fav_movie_parsed_result['Writer']
+  actors = fav_movie_parsed_result['Actors']
+  imdb_rating = fav_movie_parsed_result['imdbRating']
+
+  db_connection = PG.connect :dbname => 'movies_db', :host=> 'localhost'
+  sql = "INSERT INTO movies (title, poster, posteralt, genre, runtime, rated, plot, director, writer, actors, imdbrating)
+  VALUES ('#{title}', '#{poster}', '#{poster_alt}', '#{genre}', '#{runtime}', '#{rated}', '#{plot}', '#{director}', '#{writer}', '#{actors}', '#{imdb_rating}')"
+  db_connection.exec sql
+  db_connection.close
+  redirect '/favorites'
+end
+
 
