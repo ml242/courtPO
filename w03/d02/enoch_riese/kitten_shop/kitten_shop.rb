326c9c3a56df3d229cce940a584a813e946f8812
Enoch Riese <enoch.riese@gmail.com>
2013-10-09 00:05:10 -0400

    finished phase 2, finished hw

--- w03/d02/enoch_riese/kitten_shop/kitten_shop.rb
+++ w03/d02/enoch_riese/kitten_shop/kitten_shop.rb
@@ -3,6 +3,10 @@ require "pry"
 require "sinatra"
 require "sinatra/reloader"
 
+get "/kittens/new" do
+  erb :new
+end
+
 post "/kittens" do
   name = params[:name]
   age = params[:age]
@@ -14,36 +18,21 @@ post "/kittens" do
   redirect to "/kittens"
 end
 
-# - Creates a new kitten in the database
-# - returns a hash representation of that kitten
-
 get "/kittens" do
-   db_conn = PG.connect(:dbname => 'kitten_shop_db', :host => 'localhost')
-   sql = "SELECT * FROM kittens;"
-   response = db_conn.exec(sql)
-   db_conn.close
-   response.entries.to_s
- end
-# - Asks the database for all of the kittens
-# - returns an array of hashes where each hash represents the properties of the kitten
+  db_conn = PG.connect(:dbname => 'kitten_shop_db', :host => 'localhost')
+  sql = "SELECT * FROM kittens;"
+  response = db_conn.exec(sql)
+  db_conn.close
+  @kittens = response.entries
+  erb :kittens
+end
 
 get  "/kittens/:id" do
   id = params[:id]
   db_conn = PG.connect(:dbname => 'kitten_shop_db', :host => 'localhost')
-   sql = "SELECT * FROM kittens WHERE id = '#{id}';"
-   response = db_conn.exec(sql)
-   db_conn.close
-   response.entries.to_s
- end
-# - Asks the database for a single kitten
-# - returns a single hash representing a single kitten
-
-# # EVERYTHING AFTER IS EXTRA CREDIT
-
-# POST /kittens/:id/delete
-# - Removes a kitten from the database
-# - returns `true` if it was able to remove the kitten
-
-# POST /kittens/:id
-# - takes any of the passed in parameters
-# - updates only those parameters for a single kitten
+  sql = "SELECT * FROM kittens WHERE id = '#{id}';"
+  response = db_conn.exec(sql)
+  db_conn.close
+  @kittens = response.entries
+  erb :kittens
+end
