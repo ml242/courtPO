b7fc62abf0f5b8a732328e7a21d0b252f8f96ed0
Travis Vander Hoop <vanderhoop@me.com>
2013-10-08 20:10:42 -0400

    successfully storing my posts to psql database

---
+++ w03/d02/travis_vanderhoop/kitten_shop_app/main.rb
@@ -0,0 +1,47 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pry'
+require 'pg'
+
+get '/' do
+  "/ Works"
+end
+
+post '/kittens' do
+  name = params[:name]
+  is_cute = params[:is_cute]
+  image_url = params[:image_url]
+  db_conn = PG.connect(:dbname => 'kitten_shop_db', :host => 'localhost')
+  sql = "INSERT INTO kittens (name, is_cute, image_url) VALUES ('#{name}', '#{is_cute}', '#{image_url}')"
+  db_conn.exec(sql)                 # creates a new kitten in the database
+  binding.pry
+  db_conn.close                       #always be closing
+  redirect to ('/kittens')
+
+# - returns a hash representation of that kitten
+  end
+
+get '/kittens' do
+
+# - Asks the database for all of the kittens
+# - returns an array of hashes where each hash represents the properties of the kitten
+
+end
+
+get '/kittens/:id' do
+  @kitten = params[:id]
+  "get /kittens/:id works"
+# - Asks the database for a single kitten
+# - returns a single hash representing a single kitten
+end
+
+
+# # EVERYTHING AFTER IS EXTRA CREDIT
+
+# POST /kittens:/id/delete
+# - Removes a kitten from the database
+# - returns `true` if it was able to remove the kitten
+
+# POST /kittens/:id
+# - takes any of the passed in parameters
+# - updates only those parameters for a single kitten
