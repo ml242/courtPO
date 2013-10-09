a4fba514f121780530928ba4358ddcb81dad01e1
Tom Metzger <metzgert@gmail.com>
2013-10-09 01:26:46 -0400

    finished the basic app

--- w03/d02/tom_metzger/kitten_shop_app/main.rb
+++ w03/d02/tom_metzger/kitten_shop_app/main.rb
@@ -6,18 +6,26 @@ require 'pg'
 require_relative 'kittens'
 
 get '/' do
-  "What would you like to do?"
-  #links to the different routes
+  erb :index
+end
+
+get '/add_kitten' do
+  erb :add_kitten
 end
 
 post '/kittens' do
   # Creates a new kitten in the database
   # returns a hash representation of that kitten
-  new_kitten = {:name => "marge", :age => 3, :is_cute => true, :image_url => "http://placekitten.com/300"}
+  new_kitten = params
+  # this was the test hash
+      # new_kitten = {:name => "marge", :age => 3, :is_cute => true, :image_url => "http://placekitten.com/300"}
   # puts kitten into database
   db_connect = PG.connect(:dbname => 'animals_db', :host => 'localhost')
-  sql = "INSERT INTO kittens (name, age, is_cute, image_url) VALUES ('#{new_kitten[:name]}', #{new_kitten[:age]}, #{new_kitten[:is_cute]}, '#{new_kitten[:url]}')"
-  # sql = "INSERT INTO kittens (name, age, is_cute, image_url) VALUES ('Chelsea', 3, true, 'http://placekitten.com/300')"
+    # sql query with symbols
+        #  sql = "INSERT INTO kittens (name, age, is_cute, image_url) VALUES ('#{new_kitten[name]}', #{new_kitten[:age]}, #{new_kitten[:is_cute]}, '#{new_kitten[:url]}')"
+    # original sql query
+        # sql = "INSERT INTO kittens (name, age, is_cute, image_url) VALUES ('Chelsea', 3, true, 'http://placekitten.com/300')"
+  sql = "INSERT INTO kittens (name, age, is_cute, image_url) VALUES ('#{new_kitten["name"]}', #{new_kitten["age"]}, '#{new_kitten["is_cute"]}', '#{new_kitten["image_url"]}')"
   result = db_connect.exec(sql)
   db_connect.close
   redirect to ('/kittens')
@@ -25,7 +33,7 @@ end
 
 get '/kittens' do
   # listing of all kittens in database
-   db_connect = PG.connect(:dbname => 'animals_db', :host => 'localhost')
+  db_connect = PG.connect(:dbname => 'animals_db', :host => 'localhost')
   sql = "SELECT * FROM kittens ORDER BY id"
   # sql = "INSERT INTO kittens (name, age, is_cute, image_url) VALUES ('Chelsea', 3, true, 'http://placekitten.com/300')"
   records = db_connect.exec(sql)
@@ -35,8 +43,20 @@ get '/kittens' do
 end
 
 get '/kittens/:id' do
-  "/kittens/:id GET route"
+  kitten_id = params["id"].to_i
+  db_connect = PG.connect(:dbname => 'animals_db', :host => 'localhost')
+  sql = "SELECT * FROM kittens WHERE id = #{kitten_id} ORDER BY id"
+  # sql = "INSERT INTO kittens (name, age, is_cute, image_url) VALUES ('Chelsea', 3, true, 'http://placekitten.com/300')"
+  records = db_connect.exec(sql)
+  @records = records.entries
+  db_connect.close
+  erb :kittens
 end
+
+get '/kitten/find' do
+  erb :find_kitten
+end
+
 
 
 
