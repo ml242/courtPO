f7a0cdb8b48f89451e0aa0aa4eed78c075ecc2cf
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-10-09 19:30:00 -0400

    owners are able to be associated with cats when adding or updatting a kitten. a cat's owner is listed on the animal's profile page

--- w03/d03/jaclyn_jimenez/pet_shop_db_app/main.rb
+++ w03/d03/jaclyn_jimenez/pet_shop_db_app/main.rb
@@ -17,12 +17,13 @@ get '/' do
 end
 
 post '/kittens' do
-  #{"name"=>"kitty", "age"=>"3", "img_url"=>"n/a", "is_cute"=>"true"}
+  #{"name"=>"kitty", "age"=>"3", "img_url"=>"n/a", "is_cute"=>"true", "owner_id" => owner}
   @name = params["name"]
   @age = params["age"]
   @img = params["img_url"]
   @cute = params["is_cute"]
-  response = db_exec("INSERT INTO kitten (name, age, img_url, is_cute) VALUES ('#{@name}', '#{@age}', '#{@img}', '#{@cute}')")
+  @owner_id = params["owner_id"]
+  response = db_exec("INSERT INTO kitten (name, age, img_url, is_cute, owner_id) VALUES ('#{@name}', '#{@age}', '#{@img}', '#{@cute}', '#{@owner_id}')")
   redirect to '/kittens'
 end
 
@@ -33,6 +34,8 @@ get '/kittens' do
 end
 
 get '/new_kitten' do
+    response = db_exec("SELECT * FROM owners")
+  @owners = response.entries
   erb :new_kitten
 end
 
@@ -41,6 +44,11 @@ get '/kittens/:id' do
   kitten = db_exec("SELECT * FROM kitten WHERE id = '#{@id}'")
   kitten = kitten.entries[0]
   @kitten = kitten
+  @owner_id = kitten["owner_id"]
+  if @owner_id != nil
+    owner = db_exec("SELECT * FROM owners WHERE id = '#{@owner_id}' ")
+    @owner = owner.entries[0]['name']
+  end
   erb :kitten_profile
 end
 
@@ -50,10 +58,12 @@ post '/kittens/:id/update' do
     @age = params["age"]
     @img = params["img_url"]
     @cute = params["is_cute"]
+    @owner_id = params["owner_id"]
   response = db_exec("UPDATE kitten SET name = '#{@name}' WHERE id = #{@id}")
   response = db_exec("UPDATE kitten SET age = #{@age} WHERE id = #{@id}")
   response = db_exec("UPDATE kitten SET img_url = '#{@img}' WHERE id = #{@id}")
   response = db_exec("UPDATE kitten SET is_cute = '#{@cute}' WHERE id = #{@id}")
+  response = db_exec("UPDATE kitten SET owner_id = '#{@owner_id}' WHERE id = #{@id}")
   redirect to "/kittens/#{@id}"
 end
 
@@ -62,6 +72,8 @@ get '/kittens/:id/update' do
   @id = params[:id]
   kitten = db_exec("SELECT name FROM kitten WHERE id = #{@id}")
   @kitten_before_update = kitten.entries
+  response = db_exec("SELECT * FROM owners")
+  @owners = response.entries
   erb :update
 end
 
