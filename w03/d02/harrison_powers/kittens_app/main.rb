692c17f069f18cc043214e9a1b582db74ba7442e
Harrison Powers <fijimunkii@gmail.com>
2013-10-09 18:52:12 -0400

    helper in the mix

--- w03/d02/harrison_powers/kittens_app/main.rb
+++ w03/d02/harrison_powers/kittens_app/main.rb
@@ -5,6 +5,16 @@ require 'pg'
 require 'json'
 require 'slim'
 
+
+helpers do
+  def db_exec(sql)
+    conn = PG.connect(:dbname =>'kittens_db', :host => 'localhost')
+    result = conn.exec(sql)
+    conn.close
+    result
+  end
+end
+
 get '/' do
   redirect to('/kittens')
 end
@@ -14,30 +24,25 @@ post '/kittens' do
   age = params[:age].to_i
   is_cute = params[:is_cute]
   image_url = params[:image_url]
-  db_connect = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
   sql = "INSERT INTO kittens (name, age, is_cute, image_url) VALUES ('#{name}', #{age}, #{is_cute}, '#{image_url}')"
-  db_connect.exec(sql)
+  db_exec(sql)
   sql = "SELECT * FROM kittens ORDER BY id DESC LIMIT 1"
-  results = db_connect.exec(sql)
+  results = db_exec(sql)
   @kitten = results.entries[0]
   slim :kittens_add
 end
 
 get '/kittens' do
-  db_connect = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
   sql = 'SELECT * FROM kittens'
-  results = db_connect.exec(sql)
-  db_connect.close
+  results = db_exec(sql)
   @kittens = results.entries
   slim :kittens_list
 end
 
 get '/kittens/edit/:id' do
   id = params[:id]
-  db_connect = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
   sql = "SELECT * FROM kittens WHERE id = #{id}"
-  results = db_connect.exec(sql)
-  db_connect.close
+  results = db_exec(sql)
   @kitten = results.entries[0]
   @title = "#{@kitten['name']}"
   slim :kitten_edit
@@ -58,19 +63,15 @@ post '/kittens/edit/:id' do
     is_cute = true
   end
   image_url = params[:image_url]
-  db_connect = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
   sql = "UPDATE kittens SET name = '#{name}', age = #{age}, is_cute = #{is_cute}, image_url = '#{image_url}' WHERE id = #{id}"
-  db_connect.exec(sql)
-  db_connect.close
+  db_exec(sql)
   redirect to("/kittens/#{id}")
 end
 
 get '/kittens/:id' do
   id = params[:id]
-  db_connect = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
   sql = "SELECT * FROM kittens WHERE id = #{id}"
-  results = db_connect.exec(sql)
-  db_connect.close
+  results = db_exec(sql)
   @kitten = results.entries[0]
   slim :kitten_profile
 end
