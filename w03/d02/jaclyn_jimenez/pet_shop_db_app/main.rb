3df4c413f20547112d8f269ec5aadf8a9026e38d
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-10-08 23:07:03 -0400

    site users can add, view all, view one, and update kittens. css styled. no delete option

--- w03/d02/jaclyn_jimenez/pet_shop_db_app/main.rb
+++ w03/d02/jaclyn_jimenez/pet_shop_db_app/main.rb
@@ -36,3 +36,41 @@ end
 get '/new_kitten' do
   erb :new_kitten
 end
+
+get '/kittens/:id' do
+   @id = params[:id]
+    db_connect = PG.connect(
+    :host => 'localhost',
+    :dbname => 'pet_shop_db')
+  sql = "SELECT * FROM kitten WHERE id = '#{@id}'"
+  kitten = db_connect.exec(sql)
+  kitten = kitten.entries[0]
+  @kitten = kitten
+  db_connect.close
+  erb :kitten_profile
+end
+
+post '/kittens/:id/update' do
+   @id = params[:id]
+    @name = params["name"]
+    @age = params["age"]
+    @img = params["img_url"]
+    @cute = params["is_cute"]
+    db_connect = PG.connect(
+    :host => 'localhost',
+    :dbname => 'pet_shop_db')
+  sql = "UPDATE kitten SET name = '#{@name}' WHERE id = #{@id}"
+  kitten = db_connect.exec(sql)
+  sql = "UPDATE kitten SET age = #{@age} WHERE id = #{@id}"
+  kitten = db_connect.exec(sql)
+  sql = "UPDATE kitten SET img_url = '#{@img}' WHERE id = #{@id}"
+  kitten = db_connect.exec(sql)
+  sql = "UPDATE kitten SET is_cute = '#{@cute}' WHERE id = #{@id}"
+  kitten = db_connect.exec(sql)
+  redirect to "/kittens/#{@id}"
+end
+
+
+get '/kittens/:id/update' do
+  erb :update
+end
