ccb32e854893c731f81aac683449d709502f4162
DeProdigy <alex.hint.ah@gmail.com>
2013-10-09 21:26:24 -0400

    finished homework

--- w03/d03/alex_hint/kitten_shop_app/main.rb
+++ w03/d03/alex_hint/kitten_shop_app/main.rb
@@ -3,23 +3,44 @@ require 'sinatra/reloader' if development?
 require 'pg'
 require 'pry'
 
+helpers do
+  def db_exec(sql)
+    conn = PG.connect(:dbname =>'kittens_db', :host => 'localhost')
+    result = conn.exec(sql)
+    conn.close
+    result
+  end
+end
 
 get '/' do
   erb :main
 end
 
+get '/owner' do
+  erb :owner_new
+end
+
+post '/owner' do
+  #"g", "d"
+  name = params[:name]
+  sql = "INSERT INTO owner (name) VALUES ('#{name}')"
+  db_exec(sql)
+
+  redirect to('/')
+end
+
 get '/kittens/new' do
+  sql = "SELECT * FROM owner"
+  response = db_exec(sql)
+  @response_entries = response.entries
   erb :kittens_new
 end
 
 get '/kittens' do
   #get info from database
-
-  db_conn = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
   sql = "SELECT * FROM kittens"
-  response = db_conn.exec(sql)
+  response = db_exec(sql)
   @response_entries = response.entries
-  db_conn.close
   erb :kittens
 end
 
@@ -29,12 +50,10 @@ post '/kittens' do
   age = params[:age]
   is_cute = params[:is_cute]
   image_url = params[:image_url]
+  foreign_key = params[:foreign_key]
 
-
-  db_conn = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
-  sql = "INSERT INTO kittens (name, age, is_cute, image_url) VALUES ('#{name}', #{age}, #{is_cute}, '#{image_url}')"
-  db_conn.exec(sql)
-  db_conn.close
+  sql = "INSERT INTO kittens (name, age, is_cute, image_url, owner_id) VALUES ('#{name}', #{age}, #{is_cute}, '#{image_url}', #{foreign_key})"
+  db_exec(sql)
 
   redirect to('/kittens')
 end
@@ -43,24 +62,24 @@ get '/kitten/:id' do
   #get the ID of the kitten
   kitten_id = params[:id]
 
-  db_conn = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
   sql = "SELECT * FROM kittens WHERE id = #{kitten_id}"
-  response = db_conn.exec(sql)
+  response = db_exec(sql)
   @response_entry = response.entries[0]
-  db_conn.close
 
-  p @response_entry
+  owner_id = @response_entry["owner_id"] ####
+
+  sql = "SELECT * FROM owner WHERE id = #{owner_id}"
+  response_owner = db_exec(sql)
+
+  @response_owner_entries = response_owner.entries
+
   erb :kittens_id
 end
 
 post '/kitten/delete/:id' do
   id = params[:id] #will unlock the id number inside from @response_entry
-  p id
-   db_conn = PG.connect(:dbname => 'kittens_db', :host => 'localhost')
   sql = "DELETE FROM kittens WHERE id = #{id}"
-  db_conn.exec(sql)
-  db_conn.close
-
+  db_exec(sql)
   redirect to('/kittens')
 end
 
