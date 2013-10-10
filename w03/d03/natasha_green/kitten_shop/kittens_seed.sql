199a1ce1a6a711b660640bd37a152d7347d2e4ad
Tasha25 <greennatasha@aol.com>
2013-10-09 21:35:20 -0400

    Homework W3 D3

---
+++ w03/d03/natasha_green/kitten_shop/kittens_seed.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO kittens (name, age, is_cute, image_url) VALUES ('Tim', 8, false, 'http://www.bubblews.com/assets/images/news/803864926_1375572583.jpg ');
+INSERT INTO kittens (name, age, is_cute, image_url) VALUES ('Shara', 11, true, 'http://images5.fanpop.com/image/photos/31400000/cute-kitten-in-marshmallows-cute-kittens-31462498-1914-1762.png ');
