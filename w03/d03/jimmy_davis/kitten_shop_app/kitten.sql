9303876a16bba8c9dbd067c848a35e1c9483cc86
James Davis <jamesmdavis5@gmail.com>
2013-10-09 22:12:36 -0400

    DRY code, add owners table, made create owners form with get and post, made dropdown to assign owners

---
+++ w03/d03/jimmy_davis/kitten_shop_app/kitten.sql
@@ -0,0 +1,12 @@
+-- CREATE TABLE kittens (
+--   id SERIAL PRIMARY KEY,
+--   name VARCHAR,
+--   age INTEGER,
+--   is_cute BOOLEAN,
+--   image_url TEXT
+-- );
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR
+);
