a6f05b23f8bb86c78eb9ead5fa9789ead7025a4f
Cooper Mayne <coopermayne@gmail.com>
2013-10-09 22:12:15 -0400

    added owners... fixed everything so cats can have owners and owners can have cats....

--- w03/d03/cooper_mayne/kitten_factory_app/main.rb
+++ w03/d03/cooper_mayne/kitten_factory_app/main.rb
@@ -24,18 +24,37 @@ get '/kittens' do
   erb :kittens
 end
 
+get '/owners' do
+  owners = db_query("SELECT * FROM owners")
+  @owners = owners.entries
+  erb :owners
+end
+
 get '/kittens/new' do
+  res = db_query("SELECT * FROM owners")
+  @owners = res.entries
   erb :new
+end
+
+get '/owners/new' do
+  erb :new_owner
 end
 
 post '/kittens' do
   name = params["name"]
   age = params["age"]
+  owner_id = params["owner_id"]
   is_cute = false
   is_cute = true if params["is_cute"]=="true"
   image_url = params["image_url"]
-db_query("INSERT INTO kittens (name, age, is_cute, image_url) VALUES ('#{name}',#{age},#{is_cute},'#{image_url}')")
+  db_query("INSERT INTO kittens (name, age, is_cute, image_url, owner_id) VALUES ('#{name}',#{age},#{is_cute},'#{image_url}', #{owner_id})")
   redirect '/kittens'
+end
+
+post '/owners' do
+  name = params["name"]
+  db_query("INSERT INTO owners (name) VALUES ('#{name}')")
+  redirect '/owners'
 end
 
 post '/kittens/:id/delete' do
@@ -48,14 +67,25 @@ get '/kittens/:id' do
   id = params[:id]
   info = db_query("SELECT * FROM kittens WHERE id = #{id}")
   @kitten_info = info.entries[0]
-  #@kitten_info.to_s
+  @owner = db_query("SELECT name FROM owners WHERE id = #{@kitten_info['owner_id']}")[0]["name"]
   erb :display
+end
+
+get '/owners/:id' do
+  id = params[:id]
+  info = db_query("SELECT * FROM owners WHERE id = #{id}")
+  @owner_info = info.entries[0]
+  res = db_query("SELECT * FROM kittens WHERE owner_id = #{id}")
+  @pets = res.entries
+  erb :display_owner
 end
 
 get '/kittens/:id/update' do
   id = params[:id]
   kitten = db_query("SELECT * FROM kittens WHERE id=#{id}")
   @kitten_info = kitten.entries[0]
+  res = db_query("SELECT * FROM owners")
+  @owners = res.entries
   erb :update
 end
 
@@ -63,9 +93,10 @@ post '/kittens/:id' do
   id = params[:id]
   name = params["name"]
   age = params["age"]
+  owner_id = params['owner_id']
   is_cute = false
   is_cute = true if params["is_cute"]=="true"
   image_url = params["image_url"]
-  db_query("UPDATE kittens SET name = '#{name}', age = #{age}, is_cute = #{is_cute}, image_url = '#{image_url}' WHERE id=#{id}")
-  redirect '/kittens/#{id}'
+  db_query("UPDATE kittens SET owner_id = '#{owner_id}',name = '#{name}', age = #{age}, is_cute = #{is_cute}, image_url = '#{image_url}' WHERE id=#{id}")
+  redirect "/kittens/#{id}"
 end
