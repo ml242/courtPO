a027541d6628ef3caee0641497230e6aaf906273
JC <jorge.e.colindres@gmail.com>
2013-10-10 16:59:17 -0400

    posting succesfully to db

---
+++ w03/d04/jorge_colindres/instagram_app/main.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pry'
+
+require_relative 'entry'
+
+
+get '/instagram' do
+  # display all entries
+  "works"
+end
+
+get '/instagram/entries/new' do
+  # form to create new entry
+end
+
+post '/instagram/entries/new' do
+  author = params[:author]
+  date_taken = params[:date_taken]
+  photo_url = params[:photo_url]
+  # create a new entry and save to DB
+  Entry.create :author => author, :date_taken => date_taken, :photo_url => photo_url
+end
+
+get 'instagram/entries/:id' do
+  # dispaly a particular entry
+end
