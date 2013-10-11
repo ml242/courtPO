28812e32a422cb2d4ec9d01615f20b433538623d
James Davis <jamesmdavis5@gmail.com>
2013-10-11 09:03:37 -0400

    morgan just saved my life and helped me fix my problem... submitting now but will style this weekend

--- w03/d04/jimmy_davis/wdinstagram_app/main.rb
+++ w03/d04/jimmy_davis/wdinstagram_app/main.rb
@@ -2,29 +2,12 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'active_record'
 require 'pry'
+require 'thin'
+require_relative 'active_record'
+require_relative 'entry'
 
-# before do
-#   ActiveRecord::Base.logger = Logger.new( STDOUT )
-#   ActiveRecord::Base.establish_connection(
-#     :adapter => "postgresql",
-#     :host => "localhost",
-#     :username => "fijimunkii",
-#     :password => "",
-#     :database => "wdstagram_db"
-#   )
-# end
 
-# after do
-#   ActiveRecord::Base.connection.close
-# end
-
-# get '/' do
-#   @entries = Entry.all
-#   erb :index
-# end
-
-helpers do
-  def open
+before do
     ActiveRecord::Base.logger = Logger.new( STDOUT )
     ActiveRecord::Base.establish_connection(
       :adapter => "postgresql",
@@ -33,20 +16,16 @@ helpers do
       :password => "",
       :database => "wdinstagram_db"
     )
-  end
-  def close
-    ActiveRecord::Base.connection.close
-  end
 end
 
-class Entry < ActiveRecord::Base
+after do
+  ActiveRecord::Base.connection.close
 end
+
 
 get '/' do
-  open
   @entries = Entry.all
     # SHOW PHOTOS
-  close
   erb :index
 end
 
@@ -55,20 +34,21 @@ get '/new' do
 end
 
 post '/new' do
-  open
-  e1 = Entry.new
-  e1.author = params[:author]
-  e1.photo_url = params[:photo_url]
-  e1.date_taken = params[:date_taken]
-  e1.save
-  close
+  author = params[:author]
+  photo_url = params[:photo_url]
+  date_taken = params[:date_taken]
+  Entry.create( {
+    :author => author,
+    :photo_url => photo_url,
+    :date_taken => date_taken
+    })
   redirect to '/'
 end
 
 get '/show/:id' do
-  open
   id = params[:id].to_i
   @entry = Entry.find(id)
-  close
   erb :entries
 end
+
+# binding.pry
