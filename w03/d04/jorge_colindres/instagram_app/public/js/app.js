02288fc8fe435b3c7b3202397e2be59a7c922c30
JC <jorge.e.colindres@gmail.com>
2013-10-10 23:05:59 -0400

    added shuffle, cleaned up datepicker styles

--- w03/d04/jorge_colindres/instagram_app/public/js/app.js
+++ w03/d04/jorge_colindres/instagram_app/public/js/app.js
@@ -1,8 +1,13 @@
-$('#container').imagesLoaded(function(){
-  $('#container').isotope({
+var container =  $('#container');
+container.imagesLoaded(function(){
+  container.isotope({
     itemSelector : '.item',
     layoutMode : 'masonry'
   });
+});
+$('#shuffle').click(function () {
+  container.isotope('shuffle');
+  return false;
 });
 $('.datepicker').pickadate({
   formatSubmit: 'yyyy/mm/dd'
