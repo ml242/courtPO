dc03b7fead7f258e1d230f7f92bc89bc3cb7b19f
Amy Ruan <nyc.amy@gmail.com>
2013-10-11 08:25:35 -0400

    more style but still terrible cannot get a proper gallery

--- w03/d04/amy_ruan/wdinstagram_app/entries.rb
+++ w03/d04/amy_ruan/wdinstagram_app/entries.rb
@@ -1,10 +1,2 @@
 class Entry < ActiveRecord::Base
-  def self.show_all(responses)
-    response.each do |content|
-      @id = content[:id]
-      @author = content[:author]
-      @photo_url = content[:photo_url]
-      @date_taken = content[:date_taken]
-    end
-  end
 end
