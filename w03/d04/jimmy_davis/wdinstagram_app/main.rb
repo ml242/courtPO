b451ee6244713320148bc47fb588abfa6a9d722b
James Davis <jamesmdavis5@gmail.com>
2013-10-10 20:16:19 -0400

    add entries erb to show one photo

--- w03/d04/jimmy_davis/wdinstagram_app/main.rb
+++ w03/d04/jimmy_davis/wdinstagram_app/main.rb
@@ -3,6 +3,26 @@ require 'sinatra/reloader' if development?
 require 'active_record'
 require 'pry'
 
+# before do
+#   ActiveRecord::Base.logger = Logger.new( STDOUT )
+#   ActiveRecord::Base.establish_connection(
+#     :adapter => "postgresql",
+#     :host => "localhost",
+#     :username => "fijimunkii",
+#     :password => "",
+#     :database => "wdstagram_db"
+#   )
+# end
+
+# after do
+#   ActiveRecord::Base.connection.close
+# end
+
+# get '/' do
+#   @entries = Entry.all
+#   erb :index
+# end
+
 helpers do
   def open
     ActiveRecord::Base.logger = Logger.new( STDOUT )
@@ -18,7 +38,6 @@ helpers do
     ActiveRecord::Base.connection.close
   end
 end
-
 
 class Entry < ActiveRecord::Base
 end
@@ -36,12 +55,20 @@ get '/new' do
 end
 
 post '/new' do
-  before
+  open
   e1 = Entry.new
   e1.author = params[:author]
   e1.photo_url = params[:photo_url]
   e1.date_taken = params[:date_taken]
   e1.save
-  after
+  close
   redirect to '/'
+end
+
+get '/show/:id' do
+  open
+  id = params[:id].to_i
+  @entry = Entry.find(id)
+  close
+  erb :entries
 end
