950ea920f3db5a2c3c7be8a94824a2c47b3366ce
Enoch Riese <enoch.riese@gmail.com>
2013-10-11 00:22:32 -0400

    finished homework

---
+++ w03/d04/enoch_riese/main.rb
@@ -0,0 +1,44 @@
+require 'active_record'
+require "pry"
+require "sinatra"
+require "sinatra/reloader" if development?
+
+before do
+  ActiveRecord::Base.logger = Logger.new( STDOUT )
+  ActiveRecord::Base.establish_connection(
+    :adapter => "postgresql",
+    :host => "localhost",
+    :username => "eriese",
+    :password => "",
+    :database => "wdinstagram_db"
+  )
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
+
+class Photo < ActiveRecord::Base
+end
+
+get "/" do
+  @photos = Photo.all
+  @photos.sort!{|p1, p2| p2["date_taken"] <=> p1["date_taken"]}
+  erb :index
+end
+get "/new" do
+  erb :new
+end
+post "/" do
+  p1 = Photo.new
+  p params
+  p1.author = params[:author]
+  p1.date_taken = params[:date_taken]
+  p1.url = params[:url]
+  p1.save
+  redirect to "/"
+end
+get "/show" do
+  @photo = Photo.find(params[:id])
+  erb :show
+end
