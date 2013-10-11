3da6b4c63ab33fc1858f108bf2afbf473cf430c7
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-10-11 00:32:53 -0400

    styled with beautiful css

--- w03/d04/jaclyn_jimenez/WDInstagram_app/main.rb
+++ w03/d04/jaclyn_jimenez/WDInstagram_app/main.rb
@@ -3,17 +3,11 @@ require 'sinatra/reloader' if development?
 require 'pg'
 require_relative 'active_record'
 
-# Actions:
-# Index (Displays all entries)
-# New (Displays a form to create a new entry)
-# Create (Creates a new entry and saves it to the database)
-# Show (Displays one particular entry)
-# [#<Entry id: 1, name: "my awesome photo!",
-# photo: "http://placekitten.com/g/500/400",
-# date_taken: "2013-10-10">]
+after do
+  ActiveRecord::Base.connection.close
+end
 
 get '/' do #index
-  #This page displays all entries
   @post = Entry.all
   ActiveRecord::Base.connection.close
   erb :index
@@ -23,8 +17,7 @@ get '/new' do #new
   erb :new
 end
 
-post '/new' do #create
-  "adds entry collected from post to DB, redirects to '/'"
+post '/' do #create
   @name = params['name']
   @date = params['date']
   @photo = params['photo']
