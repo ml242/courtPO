ee36c676d0eb973f4c02f2192646faf42ddae1cb
Will Smith <will.smith@preplaysports.com>
2013-10-10 22:49:02 -0400

    create new picture and create new user

--- w03/d04/will_smith/WDInstagram_app/main.rb
+++ w03/d04/will_smith/WDInstagram_app/main.rb
@@ -1,26 +1,69 @@
-require 'active_record'
+require 'sinatra'
+require 'sinatra/reloader' if development?
 require 'pry'
+require 'active_record'
+require 'pg'
 
 # 1. Start by setting up your database using SQL: -->DONE
 #   * Plan your schema --> DONE
 #   * Create your database --> DONE
 #   * Test your database using psql --> DONE
 
+require_relative 'users'
+require_relative 'pics'
 
-ActiveRecord::Base.logger = Logger.new( STDOUT )
-
-ActiveRecord::Base.establish_connection(
-  :adapter => "postgresql",
-  :host => "localhost",
-  :username => "willsmith",
-  :password => "",
-  :database => "pics_db"
-)
-
-class Users < ActiveRecord::Base
+before do
+  ActiveRecord::Base.logger = Logger.new( STDOUT )
+  ActiveRecord::Base.establish_connection(
+    :adapter => "postgresql",
+    :host => "localhost",
+    :username => "willsmith",
+    :password => "",
+    :database => "pics_db"
+  )
 end
 
-class Pics < ActiveRecord::Base
+after do
+  ActiveRecord::Base.connection.close
 end
 
-binding.pry
+get '/' do
+  erb :index
+end
+
+get '/pics' do
+  @pics = Pics.all
+  @users = Users.all
+  erb :pics
+end
+
+post '/pics/upload' do
+  user = params[:user_id]
+  photo = params[:img_url]
+  date = params[:date_created]
+  Pics.create({
+    :img_url => photo,
+    :date_created => date,
+    :user_id => user
+    })
+  redirect to('/pics/upload')
+end
+
+get '/pics/upload' do
+  @users = Users.all
+  erb :upload
+end
+
+get '/users/new' do
+  erb :new_user
+end
+
+post '/users/new' do
+  name = params[:name]
+  Users.create({
+    :name => name
+    })
+  redirect to('/pics/upload')
+end
+
+
