17b171b55dcbdd351a91112f68706fdb8b8458a9
Harrison Powers <fijimunkii@gmail.com>
2013-10-12 16:36:01 -0400

    programmer class

--- w03/d05/harrison_powers/programming_app/import.rb
+++ w03/d05/harrison_powers/programming_app/import.rb
@@ -1,11 +1,27 @@
 require 'pry'
+require 'pg'
+require 'active_record'
+require_relative 'programmer'
+
+ActiveRecord::Base.logger = Logger.new( STDOUT )
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "postgresql",
+  :host => "localhost",
+  :username => "fijimunkii",
+  :password => "",
+  :database => "programming_db"
+)
 
 puts "Please direct me to the Programmers CSV file so I can process it into the database."
 location = gets.chomp
+
 file = File.open(location, 'r')
+
 csv_array = file.map do |line|
   line.split("'")
 end
+
 csv_array.map! do |line|
   line = {
     :name => line[0],
@@ -14,3 +30,4 @@ csv_array.map! do |line|
   }
 end
 
+binding.pry
