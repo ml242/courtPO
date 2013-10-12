8d178a7e33c7f4fc82baeb973c44da58369a5f08
Harrison Powers <fijimunkii@gmail.com>
2013-10-12 16:09:22 -0400

    set up skeleton

---
+++ w03/d05/harrison_powers/programming_app/main.rb
@@ -0,0 +1,64 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pry'
+require 'pg'
+require 'active_record'
+
+ActiveRecord::Base.logger = Logger.new( STDOUT )
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "postgresql",
+  :host => "localhost",
+  :username => "fijimunkii",
+  :password => "",
+  :database => "programming_db"
+)
+
+get '/' do
+  redirect to('/programmers')
+end
+
+get '/programmers' do
+  @programmers = Programmer.all
+  @title = "The #{@programmers.count} Programmers"
+  slim :index
+end
+
+post '/programmers' do
+  name = params[:name]
+  twitter_pic = params[:twitter_pic]
+  twitter_username = params[:twitter_username]
+  github_username = params[:github_username]
+  programmer = Programmer.create(
+    :name => "#{name}",
+    :twitter_pic => "#{twitter_pic}",
+    :twitter_username => "#{twitter_username}",
+    :github_username => "#{github_username}",
+  )
+  redirect to("/programmers/#{programmer.id}")
+end
+
+get '/programmers/add' do
+  @title = "Add a Programmer"
+  slim :add
+do
+
+get '/programmers/:id' do
+  id = params[:id].to_i
+  @programmer = Programmer.find(id)
+  @title = "#{@programmer.name}"
+  slim :view
+end
+
+get '/programmers/:id/edit' do
+  id = params[:id].to_i
+  @programmer = Programmer.find(id)
+  @title = "Edit #{@programmer.name}"
+  slim :edit
+end
+
+post '/programmers/:id/delete' do
+  id = params[:id].to_i
+  Programmer.find(id).destroy
+  redirect to('/programmers')
+end
---
+++ w03/d05/harrison_powers/programming_app/programming_schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE programmers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR,
+  twitter_pic VARCHAR,
+  twitter_username VARCHAR,
+  github_username VARCHAR
+);
