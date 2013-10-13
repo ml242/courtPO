91cfc3c7fe96e78364ac6b9d34b848c597e86735
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-10-13 16:01:56 -0400

    routes are functioning. lists all programmers, and individual programmer info

--- w03/d05/jaclyn_jimenez/programming_app/main.rb
+++ w03/d05/jaclyn_jimenez/programming_app/main.rb
@@ -1,6 +1,33 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
-require 'pg'
+require 'active_record'
+require 'pry'
+require_relative 'programmer'
+
+ActiveRecord::Base.logger = Logger.new( STDOUT )
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "postgresql",
+  :host => "localhost",
+  :username => "Jaclyn",
+  :password => "",
+  :database => "programming_db"
+)
 
 get '/' do
+  "Welcome!"
+end
+
+get '/programmers' do
+  @programmers_array = Programmer.all
+  erb :programmers
+end
+
+get '/programmers/:id' do
+  id = params[:id]
+  programmer = Programmer.find(id)
+  @name = programmer['name']
+  @image = programmer['twitter_pic']
+  @username = programmer['twitter_username']
+  erb :show_programmer
 end
