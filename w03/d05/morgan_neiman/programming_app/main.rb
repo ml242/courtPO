0f4a3afd0525039625fbf1e12cd2098b83b91fe9
Morgan Neiman <quackhouse@me.com>
2013-10-14 19:02:40 -0400

    made link to github repos but only one?

--- w03/d05/morgan_neiman/programming_app/main.rb
+++ w03/d05/morgan_neiman/programming_app/main.rb
@@ -2,6 +2,8 @@ require 'active_record'
 require 'pry'
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'github_api'
+require 'JSON'
 require_relative 'active_record'
 
 class Person < ActiveRecord::Base
@@ -40,5 +42,42 @@ get '/programmers/:id' do
   @name = person.name
   @twitter_pic = person.twitter_pic
   @twitter_username = person.twitter_username
+  @github = person.github_username
+  repos = Github.repos.list user: @github
+  repos = repos.body
+  @repos_array = []
+  repos.each do |repo|
+    name = repo['name']
+    url = repo['html_url']
+    @repos_array << {:name => name, :url => url}
+  end
   erb :programmer
 end
+
+get '/programmers/:id/edit' do
+  id = params[:id].to_i
+  @person = Person.find(id)
+  erb :programmer_edit
+end
+
+post '/programmers/edit' do
+  id = params[:id].to_i
+  name = params[:name]
+  twitter_pic = params[:twitter_pic]
+  twitter_username = params[:twitter_username]
+  github_username = params[:github_username]
+  person = Person.find(id)
+  person.name = name
+  person.twitter_pic = twitter_pic
+  person.twitter_username = twitter_username
+  person.github_username = github_username
+  person.save
+  url = '/programmers/' + id.to_s
+  redirect to url
+end
+
+
+
+
+
+
