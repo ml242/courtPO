a0dd17bf828519f2c9cfd9d3ae5cc9ce5bd01895
JC <jorge.e.colindres@gmail.com>
2013-10-14 14:28:23 -0400

    both routes working well

--- w03/d05/jorge_colindres/programming_app/main.rb
+++ w03/d05/jorge_colindres/programming_app/main.rb
@@ -1,12 +1,41 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
-require 'pg'
 require 'pry'
+require 'active_record'
+require 'slim'
+
+require_relative 'programmer'
+
+helpers do
+  def before
+    ActiveRecord::Base.logger = Logger.new( STDOUT )
+    ActiveRecord::Base.establish_connection(
+      :adapter => "postgresql",
+      :host => "localhost",
+      :username => "JC",
+      :password => "",
+      :database => "programming_db"
+    )
+  end
+  def after
+    ActiveRecord::Base.connection.close
+  end
+end
 
 get '/programmers' do
+  before
+  @all_programmers = Programmer.all
+  after
 
+  slim :all_programmers
 end
 
 get '/programmers/:id' do
+  id = params[:id]
+  programmer = Programmer.find id
+  @name = programmer.name
+  @twitter_username = programmer.twitter_username
+  @twitter_pic = programmer.twitter_pic
 
+  slim :single_programmer
 end
