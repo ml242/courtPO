bf51b3a013490cb63ab6cb7ee7090acbd60e3731
Quin Cogdell <quincogdell@gmail.com>
2013-10-14 17:54:05 -0400

    programming app now has edit and delete

--- w03/d05/quin_cogdell/programming_app/import.rb
+++ w03/d05/quin_cogdell/programming_app/import.rb
@@ -4,9 +4,6 @@ require_relative 'programmer'
 
 file = "programmers.csv"
 
-# file_csv = CSV.read(file)
-# binding.pry
-
 CSV.foreach(file) do |row|
   name = row[0]
   twitter_name = row[1]
--- w03/d05/quin_cogdell/programming_app/main.rb
+++ w03/d05/quin_cogdell/programming_app/main.rb
@@ -18,7 +18,6 @@ helpers do
     )
   end
 
-
   after do
     ActiveRecord::Base.connection.close
   end
@@ -32,18 +31,9 @@ get '/' do
 end
 
 get '/programmers' do
-  @results = Programmer.all
+  @results = Programmer.all.order('id ASC')
   erb :programmers
 end
-
-post '/programmers' do
-  id = params[:id]
-  name = params[:name]
-  twitter_pic = params[:twitter_pic]
-  twitter_username = params[:twitter_username]
-  redirect to '/'
-end
-
 
 
 
@@ -55,4 +45,24 @@ end
 get '/programmers/:id/edit' do
   @results = Programmer.find(params[:id])
   erb :programmers_edit
+end
+
+
+post '/programmers/:id' do
+  id = params[:id]
+  name = params[:name]
+  twitter_pic = params[:twitter_pic]
+  twitter_username = params[:twitter_username]
+  github_username = params[:github_username]
+  programmer = Programmer.find(id)
+  programmer.update(name: name, twitter_pic: twitter_pic, twitter_username: twitter_username, github_username: github_username )
+  programmer.save
+  redirect to '/programmers'
+end
+
+post '/programmers/:id/delete' do
+  id = params[:id]
+  programmer = Programmer.find(id)
+  programmer.destroy
+  redirect to '/programmers'
 end
