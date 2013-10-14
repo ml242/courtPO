6f3ec9fd7409fb619502728a16d2fd0162ee2c81
Travis Vander Hoop <vanderhoop@me.com>
2013-10-14 12:11:06 -0400

    now displays profiles of individual programmers.

--- w03/d05/travis_vanderhoop/programming_app/main.rb
+++ w03/d05/travis_vanderhoop/programming_app/main.rb
@@ -16,13 +16,32 @@ ActiveRecord::Base.establish_connection(
   :database => 'programming_db'      #ENTER THE DATABASE YOU WANT TO WORK WITH OR DIE UNHAPPY
 )
 
+# programmers = Programmer.all
+
+programmers = Programmer.all.to_a #grabs all programmer profiles from the programmers table in the programming_db, and converts them to a data-retrievable array.
+
 get '/' do
 
 erb :hello
 end
 
 get '/programmers' do
-  @programmers = Programmer.all
-  @programmers = @programmers.to_a
+  # @programmers = programmers.to_a
+  @programmers = programmers
 erb :programmers
 end
+
+get '/programmers/:id' do
+  id = params[:id]              #NOTE: id may need to be converted to an integer using .to_i
+  programmer = Programmer.all.find(id)
+  @name = programmer[:name]
+  @twitter_username= programmer[:twitter_username]
+  @twitter_pic = programmer[:twitter_pic]
+  erb :programmer
+end
+
+
+
+
+
+
