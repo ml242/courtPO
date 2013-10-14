ca07963920fef65b8738654fa3f868a21bf9cca4
Harrison Powers <fijimunkii@gmail.com>
2013-10-13 20:10:36 -0400

    before and after do

--- w03/d05/harrison_powers/programming_app/main.rb
+++ w03/d05/harrison_powers/programming_app/main.rb
@@ -14,30 +14,36 @@ Twitter.configure do |config|
   config.oauth_token_secret = '8GUUXS4yhARX1KLWoUH8H2Bq3YIAKXlttXcLS2zM'
 end
 
-configure :development do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///programming_db')
+before do
+  configure :development do
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///programming_db')
 
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => 'localhost',
-    :username => 'fijimunkii',
-    :password => '',
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-  )
+    ActiveRecord::Base.establish_connection(
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => 'localhost',
+      :username => 'fijimunkii',
+      :password => '',
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+    )
+  end
+
+  configure :production do
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///programming_db')
+
+    ActiveRecord::Base.establish_connection(
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+    )
+  end
 end
 
-configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///programming_db')
-
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-  )
+after do
+  ActiveRecord::Base.connection.close
 end
 
 get '/' do
