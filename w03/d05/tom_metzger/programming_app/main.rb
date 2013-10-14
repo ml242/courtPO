1d646c9ac9acb59af852c141571021f8b14f8225
Tom Metzger <metzgert@gmail.com>
2013-10-14 14:18:05 -0400

    created erb views for displaying output of database calls

--- w03/d05/tom_metzger/programming_app/main.rb
+++ w03/d05/tom_metzger/programming_app/main.rb
@@ -3,6 +3,26 @@ require 'sinatra/reloader' if development?
 require 'pg'
 require 'pry'
 
+require_relative 'programmer'
+
+helpers do
+  def db_connect
+    ActiveRecord::Base.logger = Logger.new( STDOUT )
+    ActiveRecord::Base.establish_connection(
+      :adapter => "postgresql",
+      :host => "localhost",
+      :username => "thomasmetzger",
+      :password => "",
+      :database => "programming_db")
+  end
+
+  def db_disconnect
+    ActiveRecord::Base.connection.close
+  end
+end
+
+
+
 get '/' do
 end
 
@@ -12,6 +32,11 @@ get '/programmers' do
   # connect to database
   # pull all names & ids from database
   # send names (to be displayed as text) and ids (to be used in links) to .erb file
+  db_connect
+  @result = Programmer.all.order("name ASC")
+  db_disconnect
+  binding.pry
+  erb :programmers
 end
 
 
@@ -20,6 +45,10 @@ get '/programmers/:id' do
   # connect to database
   # pull all data associated with selected id
   # send name, username & image to .erb file to be displayes
+  photo_id = params["id"]
+  db_connect
+  @result = Programmer.find(photo_id)
+  db_disconnect
 end
 
 
