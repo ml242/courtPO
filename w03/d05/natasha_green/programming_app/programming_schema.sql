872e68278f3c848b7f5bc0c5fd7e5218179713f7
Tasha25 <greennatasha@aol.com>
2013-10-14 11:47:34 -0400

    Did schema, working on import

--- w03/d05/natasha_green/programming_app/programming_schema.sql
+++ w03/d05/natasha_green/programming_app/programming_schema.sql
@@ -1,6 +1,9 @@
 CREATE TABLE programmers (
   id SERIAL PRIMARY KEY,
   name VARCHAR,
-  twitter_pic VARCHAR,
-  twitter_username VARCHAR
+  twitter_username VARCHAR,
+  twitter_pic VARCHAR
+
 );
+
+
--- w03/d05/natasha_green/programming_import.rb
+++ w03/d05/natasha_green/programming_import.rb
@@ -15,6 +15,7 @@ ActiveRecord::Base.establish_connection(
 class Programmer < ActiveRecord::Base
 
 
+=begin
 column_names = Programmer.columns.map{ |column| column.name }
 value_sets = some_method_to_load_data_from_csv_file( 'programmers.csv' )
 options = { :validate => true }
@@ -22,7 +23,6 @@ options = { :validate => true }
 Programmer.import( column_names, value_sets, options)
 
 
-=begin
 File.open('/tmp/items.csv', 'w') do |f|
      f.write(...) }
   end
@@ -30,4 +30,13 @@ File.open('/tmp/items.csv', 'w') do |f|
     "COPY items FROM '/tmp/items.csv' csv;")
 =end
 
+csvfile = File.read('/programmers.csv')
+csv = CSV.parse(csvfile, :headers => false)
+csv.each do |row|
+  p = Programmer.new
+     p.name = row[1]
+     p.twitter_username = row[2]
+    p.twitter_pic = row[3]
+   p.save
+end
 end
