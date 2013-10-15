0bbe5876604ca5bd5c2efe8cd6919ee4d32dfc65
Harrison Powers <fijimunkii@gmail.com>
2013-10-15 19:28:16 -0400

    edit controller and view

--- w04/d01/harrison_powers/programming_app/app/controllers/programmer_controller.rb
+++ w04/d01/harrison_powers/programming_app/app/controllers/programmer_controller.rb
@@ -18,4 +18,47 @@ class ProgrammerController < ApplicationController
     end
   end
 
+  def view
+
+  end
+
+  def edit
+    id = params[:id]
+    if request.post?
+      new_info = Hash.new
+      new_info[name] = params[:name] if params[:name]
+      new_info[github_username] = params[:github_username] if params[:github_username]
+      new_info[twitter_username] = params[:twitter_username] if params[:twitter_username]
+      new_info[img_url] = params[:img_url] if params[:img_url]
+      p1 = Programmer.find(id)
+      new_info.each do |k, v|
+        p1.k = v
+      end
+      p1.save
+      redirect_to("/programmers/#{id}")
+    elsif request.get?
+      @programmer = Programmer.find(id)
+    end
+  end
+
+  def delete
+    if request.post?
+      id = params[:id]
+      Programmer.find(id).destroy
+    end
+    redirect_to('/programmers')
+  end
+
+
+
 end
+
+
+
+
+  get '/programmers/:id' => 'programmer#view'
+
+  get '/programmers/:id/edit' => 'programmer#edit'
+  post '/programmers/:id' => 'programmer#edit'
+
+  post '/programmers/:id/delete' => 'programmer#delete'
