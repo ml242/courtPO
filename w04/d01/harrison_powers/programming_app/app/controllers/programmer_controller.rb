1bdafd50169c51777bb481a63b2048a955cdf303
Harrison Powers <fijimunkii@gmail.com>
2013-10-15 19:47:41 -0400

    twitter time

--- w04/d01/harrison_powers/programming_app/app/controllers/programmer_controller.rb
+++ w04/d01/harrison_powers/programming_app/app/controllers/programmer_controller.rb
@@ -1,3 +1,10 @@
+Twitter.configure do |config|
+  config.consumer_key = twitter_consumer_key
+  config.consumer_secret = twitter_consumer_secret
+  config.oauth_token = twitter_oauth_token
+  config.oauth_token_secret = twitter_oauth_token_secret
+end
+
 class ProgrammerController < ApplicationController
 
   def index
