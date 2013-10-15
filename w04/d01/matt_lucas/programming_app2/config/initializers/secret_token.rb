bb8eeb81ae2eaee766cddbc030d0d17f28976bb5
Matt Lucas <mattlucas@gmail.com>
2013-10-15 18:27:05 -0400

    deploying rails

---
+++ w04/d01/matt_lucas/programming_app2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProgrammingApp2::Application.config.secret_token = '294a17bfbc1e15670905e821609c78f959f72d0fcdeb965e9a4a0c8bcf882aedb90436c6dd205e4e04d7a86b9b83d32a1e605b09b702786a8693e9ec408656f7'
