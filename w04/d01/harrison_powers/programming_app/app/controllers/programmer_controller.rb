26684dc6152cd925e500c2cd6a7c36c51918cc12
Harrison Powers <fijimunkii@gmail.com>
2013-10-15 20:11:27 -0400

    twitter.yml

--- w04/d01/harrison_powers/programming_app/app/controllers/programmer_controller.rb
+++ w04/d01/harrison_powers/programming_app/app/controllers/programmer_controller.rb
@@ -1,11 +1,11 @@
-Twitter.configure do |config|
-  config.consumer_key = twitter_consumer_key
-  config.consumer_secret = twitter_consumer_secret
-  config.oauth_token = twitter_oauth_token
-  config.oauth_token_secret = twitter_oauth_token_secret
-end
-
 class ProgrammerController < ApplicationController
+
+  Twitter.configure do |config|
+    config.consumer_key = ENV["twitter_consumer_key"]
+    config.consumer_secret = ENV["twitter_consumer_secret"]
+    config.oauth_token = ENV["twitter_oauth_token"]
+    config.oauth_token_secret = ENV["twitter_oauth_token_secret"]
+  end
 
   def index
     if request.post?
