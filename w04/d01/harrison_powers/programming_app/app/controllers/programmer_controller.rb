3fe126f562de03c2a77b3c6b7a5817b4e833cf62
Harrison Powers <fijimunkii@gmail.com>
2013-10-15 20:13:52 -0400

    plz work

--- w04/d01/harrison_powers/programming_app/app/controllers/programmer_controller.rb
+++ w04/d01/harrison_powers/programming_app/app/controllers/programmer_controller.rb
@@ -1,12 +1,5 @@
 class ProgrammerController < ApplicationController
 
-  Twitter.configure do |config|
-    config.consumer_key = ENV["twitter_consumer_key"]
-    config.consumer_secret = ENV["twitter_consumer_secret"]
-    config.oauth_token = ENV["twitter_oauth_token"]
-    config.oauth_token_secret = ENV["twitter_oauth_token_secret"]
-  end
-
   def index
     if request.post?
       new_info = Hash.new
