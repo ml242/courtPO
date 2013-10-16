1f7cb55655686d8395500cc10dd8f01a40b8732c
Matt Lucas <mattlucas@gmail.com>
2013-10-16 15:48:15 -0400

    m

--- w04/d01/matt_lucas/programming_app_two/app/models/company.rb
+++ w04/d01/matt_lucas/programming_app_two/app/models/company.rb
@@ -1,3 +1,4 @@
 class Company < ActiveRecord::Base
   attr_accessible :name
+  has_many(:programmers)
 end
--- w04/d01/matt_lucas/programming_app_two/app/models/programmer.rb
+++ w04/d01/matt_lucas/programming_app_two/app/models/programmer.rb
@@ -1,3 +1,4 @@
 class Programmer < ActiveRecord::Base
   attr_accessible :name, :twitter, :img_url, :company_id
+  belongs_to :company
 end
