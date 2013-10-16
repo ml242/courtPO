4bb040c455a0b0e3c24ff428919972024a0f41bd
Harrison Powers <fijimunkii@gmail.com>
2013-10-16 15:13:40 -0400

    auto updated schema

--- w04/d01/harrison_powers/programming_app/db/schema.rb
+++ w04/d01/harrison_powers/programming_app/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131016190327) do
+ActiveRecord::Schema.define(:version => 20131016191047) do
+
+  create_table "companies", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "programmers", :force => true do |t|
     t.string   "name"
