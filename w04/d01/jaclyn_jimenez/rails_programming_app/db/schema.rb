d80958b47a4be5858544c248b38c60c2390370c3
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-10-16 08:03:34 -0400

    added timestamp column to programmers table

--- w04/d01/jaclyn_jimenez/rails_programming_app/db/schema.rb
+++ w04/d01/jaclyn_jimenez/rails_programming_app/db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131016114832) do
+ActiveRecord::Schema.define(:version => 20131016115746) do
 
   create_table "programmers", :force => true do |t|
-    t.string "name"
-    t.string "twitter_pic"
-    t.string "twitter_username"
+    t.string   "name"
+    t.string   "twitter_pic"
+    t.string   "twitter_username"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
