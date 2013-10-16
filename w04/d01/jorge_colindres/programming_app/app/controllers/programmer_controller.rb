d793ccf2de04150fb252f13eeb6fec55af384fcd
JC <jorge.e.colindres@gmail.com>
2013-10-15 23:49:03 -0400

    added github api

--- w04/d01/jorge_colindres/programming_app/app/controllers/programmer_controller.rb
+++ w04/d01/jorge_colindres/programming_app/app/controllers/programmer_controller.rb
@@ -1,10 +1,32 @@
 class ProgrammerController < ApplicationController
 
+  # GET from '/programmers'
   def index
     @all_programmers = Programmer.all
   end
 
+  # GET form '/programmers/new'
+  def new
+  end
+
+  # GET from '/programmers/:id'
   def individual
+    @id = params[:id]
+
+    programmer = Programmer.find @id
+
+    @name = programmer.name
+    @twitter_username = programmer.twitter_username
+    @twitter_pic = programmer.twitter_pic
+    @github_username = programmer.github_username
+
+    github_response = Github.repos.list user: "#{@github_username}"
+    @repos = github_response.body
+
+  end
+
+  # GET from '/programmers/:id/edit'
+  def edit
     @id = params[:id]
 
     programmer = Programmer.find @id
@@ -15,9 +37,7 @@ class ProgrammerController < ApplicationController
     @github_username = programmer.github_username
   end
 
-  def new
-  end
-
+  # POST from '/programmers'
   def create
     id = params[:id]
     name = params[:name]
@@ -31,17 +51,7 @@ class ProgrammerController < ApplicationController
     redirect_to "/programmers/#{id}"
   end
 
-  def edit
-    @id = params[:id]
-
-    programmer = Programmer.find @id
-
-    @name = programmer.name
-    @twitter_username = programmer.twitter_username
-    @twitter_pic = programmer.twitter_pic
-    @github_username = programmer.github_username
-  end
-
+  # POST from '/programmers/:id'
   def update
     id = params[:id]
     name = params[:name]
@@ -60,6 +70,7 @@ class ProgrammerController < ApplicationController
     redirect_to "/programmers/#{id}"
   end
 
+  # POST from '/programmers/:id/delete'
   def destroy
     id = params[:id]
 
