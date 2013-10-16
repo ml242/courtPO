6f338b2bfdb1b5e1f5164652b4aa6b547df7ac31
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-10-16 07:56:07 -0400

    migrated programmers table

--- w04/d01/jaclyn_jimenez/rails_programming_app/db/migrate/20131016114832_create_programmers_table.rb
+++ w04/d01/jaclyn_jimenez/rails_programming_app/db/migrate/20131016114832_create_programmers_table.rb
@@ -1,7 +1,15 @@
 class CreateProgrammersTable < ActiveRecord::Migration
+
   def up
+    create_table :programmers do |t|
+      t.string :name
+      t.string :twitter_pic
+      t.string :twitter_username
+    end
   end
 
   def down
+    drop table :programmers
   end
+
 end
