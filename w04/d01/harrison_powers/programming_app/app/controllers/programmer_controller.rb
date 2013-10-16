792d2c94608895e81c7cc379564287ca74142c2e
Harrison Powers <fijimunkii@gmail.com>
2013-10-15 21:09:13 -0400

    editing works!!

--- w04/d01/harrison_powers/programming_app/app/controllers/programmer_controller.rb
+++ w04/d01/harrison_powers/programming_app/app/controllers/programmer_controller.rb
@@ -32,15 +32,11 @@ class ProgrammerController < ApplicationController
   def edit
     id = params[:id].to_i
     if request.post?
-      new_info = Hash.new
-      new_info[name] = params[:name] if params[:name]
-      new_info[github_username] = params[:github_username] if params[:github_username]
-      new_info[twitter_username] = params[:twitter_username] if params[:twitter_username]
-      new_info[img_url] = params[:img_url] if params[:img_url]
       p1 = Programmer.find(id)
-      new_info.each do |k, v|
-        p1.k = v
-      end
+      p1.name = params[:name]
+      p1.github_username = params[:github_username]
+      p1.twitter_username = params[:twitter_username]
+      p1.img_url = params[:img_url]
       p1.save
       redirect_to("/programmers/#{id}")
     elsif request.get?
