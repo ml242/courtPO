77430cf94406070eed7abdf4fd6433db6d4e4d80
James Davis <jamesmdavis5@gmail.com>
2013-10-15 21:56:16 -0400

    can view all programmers, and view individual programmers on separate page by id

--- w04/d01/jimmy_davis/programming_app/app/controllers/programmers_controller.rb
+++ w04/d01/jimmy_davis/programming_app/app/controllers/programmers_controller.rb
@@ -1,7 +1,18 @@
 class ProgrammersController < ApplicationController
 
   def index
+  end
+
+  def all
     @programmers = Programmer.all
+  end
+
+  def single
+    id = params[:id]
+    programmer = Programmer.find(id)
+    @name = programmer.name
+    @username = programmer.twitter_username
+    @pic = programmer.twitter_pic
   end
 
 end
