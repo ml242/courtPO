d573bd9e17fc99ded0107bf5c447f2a0f7003414
Harrison Powers <fijimunkii@gmail.com>
2013-10-16 17:27:15 -0400

    set up pokemon seed

--- w04/d02/harrison_powers/pokedex_app/db/seeds.rb
+++ w04/d02/harrison_powers/pokedex_app/db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#
+
+file = File.open("#{Rails.root}/db/pokemon.json", 'r')
+
+json_data = file.read
+
+pokemon_json = JSON.parse(json_data)
+
+pokemon_json.each do |k, v|
+  p1 = Pokemon.create(
+    :name => v["name"],
+    :type1 => v["type1"],
+    :type2 => v["type2"],
+    :ability1 => v["ability1"],
+    :gender_ratio => v["genderRatio"],
+    :experience_curve => v["experienceCurve"],
+    :catch_rate => v["catchRate"],
+    :base_exp => v["baseExp"],
+    :base_stats_hp => v["baseStats"]["hp"],
+    :base_stats_atk => v["baseStats"]["atk"],
+    :base_stats_def => v["baseStats"]["def"],
+    :base_stats_spatk => v["baseStats"]["spAtk"],
+    :base_stats_spdef => v["baseStats"]["spDef"],
+    :base_stats_speed => v["baseStats"]["speed"],
+    :evolve_to => v["evolveTo"],
+    :evolve_level => v["evolveLevel"],
+    :learnabletm => v["learnableTM"],
+  )
+  puts "Added ID: #{p1.id} - #{p1.name}"
+end
