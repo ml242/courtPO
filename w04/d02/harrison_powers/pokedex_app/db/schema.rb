b0ef88b9021389faef08fc01c05adaaabd1174ca
Harrison Powers <fijimunkii@gmail.com>
2013-10-16 23:07:30 -0400

    new schema for migration gyms table change up

--- w04/d02/harrison_powers/pokedex_app/db/schema.rb
+++ w04/d02/harrison_powers/pokedex_app/db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131016202127) do
+ActiveRecord::Schema.define(:version => 20131017020532) do
 
   create_table "gyms", :force => true do |t|
     t.string   "name"
-    t.string   "location"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "game_version"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   create_table "pokemons", :force => true do |t|
