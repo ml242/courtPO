9c3bd1cedb4e5e07b7ec301bf81d71b545e19018
Cooper Mayne <coopermayne@gmail.com>
2013-10-17 18:01:27 -0400

    added routes and models and migrations....

---
+++ w04/d03/cooper_mayne/Blog/app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
