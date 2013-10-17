debc6297b0de35e89080eb50c2a5110ec6793347
Harrison Powers <fijimunkii@gmail.com>
2013-10-16 23:08:07 -0400

    updated gym model for new schema via annotate

--- w04/d02/harrison_powers/pokedex_app/app/models/gym.rb
+++ w04/d02/harrison_powers/pokedex_app/app/models/gym.rb
@@ -2,11 +2,11 @@
 #
 # Table name: gyms
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  location   :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :integer          not null, primary key
+#  name         :string(255)
+#  game_version :string(255)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
 #
 
 class Gym < ActiveRecord::Base
