2050ae28d6b25c265dff44fde480af953391b3a3
Morgan Neiman <quackhouse@me.com>
2013-10-17 18:21:31 -0400

    done

--- w04/d03/morgan_neiman/Blarghticles/app/controllers/comments_controller.rb
+++ w04/d03/morgan_neiman/Blarghticles/app/controllers/comments_controller.rb
@@ -3,6 +3,10 @@ class CommentsController < ApplicationController
     article_id = params[:article_id].to_i
     @comments = Comment.where(article_id: article_id)
   end
+  def show
+    id = params[:id].to_i
+    @comment = Comment.find(id)
+  end
   def new
     @comment = Comment.new
     @article_id = params[:article_id].to_i
@@ -14,12 +18,23 @@ class CommentsController < ApplicationController
     redirect_to("/articles/#{article_id}")
   end
   def edit
-
+    id = params[:id].to_i
+    @comment = Comment.find(id)
+    @article_id = @comment.article_id
   end
   def update
-
+    comment = params[:comment]
+    id = params[:comment][:id].to_i
+    article_id = params[:comment][:article_id].to_s
+    to_edit = Comment.find(id)
+    to_edit.update_attributes(comment)
+    redirect_to("/articles/#{article_id}")
   end
   def destroy
-
+    id = params[:id].to_i
+    comment = Comment.find(id)
+    article_id = comment.article_id
+    comment.destroy
+    redirect_to("/articles/#{article_id}")
   end
 end
