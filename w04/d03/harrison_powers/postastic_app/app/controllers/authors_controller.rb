a1e87d48bd6c758cbe0c4d7da43591bf176ec7a7
Harrison Powers <fijimunkii@gmail.com>
2013-10-17 22:15:29 -0400

    authors controller

--- w04/d03/harrison_powers/postastic_app/app/controllers/authors_controller.rb
+++ w04/d03/harrison_powers/postastic_app/app/controllers/authors_controller.rb
@@ -3,20 +3,29 @@ class AuthorsController < ApplicationController
   end
 
   def create
+    a1 = Author.create params[:author]
+    redirect_to "/authors/#{a1.id}"
   end
 
   def update
+    a1 = Author.find(params[:id]).update params[:author]
+    redirect_to "/authors/#{params[:id]}"
   end
 
   def edit
+    @author = Author.find params[:id]
   end
 
   def destroy
+    Author.find(params[:id]).destroy
+    redirect_to "/authors"
   end
 
   def index
+    @authors = Author.all
   end
 
   def show
+    @author = Author.find params[:id]
   end
 end
