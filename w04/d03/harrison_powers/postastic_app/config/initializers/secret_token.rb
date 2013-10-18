477638f70c143d1d7ec30ac3cd4db216479ab769
Harrison Powers <fijimunkii@gmail.com>
2013-10-17 21:17:24 -0400

    create new rails app postastic

---
+++ w04/d03/harrison_powers/postastic_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostasticApp::Application.config.secret_token = '57e0b0e3907499dc16a5a842409f60a8c582974a460202354c093a434692acafa699ea6bed3b447a38a163c80f0f6cb1b09c95e25b6c96c84409400e9219f5ac'
