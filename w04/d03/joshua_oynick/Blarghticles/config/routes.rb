610f1df0851fc58f5652b1990b59ad1f57749edb
Josh Oynick <oynickj@gmail.com>
2013-10-18 00:25:39 -0400

    complete structure with comments and links

--- w04/d03/joshua_oynick/Blarghticles/config/routes.rb
+++ w04/d03/joshua_oynick/Blarghticles/config/routes.rb
@@ -2,6 +2,13 @@ Blarghticles::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  root to: 'articles#index'
+
+  resources :articles
+  post '/comments' => 'comments#create'
+
+end
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -55,4 +62,3 @@ Blarghticles::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
--- w04/d03/joshua_oynick/Blarghticles/db/seeds.rb
+++ w04/d03/joshua_oynick/Blarghticles/db/seeds.rb
@@ -9,7 +9,10 @@
 Article.delete_all
 Comment.delete_all
 
-a1 = Article.create(title: 'Study Finds Oreos to be Best Cookies Ever', body: 'If you have ever found yourself unable to resist just one more Oreo, you’re not alone. That “stuf” is like crack, neurologically speaking. A new study from Connecticut College shows that Oreos are as addictive as cocaine, at least for lab rats. According to the new study,  eating the iconic black and white cookies activated more neurons in the rat brain’s “pleasure center” than drugs such as cocaine. “I haven’t touched an Oreo since doing this experiment,” neuroscience assistant professor Joseph Schroeder said in a school press release. The research looked at the rats’ behaviors and the effects the cookies had on their brains. Rats were put into a maze and given the choice of hanging out near rice cakes or Oreos. The tasty sandwich cookies won that popularity contest handily. Those results were compared to a different test, where rats were given the choice of loitering in an area of a maze where they were injected with saline or in another corner where they could get a shot of cocaine or morphine. The rats in the study liked the cookies about as much as they liked the drugs, congregating near the cookie side of the maze as much as they would on the drug side.')
+a1 = Article.create :title => 'Article', :body => 'Nulla vitae elit libero, a pharetra augue.'
+a2 = Article.create :title => 'Another Article', :body => 'Maecenas sed diam eget risus varius blandit sit amet non magna.'
 
-
+Comment.create :body => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', :article_id => a1.id
+Comment.create :body => 'Sed posuere consectetur est at lobortis.', :article_id => a1.id
+Comment.create :body => 'Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.', :article_id => a2.id
 
