aa45b15b5c4097e603a69c5513898aec77b79ca6
Morgan Neiman <quackhouse@me.com>
2013-10-28 17:31:16 -0400

    wrote rectangle as all one object but it works

---
+++ w06/d01/morgan_neiman/triangle.js
@@ -0,0 +1,33 @@
+var rectangle = {
+  length: 4,
+  width: 4,
+
+isSquare: function() {
+  if (this.length == this.width) {
+    return true;
+  }
+  else {
+    return false;
+  }
+},
+
+area: function() {
+  return this.length * this.width;
+},
+
+perimeter: function() {
+  return (this.length * 2) + (this.width * 2);
+}
+
+
+};
+console.log(rectangle.isSquare());
+console.log(rectangle.area());
+console.log(rectangle.perimeter());
+
+var triangle = {
+  sideA: 3,
+  sideB: 4,
+  sideC: 4,
+};
+
