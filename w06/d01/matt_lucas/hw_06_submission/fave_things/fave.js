5120178e187604f3192adbbb49e0f51eddb99abb
Matt Lucas <mattlucas@gmail.com>
2013-10-28 18:38:48 -0400

    iterations of hw

--- w06/d01/matt_lucas/hw_06_submission/fave_things/fave.js
+++ w06/d01/matt_lucas/hw_06_submission/fave_things/fave.js
@@ -1,11 +1,15 @@
-var faveThings = new Array();
-faveThings[0] = "Beetlejuice";
-faveThings[1] = "Ghostbusters";
-faveThings[2] = "Nightmare before xmas";
-faveThings[3] = "Scream";
+// var faveThings = new Array();
+// faveThings[0] = "Beetlejuice";
+// faveThings[1] = "Ghostbusters";
+// faveThings[2] = "Nightmare before xmas";
+// faveThings[3] = "Scream";
+
+var faveThings = ["beer", "wine", "whisky", "tequila"]
 
 for (var i=0, total=faveThings.length; i < total; i++){
-  console.log(faveThings[i]);
+  console.log("I quite enjoy "+(faveThings[i]));
 }
 
 console.log(faveThings)
+
+// add feature: prompt the user for favorite things and then print all of the items.
