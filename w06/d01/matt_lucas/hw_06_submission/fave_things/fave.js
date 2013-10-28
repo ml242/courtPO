8cced4415b900550f7c770a7ced966b21d8055ea
Matt Lucas <mattlucas@gmail.com>
2013-10-28 18:31:18 -0400

    made an iteration

---
+++ w06/d01/matt_lucas/hw_06_submission/fave_things/fave.js
@@ -0,0 +1,11 @@
+var faveThings = new Array();
+faveThings[0] = "Beetlejuice";
+faveThings[1] = "Ghostbusters";
+faveThings[2] = "Nightmare before xmas";
+faveThings[3] = "Scream";
+
+for (var i=0, total=faveThings.length; i < total; i++){
+  console.log(faveThings[i]);
+}
+
+console.log(faveThings)
