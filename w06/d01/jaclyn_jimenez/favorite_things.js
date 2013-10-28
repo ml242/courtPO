c76872ef030912963dfb6d2e6d2ede7d6bcbcda7
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-10-28 19:19:10 -0400

    completed maths hw

---
+++ w06/d01/jaclyn_jimenez/favorite_things.js
@@ -0,0 +1,7 @@
+var favorite_things = ["netflix", "pc games", "happy dogs", "WDI kickhash"];
+
+for (x in favorite_things) {
+  console.log("one of my favorite things is..." + favorite_things[x]);
+};
+
+
