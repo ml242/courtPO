90956a6411f8e9e5c6cfa8772f6162687547d57f
Tom Metzger <metzgert@gmail.com>
2013-10-28 17:28:32 -0400

    created files for each exercise

---
+++ w06/d01/tom_metzger/calculator.js
@@ -0,0 +1,10 @@
+// create a calculator object
+
+// create a sub-object with the mathematical operations in it
+
+// create two operand prompts
+// create one operator prompt
+
+// create a function that calls the two operand prompts and the operator prompt and computes the result
+
+
