582aa411148507fe2848526970b88f5fa920c728
Travis Vander Hoop <vanderhoop@me.com>
2013-10-28 23:17:58 -0400

    calculator is complete

--- w06/d01/travis_vanderhoop/geometry.js
+++ w06/d01/travis_vanderhoop/geometry.js
@@ -4,10 +4,17 @@ var rectangle = {
 };
 
 var triangle = {
-    sideA: 3,
-    sideB: 4,
-    sideC: 4
+  sideA: 3,
+  sideB: 4,
+  sideC: 4
 };
+
+var simpleTriangle = function(triangle) {
+  var a = triangle.sideA;
+  var b = triangle.sideB;
+  var c = triangle.sideC;
+  return [a, b, c].sort(function(a,b){return a-b});
+}
 
 isSquare=function(length, width) {
   if (length == width) {
@@ -41,18 +48,27 @@ isIsosceles = function(side1, side2, side3) {
   return isosceles;
 }
 
-var simpleTriangle = function(triangle) {
-  var a = triangle.sideA;
-  var b = triangle.sideB;
-  var c = triangle.sideC;
-  return [a, b, c].sort();
-}
+
+// triangleArea = function(triangle) {
+//   var area_answer = (simpleTriangle(triangle)[0] + simpleTriangle(triangle)[1])/2;
+//   return area_answer;
+// }
 
 triangleArea = function(triangle) {
-  var area_answer = (simpleTriangle(triangle)[0] + simpleTriangle(triangle)[1])/2;
-  return area_answer;
+  p = (triangle.sideA + triangle.sideB + triangle.sideC)/2;
+  weird_sum = p*(p-triangle.sideA)*(p-triangle.sideB)*(p-triangle.sideC);
+  var area = Math.sqrt(weird_sum);
+  return area;
 }
 
+isObtuse = function(triangle) {
+  if ( (Math.pow(simpleTriangle(triangle)[0], 2) + Math.pow(simpleTriangle(triangle)[1], 2)) < Math.pow(simpleTriangle(triangle)[2], 2) ) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
 
 
