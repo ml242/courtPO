ff922cd3909144efb94420cc43777c248c6f84f2
Travis Vander Hoop <vanderhoop@me.com>
2013-10-28 23:35:17 -0400

    refactored calculator

--- w06/d01/travis_vanderhoop/calculator.js
+++ w06/d01/travis_vanderhoop/calculator.js
@@ -1,24 +1,32 @@
-var operator = prompt('What operation? Choose:\n\t 1 for addition\n\t 2 for subtraction\n\t 3 for multiplication\n\t or\n\t 4 for division')
-var firstValue = prompt('What is the first value?')
-var secondValue = prompt('What is the second value?')
+var calculator = {
 
-switch (operator)
-{
-case '1':
-  var answer = firstValue+secondValue;
-  debugger;
-  break;
-case '2':
-  var answer = firstValue-secondValue;
-  break;
-case '3':
-  var answer = firstValue*secondValue;
-  break;
-case '4':
-  var answer = firstValue/secondValue;
-  break;
-default:
-  alert('You entered a moronic value.');
+  calculate: function(){
+
+    var operator = prompt('What operation? Choose:\n\t 1 for addition\n\t 2 for subtraction\n\t 3 for multiplication\n\t or\n\t 4 for division')
+    var firstValue = prompt('What is the first value?')
+    var secondValue = prompt('What is the second value?')
+
+    switch (operator)
+    {
+    case '1':
+      var answer = firstValue+secondValue;
+      debugger;
+      break;
+    case '2':
+      var answer = firstValue-secondValue;
+      break;
+    case '3':
+      var answer = firstValue*secondValue;
+      break;
+    case '4':
+      var answer = firstValue/secondValue;
+      break;
+    default:
+      alert('You entered a moronic value.');
+    }
+
+    alert('The answer is ' + answer)
+
+  }
+
 }
-
-alert('The answer is ' + answer)
