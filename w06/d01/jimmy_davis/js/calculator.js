d613f4884392e4dcb433336140f1c5c9a4251d5f
James Davis <jamesmdavis5@gmail.com>
2013-10-28 22:29:33 -0400

    finished

--- w06/d01/jimmy_davis/js/calculator.js
+++ w06/d01/jimmy_davis/js/calculator.js
@@ -11,4 +11,32 @@ var calculator = {
   divide: function(a, b) {
     return a / b
   },
+  modulus: function(a, b) {
+    return a % b
+  },
+
+  calculate: function() {
+    a = parseInt(prompt("Enter first number"), 10);
+    b = parseInt(prompt("Enter second number"), 10);
+    operator = prompt("Enter operator (+, -, *, /, or %)");
+    switch(operator) {
+      case "+":
+        console.log(this.add(a, b));
+        break;
+      case "-":
+        console.log(this.subtract(a, b));
+        break;
+      case "*":
+        console.log(this.multiply(a, b));
+        break;
+      case "/":
+        console.log(this.divide(a, b));
+        break;
+      case "%":
+        console.log(this.modulus(a, b));
+        break;
+    }
+  }
 };
+
+calculator.calculate();
--- w06/d01/jimmy_davis/js/geometry.js
+++ w06/d01/jimmy_davis/js/geometry.js
@@ -1,6 +1,6 @@
 var rectangle = {
   length: 4,
-  width: 4
+  width: 4,
 
   isSquare: function() {
     if (this.length == this.width) {
@@ -27,14 +27,42 @@ console.log(rectangle.perimeter());
 var triangle = {
   sideA: 3,
   sideB: 4,
-  sideC: 4
+  sideC: 4,
+};
 
+function isEquilateral(triangle) {
   if (triangle.sideA == triangle.sideB && triangle.sideB == triangle.sideC) {
     return true;
   } else {
     return false;
   }
+}
 
-};
+console.log(isEquilateral(triangle));
 
+function isIsosceles(triangle) {
+  if (triangle.sideA == triangle.sideB || triangle.sideA == triangle.sideC || triangle.sideB == triangle.sideC) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isIsosceles(triangle));
+
+function calcArea(triangle) {
+   return (triangle.sideA * triangle.sideB) / 2
+}
+
+console.log(calcArea(triangle));
+
+function isObtuse(triangle) {
+  if ((triangle.sideA * triangle.sideA) + (triangle.sideB * triangle.sideB) < (triangle.sideC * triangle.sideC)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isObtuse(triangle));
 
