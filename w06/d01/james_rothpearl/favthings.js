b01938a8442f07bdb14c0b9929e43bb3a61e707a
James Rothpearl <jrothpearl@gmail.com>
2013-10-29 13:46:16 -0400

    up to calc step

---
+++ w06/d01/james_rothpearl/favthings.js
@@ -0,0 +1,5 @@
+var favThings = ["dogs", "coffee", "interw3bz", "phil", "alicia", "jon"]
+
+for (var i = 0; i < favThings.length; i++) {
+  console.log("One of my favorite things: " + favThings[i]);
+}
