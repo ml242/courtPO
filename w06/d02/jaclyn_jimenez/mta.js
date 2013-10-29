21bf0111c35d61dee49aac16b8455bacb029fbad
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-10-29 19:39:34 -0400

    transfers work

--- w06/d02/jaclyn_jimenez/mta.js
+++ w06/d02/jaclyn_jimenez/mta.js
@@ -7,26 +7,44 @@
 // first do it for number of stops in one line
 // create function that calculates number of stops
 
+
+//constructor function
 function Line(lineStops) {
   this.stops = lineStops
 };
 
+//functions
 Line.prototype.calculate_number_of_stops = function(stop1, stop2) {
-  var start_station = this.stops.indexOf(stop1) //finds index of first stop
-  var final_station = this.stops.indexOf(stop2)
-  return Math.abs(start_station - final_station)
+  return Math.abs(stop1 - stop2)
 }
 
-Line.prototype.calculate_transfers_of_stops = function() {
-  //code for function
+Line.prototype.calculate_transfers_of_stops = function(stop1, transfer1, stop2, transfer2) {
+  first_ride = Math.abs(stop1 - transfer1)
+  second_ride = Math.abs(stop2 - transfer2)
+  return first_ride + second_ride
 }
-// startLine = prompt("What line are you starting from?")
+
+//subway lines
+var nycLines = new Line({nLine: ["timesSquare", "34th", "28thN", "23rdN", "unionSquare", "8thN"],
+  sixLine: ["grandCentral", "33rd", "28th6", "23rd6", "unionSquare", "astorPlace"],
+  lLine: ["8thL", "6th", "unionSquare", "3rd", "1st"]
+  }
+);
+
+//prompts
+startLine = prompt("What line are you starting from?(nLine, sixLine, lLine)")
 firstStop = prompt("What stop are you getting on?")
+endLine = prompt("What line are you getting off? (nLine, sixLine, lLine)")
 finalStop = prompt("What stop are you getting off?")
 
-var nLine = new Line(["timesSquare", "34th", "28thN", "23rdN", "unionSquare", "8thN"]);
+//variables
+firstStop = nycLines.stops[startLine].indexOf(firstStop)
+transfer1 = nycLines.stops[startLine].indexOf("unionSquare")
+finalStop = nycLines.stops[endLine].indexOf(finalStop)
+transfer2 = nycLines.stops[endLine].indexOf("unionSquare")
 
-console.log(nLine.stops)
-console.log(nLine.calculate_number_of_stops(firstStop, finalStop))
+//runs everything
+total_stops = (nycLines.calculate_transfers_of_stops(firstStop, transfer1, finalStop, transfer2))
+alert("You will travel " + total_stops + " stops!")
 
 
