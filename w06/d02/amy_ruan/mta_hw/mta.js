569c7b91d06b703264e759a9cc88724426949ec9
Amy Ruan <nyc.amy@gmail.com>
2013-10-29 18:41:06 -0400

    one line with prompt

--- w06/d02/amy_ruan/mta_hw/mta.js
+++ w06/d02/amy_ruan/mta_hw/mta.js
@@ -1,6 +1,6 @@
-stops_n = ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th']
-stops_l = ['8th', '6th', 'Union Square', '3rd','1st']
-stops_s = ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th']
+stops_n = ['times square', '34th', '28th', '23rd', 'union square', '8th']
+stops_l = ['8th', '6th', 'union square', '3rd','1st']
+stops_s = ['times square', '34th', '28th', '23rd', 'union square', '8th']
 
 function Line(name,stops) {
   this.name = name;
@@ -11,20 +11,20 @@ function SubwaySystem(line1, line2, line3) {
   this.line1 = line1;
   this.line2 = line2;
   this.line3 = line3;
+
+
 }
 
 SubwaySystem.prototype.countStops = function(start,end) {
-  // this.line1
-  // this.line2
-  // this.start = start
-  // this.end = end
+  this.start_stop = prompt("What is your starting stop?");
+  this.end_stop = prompt("What is your ending stop?");
 
-  var num_of_start = this.line1.stops.indexOf(start)
-  var num_of_end = this.line1.stops.indexOf(end)
+  var num_of_start = this.line1.stops.indexOf(this.start_stop)
+  var num_of_end = this.line1.stops.indexOf(this.end_stop)
   var delta = Math.abs(num_of_end - num_of_start)
   console.log(delta);
-  return delta;
-  }
+  return delta
+}
 
 
 var n = new Line("N", stops_n);
