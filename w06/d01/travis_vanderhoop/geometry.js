e1fb6011c58869838f2cf3151ca190d584cd9238
Travis Vander Hoop <vanderhoop@me.com>
2013-10-28 20:11:30 -0400

    calculate area algorithm now complete

--- w06/d01/travis_vanderhoop/geometry.js
+++ w06/d01/travis_vanderhoop/geometry.js
@@ -3,6 +3,12 @@ var rectangle = {
   width: 4
 };
 
+var triangle = {
+    sideA: 3,
+    sideB: 4,
+    sideC: 4
+};
+
 isSquare=function(length, width) {
   if (length == width) {
     return true;
@@ -21,6 +27,32 @@ perimeter = function(length, width) {
   var perimeter_answer = (length*2) + (width*2);
   return perimeter_answer;
 }
+
+isEquilateral = function(side1, side2, side3) {
+  var equilateral = (side1==side2 && side1==side3);
+  return equilateral;
+}
+
+isIsosceles = function(side1, side2, side3) {
+  var isosceles = (
+    side1==side2 ||
+    side1==side3 ||
+    side2==side3);
+  return isosceles;
+}
+
+var simpleTriangle = function(triangle) {
+  var a = triangle.sideA;
+  var b = triangle.sideB;
+  var c = triangle.sideC;
+  return [a, b, c].sort();
+}
+
+triangleArea = function(triangle) {
+  var area_answer = (simpleTriangle(triangle)[0] + simpleTriangle(triangle)[1])/2;
+  return area_answer;
+}
+
 
 
 
