cb19b268e8267d19f40932c5f320a1ad0e3602ad
Amy Ruan <nyc.amy@gmail.com>
2013-10-29 08:24:08 -0400

    added extra - no two operands added

--- w06/d01/amy_ruan/javascript_hw/calculator.js
+++ w06/d01/amy_ruan/javascript_hw/calculator.js
@@ -1,8 +1,9 @@
 var calculator = {
  convert: function() {
   this.getFirstNumber()
-  this.getSecondNumber()
   this.getOperation()
+  this.getSecondNumber()
+
 
   switch(calculator.operation){
   case "add":
@@ -17,17 +18,25 @@ var calculator = {
   case "divide":
     var answer = (calculator.firstNumber)/(calculator.secondNumber);
     break;
+  case "sqrt":
+    var answer = Math.sqrt(calculator.firstNumber);
+    break;
+  case "modulus":
+    var answer = (calculator.firstNumber)%(calculator.secondNumber);
+    break;
+  default:
+    var answer = "Did you spell your operations correctly?"
   }
   return console.log(answer)
  },
 
  getOperation: function() {
-  this.operation = prompt("What operation? [add, subtract, multiply, divide")
+  this.operation = prompt("What operation? [add, subtract, multiply, divide, modulus, sqrt")
  },
  getFirstNumber: function() {
   this.firstNumber = parseFloat(prompt("Enter first number"));
  },
  getSecondNumber: function() {
-  this.secondNumber = parseFloat(prompt("Enter second number"));
+  this.secondNumber = parseFloat(prompt("Enter second number (leave blank if sqrt)"));
  }
 }
