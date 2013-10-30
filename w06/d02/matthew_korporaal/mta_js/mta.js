9e71933d1c91aee34da364386e9f1d5772fe0cf7
Matthew Korporaal <matthew.korporaal@gmail.com>
2013-10-29 22:11:17 -0400

    add bonus

--- w06/d02/matthew_korporaal/mta_js/mta.js
+++ w06/d02/matthew_korporaal/mta_js/mta.js
@@ -4,6 +4,15 @@ var lines = {
   6: ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"],
   xSect: "Union Square"
 }
+// var Line = function (line, station_array){
+//   debugger;
+//   this.setLines(line, station_array);
+// }
+
+// Line.prototype.setLines = function setLines(line, station_array)  {
+//   debugger;
+//   this.line = station_array;
+// }
 
 var MTA = function(){
 }
@@ -48,17 +57,27 @@ MTA.prototype.tell = function tell(response){
   return alert(response)
 }
 
-var locations = [];
-var mta = new MTA();
+// var N = new Line('N', ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th']);
+// var L = new Line('L', ['8th', '6th', 'Union Square', '3rd','1st']);
+// var S = new Line('S', ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th']);
 
-locations.push(mta.ask("What line are you getting on at: "));
-locations.push(mta.ask("Which station: "));
-locations.push(mta.ask("What line are you getting off at: "));
-locations.push(mta.ask("and which station: "));
 
-mta.setLocations(locations);
-mta.findRoute()
-mta.tell("It takes " + mta.delta + " stops to get from " + mta.startStation + " on the " + mta.startLine + " to " + mta.stopStation + " on the " + mta.stopLine + ".");
+var totalTrips = 0;
 
+while (confirm("Find Directions?")) {
+  var locations = [];
+  var mta = new MTA();
+
+  locations.push(mta.ask("What line are you getting on at: "));
+  locations.push(mta.ask("Which station: "));
+  locations.push(mta.ask("What line are you getting off at: "));
+  locations.push(mta.ask("and which station: "));
+
+  mta.setLocations(locations);
+  mta.findRoute()
+  mta.tell("It takes " + mta.delta + " stops to get from " + mta.startStation + " on the " + mta.startLine + " to " + mta.stopStation + " on the " + mta.stopLine + ".");
+  totalTrips++;
+  mta.tell("You have taken " + totalTrips + " trips, totaling: $" + (totalTrips*2.5));
+}
 
 
