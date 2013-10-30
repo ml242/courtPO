a405336d1259d3d09f6d1f3041cf2f1f867b1704
James Davis <jamesmdavis5@gmail.com>
2013-10-29 22:26:21 -0400

    need to refactor

--- w06/d02/jimmy_davis/mta/javascripts/mta.js
+++ w06/d02/jimmy_davis/mta/javascripts/mta.js
@@ -9,6 +9,9 @@ TrainLines = {
   s: "Grand Central, 33rd, 28th, 23rd, Union Square, Astor Place".split(", ")
 }
 
+// not as clear how to iterate through this
+// so I just used the above hash
+
 // var N = new Line('N', ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th']);
 // var L = new Line('L', ['8th', '6th', 'Union Square', '3rd','1st']);
 // var S = new Line('S', ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th']);
@@ -26,11 +29,8 @@ var startingLineArray = TrainLines[startLine];
 var startingIndex = startingLineArray.indexOf(startStation);
 var endingLineArray = TrainLines[endLine];
 var endingIndex = endingLineArray.indexOf(endStation);
-
-
 var distance = endingIndex - startingIndex;
 console.log(distance);
-
 
 
 console.log("You will be traveling for " + distance + " stops.");
