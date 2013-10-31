116a8f6cff7bcd8dd0648dd051ef3778dfc7d26c
James Davis <jamesmdavis5@gmail.com>
2013-10-31 19:51:14 -0400

    all 3 rollers there, can click all 3 buttons and get result, need to post results and only allow to click once

---
+++ w06/d04/jimmy_davis/d4_slottoween/v0_base/javascripts/roller.js
@@ -0,0 +1,25 @@
+function Roller(rollerNumber) {
+  this.rollerNumber = rollerNumber;
+  this.spinning = false;
+
+  if (this.rollerNumber === 1) {
+    this.images = document.getElementById("images-1").children;
+  }
+  else if (this.rollerNumber === 2) {
+    this.images = document.getElementById("images-2").children;
+  }
+  else if (this.rollerNumber === 3) {
+    this.images = document.getElementById("images-3").children;
+  }
+}
+
+Roller.prototype.spin = function() {
+  this.spinning = true;
+  this.images[this.curIndex].hide();
+}
+
+Roller.prototype.stop = function() {
+  this.curIndex = Math.floor(Math.random()*10);
+  var image = this.images[this.curIndex];
+  image.className = "";
+}
