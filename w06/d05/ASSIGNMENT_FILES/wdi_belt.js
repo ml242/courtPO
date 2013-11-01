22ac1adb4f576f827aa73081f0fa858e77414acd
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-11-01 12:47:28 -0400

    update belt

--- w06/d05/ASSIGNMENT_FILES/wdi_belt.js
+++ w06/d05/ASSIGNMENT_FILES/wdi_belt.js
@@ -5,6 +5,7 @@ WDIBelt.version = 0.2;
 WDIBelt.each = function(array, func) {
   for (var index = 0; index < array.length; index++) {
     var element = array[index];
+    // call the function in the context of this referring to the element
     func.call(element, index);
   }
 }
