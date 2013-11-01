f3b73b1a04a3fcb9a116ab03be75eb1f705e0079
James Davis <jamesmdavis5@gmail.com>
2013-10-31 21:12:17 -0400

    working on results alert

--- w06/d04/jimmy_davis/d4_slottoween/v0_base/javascripts/slot_machine.js
+++ w06/d04/jimmy_davis/d4_slottoween/v0_base/javascripts/slot_machine.js
@@ -3,41 +3,39 @@ SlotMachine = function() {
     // this will call the .start method
 
   var handleNode = document.getElementById("handle");
-  handleNode.addEventListener("click", this.start);
 
+  var mySlotMachine = this;
+
+  handleNode.addEventListener("click", function() {
+    mySlotMachine.start();
+  });
+
+// Need to refactor
   this.roller1 = new Roller(1);
   this.roller2 = new Roller(2);
   this.roller3 = new Roller(3);
 
-//    this.rollers = [];
-//   for (var i=0; i<3; i++) {
-//     var roller = new Slottoween.Roller(i);
-//     this.rollers.push(roller);
-//   }
-// var roller1 = roller[0]
-// var roller2 = roller[1]
-// var roller3 = roller[2]
+  this.totalGames = 0;
+  this.wins = 0;
 
 
-  var mySlotMachine = this;
-
-for (var i = 0; i < 1; i++) {
   var button1 = document.getElementById("button-1");
   button1.addEventListener("click", function() {
     mySlotMachine.stopRoller(1);
+    // mySlotMachine.checkWinOrLose();
   });
 
   var button2 = document.getElementById("button-2");
   button2.addEventListener("click", function() {
     mySlotMachine.stopRoller(2);
+    // mySlotMachine.checkWinOrLose();
   });
 
   var button3 = document.getElementById("button-3");
   button3.addEventListener("click", function() {
     mySlotMachine.stopRoller(3);
+    // mySlotMachine.checkWinOrLose();
   });
-}
-
 
 
   //  I need to setup my eventListeners so that when I click on the button
@@ -81,9 +79,10 @@ SlotMachine.prototype.stopRoller =  function(rollerNumber) {
 
 SlotMachine.prototype.checkWinOrLose = function() {
   // determine if you have won or not and does the appropiate "messaging"
-  if (roller1.curIndex === roller2.curIndex && roller2.curIndex === roller3.curIndex) {
+  if (this.roller1.curIndex === this.roller2.curIndex && this.roller2.curIndex === this.roller3.curIndex) {
     alert("You win!");
   } else {
     alert("You lose!");
   }
 }
+mySlotMachine.checkWinOrLose();
