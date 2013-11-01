32e2974e3d0fb3562ac7f2f89c4d7e0171a7f96d
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-11-01 14:29:51 -0400

    alderaan

---
+++ w06/d05/ASSIGNMENT_FILES/main.js
@@ -0,0 +1,3 @@
+// this is my file
+// ---------------
+console.log('hi');
--- w06/d05/ASSIGNMENT_FILES/wdi_belt.js
+++ w06/d05/ASSIGNMENT_FILES/wdi_belt.js
@@ -6,7 +6,7 @@ WDIBelt.each = function(array, func) {
   for (var index = 0; index < array.length; index++) {
     var element = array[index];
     // call the function in the context of this referring to the element
-    func.call(element, index);
+    func.call(element, element, index);
   }
 }
 
@@ -16,5 +16,16 @@ WDIBelt.map = function(array, func) {
     var element = array[index];
     results.push(func(element));
   }
+  return results;
+}
+
+WDIBelt.select = function(array, func) {
+  var results = [];
+  this.each(array, function(value, index) {
+    debugger;
+    if (func(value)) {
+      results.push(this);
+    }
+  });
   return results;
 }
