333dfa5e0a544cd6c22eb0966310a53034612f09
JC <jorge.e.colindres@gmail.com>
2013-11-01 18:15:05 -0400

    turned star wars into objects

--- w06/d05/jorge_colindres/alderaan/main.js
+++ w06/d05/jorge_colindres/alderaan/main.js
@@ -1,55 +1,88 @@
-var alderaan = document.getElementById('alderaan');
-var deathStar = document.getElementById('death-star');
-var buttonNever = document.getElementById('never');
-var tellLink = document.getElementById('tell');
-var secretForm = document.getElementById('secret-form');
-var locationInput = document.getElementById('location');
-var helpForm = document.getElementById('help-form');
-var helpMe = document.getElementById('help-me');
 
-window.onload = function(){
-  alderaan.addEventListener('mouseover', function(){
-    thisNewText.call(this, 'Alderaan');
-  });
-  deathStar.addEventListener('mouseover', function(){
-    thisNewText.call(this, 'Deathstar');
-  });
-  buttonNever.addEventListener('click', function(){
-    window.setTimeout(function(){
-      alderaan.remove();
-    }, 5000);
-  });
-  tellLink.addEventListener('click', function(e){
-    revealItem(secretForm);
-    e.preventDefault();
-  });
-  locationInput.addEventListener('keyup', function(){
-    checkInputValue.call(this, 'Yavin 4', 'green', 'red');
-  });
-  document.body.addEventListener('keypress', function(e){
-    var letterH = String.fromCharCode(e.which);
-    if (letterH === 'h'){
-      revealItem(helpForm);
+function Galaxy(){
+  this.buttonNever = document.getElementById('never');
+  this.tellLink = document.getElementById('tell');
+  this.secretForm = document.getElementById('secret-form');
+  this.locationInput = document.getElementById('location');
+  this.helpForm = document.getElementById('help-form');
+  this.helpMe = document.getElementById('help-me');
+  this.go =
+  function(){
+    self = this;
+
+    this.alderaan.init();
+    this.deathStar.init();
+
+    this.hoverPlanets([theGalaxy.alderaan, theGalaxy.deathStar]);
+
+    this.buttonNever.addEventListener('click', function(){
+      window.setTimeout(function(){
+        alderaan.remove();
+      }, 5000);
+    });
+
+    this.tellLink.addEventListener('click', function(e){
+      self.revealItem(self.secretForm);
+      e.preventDefault();
+    });
+
+    this.inputColorfy(self.locationInput, 'Yavin 4', 'green', 'red');
+
+    document.body.addEventListener('keypress', function(e){
+      var letterH = String.fromCharCode(e.which);
+      if (letterH === 'h'){
+        self.revealItem(self.helpForm);
+      }
+    });
+
+    this.inputColorfy(self.helpMe, "Help me Obi Wan, you're my only hope", 'blue', 'white');
+
+  };
+  this.hoverPlanets =
+  function (planets){
+    for (var i = 0; i < planets.length; i++) {
+      planets[i].node.addEventListener('mouseover', function(){
+        this.textContent = this.parent.name;
+      });
     }
-  });
-  helpMe.addEventListener('keyup', function(){
-    checkInputValue.call(this, "Help me Obi Wan, you're my only hope", 'blue', 'white');
-  });
-};
+  };
+  this.inputColorfy =
+  function (element, textValue, colorTrue, colorFalse){
+    var self = this;
+    element.addEventListener('keyup', function(){
+      self.checkInputValue.call(this, self, textValue, colorTrue, colorFalse);
+    });
+  };
+  this.changeBodyBgColor =
+  function (color){
+    document.body.style.backgroundColor = color;
+  };
+  this.checkInputValue =
+  function (galaxy, textValue, colorTrue, colorFalse){
+    var self = galaxy;
+    if (this.value === textValue){
+      self.changeBodyBgColor(colorTrue);
+    } else {
+      self.changeBodyBgColor(colorFalse);
+    }
+  };
+  this.revealItem = function (item){
+    item.className = 'block';
+  };
+}
 
-function changeBodyBgColor(color){
-  document.body.style.backgroundColor = color;
+function Planet(name, node){
+  this.name = name;
+  this.node = node;
+  this.init =
+  function() {
+    this.node.parent = this;
+  };
 }
-function revealItem(item){
-  item.className = 'block';
-}
-function thisNewText(content){
-  this.textContent = content;
-}
-function checkInputValue (textValue, colorTrue, colorFalse){
-  if (this.value === textValue){
-    changeBodyBgColor(colorTrue);
-  } else {
-    changeBodyBgColor(colorFalse);
-  }
-}
+
+theGalaxy = new Galaxy();
+
+theGalaxy.alderaan = new Planet('Alderaan', document.getElementById('alderaan'));
+theGalaxy.deathStar = new Planet('Deathstar', document.getElementById('death-star'));
+
+theGalaxy.go();
