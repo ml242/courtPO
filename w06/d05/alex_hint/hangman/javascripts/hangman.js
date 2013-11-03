4c434bbf5a792c896e6c6aaa993171a2a1549410
DeProdigy <alex.hint.ah@gmail.com>
2013-11-03 18:05:11 -0500

    one input working

---
+++ w06/d05/alex_hint/hangman/javascripts/hangman.js
@@ -0,0 +1,66 @@
+//The game selects a random word from a word list and makes that the secret word
+//The player guesses one letter at a time, trying to figure out what the word is
+//If the player guesses correctly, any instances of that letter are revealed in the secret word
+
+//If the player guesses incorrectly, they are penalized by taking away a guess
+
+//If a player reveals all of the letters of the secret word, they win
+
+//If a player makes 8 incorrect guesses before solving the secret word, they lose
+
+Hangman = function() {
+
+  //word list
+  this.wordList = ["yes", "basic", "maybe", "hello"];
+  //randomly select a word from the word list and make it the secret word
+  this.createSecretWord();
+  this.playGame();
+
+}
+
+Hangman.prototype.createSecretWord = function() {
+  //debugger;
+  var randomIndex = Math.floor(Math.random()*(this.wordList.length));
+  this.secretWord = this.wordList[randomIndex];
+}
+
+Hangman.prototype.playGame = function() {
+  this.incorrectGuesses = [];
+  this.correctGuesses = [];
+  this.keepPlaying = true;
+
+  while (this.keepPlaying) {
+    this.askForInput();
+    this.checkTheInput();
+    if (this.incorrectGuesses.length === 8) {
+      this.keepPlaying = false;
+    }
+  }
+}
+
+Hangman.prototype.askForInput = function() {
+  this.currentInput = prompt('Enter a letter');
+}
+
+Hangman.prototype.checkTheInput = function() {
+  //turn the secret word into array
+  var secretWordArray = this.secretWord.split('');
+
+  for (var i = 0; i < secretWordArray.length; i++) {
+    var letter = secretWordArray[i];
+    if (this.currentInput === letter) {
+      this.correctGuesses.push(this.currentInput);
+    } else {
+      this.incorrectGuesses.push(this.currentInput);
+    }
+  }
+
+  console.log("Current input: " + this.currentInput);
+  console.log(this.secretWord);
+
+  this.keepPlaying = false;
+}
+
+
+
+
---
+++ w06/d05/alex_hint/wdi_belt.js
@@ -0,0 +1,19 @@
+var WDIBelt = WDIBelt || {};
+
+WDIBelt.version = 0.2;
+
+WDIBelt.each = function(array, func) {
+  for (var index = 0; index < array.length; index++) {
+    var element = array[index];
+    func.call(element, element, index);
+  }
+}
+
+WDIBelt.map = function(array, func) {
+  var results = [];
+  for (var index = 0; index < array.length; index++) {
+    var element = array[index];
+    results.push(func(element));
+  }
+  return results;
+}
