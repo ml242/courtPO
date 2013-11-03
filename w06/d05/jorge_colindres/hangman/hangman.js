35843d173cbd5b533d9ecd6e5a8c3c3726d7231d
JC <jorge.e.colindres@gmail.com>
2013-11-03 18:45:19 -0500

    input and letter display working

--- w06/d05/jorge_colindres/hangman/hangman.js
+++ w06/d05/jorge_colindres/hangman/hangman.js
@@ -2,13 +2,20 @@
 // Build a hangman object
 function Hangman(wordBank) {
   var self = this;
-  this.wordBank = wordBank;
+  this.wordBank = wordBank || ['smiths', 'dog']; // Can pass in custom wordbank or take default
   this.gamesPlayed = 0;
   this.gamesWon = 0;
+  this.shuffleWordBank = function(wordBank) {
+    wordBank.sort(function(){
+      return 0.5 - Math.random();
+    });
+    return wordBank;
+  };
   this.init = function(){
-    var currentGame = new Game('smith');
-    currentGame.init();
-    if (currentGame.won === true) {
+    var shuffledWordBank = self.shuffleWordBank(self.wordBank);
+    self.currentGame = new Game(shuffledWordBank[0]);
+    self.currentGame.init();
+    if (self.currentGame.won === true) {
       self.gamesWon++;
     }
     self.gamesPlayed++;
@@ -20,29 +27,41 @@ function Game(secretWord){
   var self = this;
   this.won = false;
   this.secretWord = secretWord;
+  this.secretArray = this.secretWord.split('');
   this.guessesLeft = 8;
   this.guesses = [];
   this.correctGuesses = [];
-  this.promptGuess = function(){
-    var currentGuess = prompt('Guess a letter');
+  this.submitGuess = function(wordInput){
+    var currentGuess = wordInput.value;
+    wordInput.value = '';
     self.guesses.push(currentGuess);
     self.guessesLeft--;
     self.checkGuess(currentGuess);
+    self.checkGame();
   };
   this.checkGuess = function(currentGuess){
-    var secretArray = self.secretWord.split('').sort();
+    var secretArray = self.secretArray.sort();
     self.each(secretArray, function(element, index){
       if (element === currentGuess){
-        console.log('Right');
-        console.log(element);
         self.correctGuesses.push(element);
         if (self.correctGuesses.sort().join() === secretArray.join()){
           self.won = true;
         }
-      } else {
-        console.log('Wrong');
       }
     });
+  };
+  this.checkGame = function(){
+    self.each(self.correctGuesses, function(element, index){
+      var lettersToShow = document.getElementsByClassName('letter-' + element);
+      self.each(lettersToShow, function(element, index){
+        element.className = 'letter visible';
+      });
+    });
+    if (self.guessesLeft > 0 && self.won){
+      alert('You win!');
+    } else if (self.guessesLeft === 0) {
+      alert('You loose');
+    }
   };
   this.each = function(array, func) {
     for (var index = 0; index < array.length; index++) {
@@ -51,18 +70,21 @@ function Game(secretWord){
     }
   };
   this.init = function(){
-    while (self.guessesLeft > 0){
-      if (self.won){
-        alert("You win!");
-        break;
-      }
-      self.promptGuess();
-      if (self.guessesLeft === 0) {
-        alert ('You lose!');
-      }
-    }
+    var secretWordNode = document.getElementById('secretWord');
+    self.each(self.secretArray, function(element, index){
+      var letter = document.createElement('span');
+      letter.textContent = element;
+      letter.className = 'letter hidden letter-' + element;
+      secretWordNode.appendChild(letter);
+    });
+    var userGuessFormNode = document.getElementById('userGuessForm');
+    var userGuessNode = document.getElementById('userGuess');
+    userGuessFormNode.addEventListener('submit', function(e){
+      e.preventDefault();
+      self.submitGuess(userGuessNode);
+    });
   };
 }
 
-var testing = new Hangman([]);
+var testing = new Hangman();
 testing.init();
