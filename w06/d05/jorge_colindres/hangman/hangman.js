e0f2e5b6a402c2f51419cc587cb0098ec81ee86d
JC <jorge.e.colindres@gmail.com>
2013-11-03 21:42:38 -0500

    reload

--- w06/d05/jorge_colindres/hangman/hangman.js
+++ w06/d05/jorge_colindres/hangman/hangman.js
@@ -3,8 +3,6 @@
 function Hangman(wordBank) {
   var self = this;
   this.wordBank = wordBank || ['smiths', 'dog']; // Can pass in custom wordbank or take default
-  this.gamesPlayed = 0;
-  this.gamesWon = 0;
   this.shuffleWordBank = function(wordBank) {
     wordBank.sort(function(){
       return 0.5 - Math.random();
@@ -15,10 +13,6 @@ function Hangman(wordBank) {
     var shuffledWordBank = self.shuffleWordBank(self.wordBank);
     self.currentGame = new Game(shuffledWordBank[0]);
     self.currentGame.init();
-    if (self.currentGame.won === true) {
-      self.gamesWon++;
-    }
-    self.gamesPlayed++;
   };
 }
 
@@ -31,6 +25,7 @@ function Game(secretWord){
   this.guessesLeft = 8;
   this.guesses = [];
   this.correctGuesses = [];
+  this.gameOver = false;
   this.submitGuess = function(wordInput){
     var currentGuess = wordInput.value;
     wordInput.value = '';
@@ -59,14 +54,17 @@ function Game(secretWord){
     });
     self.showGuesses();
     if (self.guessesLeft > 0 && self.won){
+      self.restartGame();
       alert('You win!');
     } else if (self.guessesLeft === 0) {
+      self.revealAnswer();
+      self.restartGame();
       alert('You loose');
     }
   };
   this.showGuesses = function(){
     var guessesListNode = document.getElementById('yourGuesses');
-    if (self.guesses.length > 1){
+    if (self.guesses.length >= 0){
       // Delete all list items if they already exist so that when
       // we add them a few lines below, there won't be duplicates
       while (guessesListNode.firstChild){
@@ -79,11 +77,33 @@ function Game(secretWord){
       guessesListNode.appendChild(listItem);
     });
   };
+  this.giveUp = function(){
+    self.guessesLeft = 0;
+    self.revealAnswer();
+    self.restartGame();
+    alert('You gave up!');
+  };
+  this.revealAnswer = function(){
+    self.each(self.secretArray, function(element, index){
+      var lettersToShow = document.getElementsByClassName('letter-' + element);
+      self.each(lettersToShow, function(element, index){
+        element.className = 'letter visible';
+      });
+    });
+  };
   this.each = function(array, func) {
     for (var index = 0; index < array.length; index++) {
       var element = array[index];
       func.call(element, element, index);
     }
+  };
+  this.restartGame = function(){
+    self.gameOver = true;
+    var restartGame = document.getElementById('restartGame');
+    restartGame.className = 'yes-display';
+    restartGame.addEventListener('click', function (){
+      location.reload();
+    });
   };
   this.init = function(){
     var secretWordNode = document.getElementById('secretWord');
@@ -95,9 +115,14 @@ function Game(secretWord){
     });
     var userGuessFormNode = document.getElementById('userGuessForm');
     var userGuessNode = document.getElementById('userGuess');
+    var giveUpButton = document.getElementById('giveUp');
+    var play = document.getElementById('playAgain');
     userGuessFormNode.addEventListener('submit', function(e){
       e.preventDefault();
       self.submitGuess(userGuessNode);
+    });
+    giveUpButton.addEventListener('click', function (){
+      self.giveUp();
     });
   };
 }
