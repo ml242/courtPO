bb98a7fa9c72da006f843511135095ad2bb65c39
Matthew Korporaal <matthew.korporaal@gmail.com>
2013-11-04 00:39:55 -0500

    hangman working copy

--- w06/d05/matthew_korporaal/hangman_app/javascripts/game.js
+++ w06/d05/matthew_korporaal/hangman_app/javascripts/game.js
@@ -1,101 +1,120 @@
 var Hangman = Hangman || {};
 
 Hangman.Game = function() {
-
   //add start button listener
+  this.firstGame = true;
   this.start = document.getElementById('start-button');
   this.addStartListeners(this.start);
+  this.gamesWonNode = document.getElementsByClassName("games-won")[0];
+  this.gamesLostNode = document.getElementsByClassName("games-lost")[0];
+}
 
+Hangman.Game.prototype.addStartListeners = function(start) {
+  thisGame = this;
+  start.addEventListener("click", function(e) {
+    if (confirm("Would you like to start a new game? Confirm(Ok) or No(Cancel)")) {
+      thisGame.startGame();
+    }
+  });
+}
+
+Hangman.Game.prototype.startGame = function() {
+  //initialize data
+  this.hmData = new Hangman.Data();
   //initialize words array, letters object w/ used/unused toggle
-  this.secretWord = this.getSecretWord();
+  this.secretWord = this.hmData.getSecretWord();
   this.createAnswer();
-  this.lettersCorrect = 0;
+
+  pNodes = document.getElementsByTagName('p');
+  this.guesses = [];
+  for(var i=0; i<pNodes.length; i++) {
+    var guessNode = pNodes[i];
+    this.guesses[i] = new Hangman.Image(guessNode);
+  }
+
   //initialize letters & add letter listeners
-  // imageNodes = document.getElementsByClassName('images');
   aNodes = document.getElementsByTagName('a');
-  debugger;
   this.images = [];
   for(var i=0; i<aNodes.length; i++) {
     var imageNode = aNodes[i];
     this.images[i] = new Hangman.Image(imageNode);
-    thisHangman = this;
-    // Add event click event if image is a letter
-    if (i < 26) {
-      this.addLetterListeners(imageNode, this.secretWord, this.checkMatch)
+    // Add event click event to the letter
+    if(this.firstGame == true) {
+      this.addLetterListeners(imageNode, this.checkMatch);
     }
   }
-
-  //load wins/losses
-  this.gamesWon = 0;
-  this.gamesWonNode = document.getElementsByClassName("games-won")[0];
-
-  this.gamesLost = 0;
-  this.gamesLostNode = document.getElementsByClassName("games-lost")[0];
-
-  // this.soundBoard = new Slottoween.SoundBoard();
-
-
+  this.firstGame = false;
 }
 
-Hangman.Game.prototype.addStartListeners = function(start) {
-  start.addEventListener("mouseup", function(e) {
-    if (confirm("Would you like to start a new game? Confirm(Ok) or No(Cancel)")) {
-      this.start();
-    }
-  });
-}
-
-Hangman.Game.prototype.getSecretWord = function() {
-  var data = new Hangman.Data();
-  return data.getSecretWord();
-}
 Hangman.Game.prototype.createAnswer = function() {
   ansDiv = document.getElementById("answer");
+  // Clear previous answer
+  ansDiv.innerHTML = ""
   for(var i=0;i<this.secretWord.length;i++){
-    a = document.createElement("a");
-    a.textContent = "_ ";
-    ansDiv.appendChild(a);
+    // make _ for each letter of secret word
+    p = document.createElement("p");
+    p.textContent = "_ ";
+    ansDiv.appendChild(p);
   }
 }
-Hangman.Game.prototype.addLetterListeners = function(imageNode, secretWord, checkMatch) {
-  imageNode.addEventListener("mouseup", function(e) {
-      checkMatch(secretWord, imageNode);
+
+Hangman.Game.prototype.addLetterListeners = function(imageNode, checkMatch) {
+  imageNode.addEventListener("click", function(e) {
+    e.stopPropagation();
+    checkMatch(e, imageNode);
   });
 }
-Hangman.Game.prototype.checkMatch = function(secretWord, imageNode){
-  imageNode.className = "hidden"
-  debugger
+
+Hangman.Game.prototype.checkMatch = function(event, imageNode){
+  event.stopPropagation();
+  imageNode.className = "hidden";
+  thisGame = this.hangman;
+  hmData = thisGame.hmData;
+  hmData.foundMatch = false;
   ansLetters = document.getElementById("answer").children;
-  this.hangman.match = false;
+
   //check each letter of secret word for match
-  for(var i=0;i<secretWord.length;i++) {
-    if (secretWord[i].match(imageNode.textContent) != null) {
+  for(var i=0;i<thisGame.secretWord.length;i++) {
+    // correct guess, reveal letter on answer, check if game is over
+    if (thisGame.secretWord[i].match(imageNode.textContent) != null) {
       ansLetters[i].textContent = imageNode.textContent + " ";
-      this.hangman.lettersCorrect++;
-      if(this.hangman.lettersCorrect === secretWord.join("").length){
-        this.hangman.gameOver = true;
-        this.hangman.gamesWon++;
-
+      hmData.lettersCorrect++;
+      if(hmData.lettersCorrect === thisGame.secretWord.join("").length){
+        hmData.gameOver = true;
+        hmData.gamesWon++;
+        alert("You won.")
       }
-      this.hangman.match = true;
+      hmData.foundMatch = true;
     }
+  }
+  // wrong guess, remove chance from board, inc badGuess, and check for end game
+  if(!hmData.foundMatch) {
+    badGuessNode = document.getElementsByTagName("p");
+    badGuess = badGuessNode[hmData.badGuessCount];
+    badGuess.className = "hidden"
+    hmData.badGuessCount++;
+    // if bad guesses used up, you lose
+    if(hmData.badGuessCount == 10){
+      hmData.gameOver = true;
+      hmData.gamesLost++;
+      alert("You Lost.")
+    }
+  }
 
+  // end of game, hide leftover letters and update scoreboard
+  if(hmData.gameOver){
+    thisGame.hideLetters();
+    thisGame.updateScoreBoard();
   }
-  if(!this.hangman.match) {
-    badGuess = document.getElementById("wrong-guess");
-    if(badGuess.textContent === "9"){
-      this.hangman.gameOver = true;
-      this.hangman.gamesLost++;
-    }
-    badGuess.id = "";
-    badGuess.textContent = "";
-  }
-  if(this.hangman.gameOver){
-    this.hangman.updateScoreBoard()
+}
+
+Hangman.Game.prototype.hideLetters = function() {
+  for(var i=0;i<this.images.length;i++){
+    this.images[i].hide();
   }
 }
 Hangman.Game.prototype.updateScoreBoard = function() {
-  debugger
-  this.gamesWonNode.innerHTML = this.gamesWon;
-  this.gamesLostNode.innerHTML = this.gamesLost;
+  this.gamesWonNode.innerHTML = hmData.gamesWon;
+  this.gamesLostNode.innerHTML = hmData.gamesLost;
+
 }
