2cceff968bbaedec55c5f95bb911544a764c7097
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-11-04 01:32:54 -0500

    hangman guesses and win/lose work. working on allowing user to play again

--- w06/d05/jaclyn_jimenez/hangman/app/assets/javascripts/hangman.js
+++ w06/d05/jaclyn_jimenez/hangman/app/assets/javascripts/hangman.js
@@ -1,74 +1,142 @@
-//alert("javascript is running")
 function HangManGame() {
-var words = ["COMPACT", "DISGUISES", "SMARTEST", "COOKIE", "EXTRA"] //hangman answers
-var secretIndex = Math.floor(Math.random() * words.length)
-this.secretWord = words[secretIndex] //chooses random word from words array
 
-convertToArray = function(word) { //function that turns word into array. example: ["w","o","r","d"]
-  var wordArray = []
-  for (var index = 0; index < word.length; index++) {
-    var letter = word[index];
-    wordArray.push(letter)
+  var words = ["COMPACT", "DISGUISES", "SMARTEST", "COOKIE", "EXTRA"] //hangman answers
+
+  //create textbox that takes a letter input
+  var textBox = document.createElement('input')
+  textBox.type = "text"
+  textBox.className = "textBox"
+  textBox.name = "textBox"
+
+  document.body.appendChild(textBox)
+
+    textBox.addEventListener.call(textBox, "input", function() {
+      if (this.value.length > 1) {
+          this.value = this.value.substring(1, 2);
+      }
+  })
+//Begin Function Start Game
+function StartGame() {
+
+  var secretIndex = Math.floor(Math.random() * words.length)
+  this.secretWord = words[secretIndex] //chooses random word from words array
+
+  convertToArray = function(word) { //function that turns word into array. example: ["w","o","r","d"]
+    var wordArray = []
+    for (var index = 0; index < word.length; index++) {
+      var letter = word[index];
+      wordArray.push(letter)
+    }
+    return wordArray
   }
-  return wordArray
-}
-secretWordArray = convertToArray(secretWord); //executes toArray function
+  secretWordArray = convertToArray(secretWord); //executes toArray function
 
-createWordSpace = function(word) {
+
   var secretDiv = document.createElement('div'); // creates div
-  for (var index=0; index < word.length; index++) {
-    var letterDiv = document.createElement('div')
-    var secretH2 = document.createElement('h2'); // creates h2
-    secretH2.textContent = word[index]; // adds secret word as text content to the h2
-    secretH2.className = "hidden "+word[index]
-    letterDiv.className = "letter"
-    letterDiv.id = "letter-"+index
-    letterDiv.appendChild(secretH2)
-    secretDiv.appendChild(letterDiv); //puts h2 in the div
+  createWordSpace = function(word) {
+
+    for (var index=0; index < word.length; index++) {
+      var letterDiv = document.createElement('div')
+      var secretH2 = document.createElement('h2'); // creates h2
+      secretH2.textContent = word[index]; // adds secret word as text content to the h2
+      secretH2.className = "hidden "+word[index]
+      letterDiv.className = "letter"
+      letterDiv.id = "letter-"+index
+      letterDiv.appendChild(secretH2)
+      secretDiv.appendChild(letterDiv); //puts h2 in the div
+    }
+    document.body.appendChild(secretDiv); // adds div to document
+    document.body.insertBefore(secretDiv,textBox);
   }
-  document.body.appendChild(secretDiv); // adds div to document
-}
 
-createWordSpace(secretWordArray);
+  createWordSpace(secretWordArray);
 
-//create textbox that takes a letter input
-var textBox = document.createElement('input')
-textBox.type = "text"
-textBox.className = "textBox"
-textBox.name = "textBox"
+  var guessesLeft = 8
+  this.guessesLeft = guessesLeft
+  var guessesLeftOutput = document.createElement('h2')
+  var guessesLeftDiv = document.createElement('div')
+  guessesLeftOutput.textContent = "Guesses Left: " + guessesLeft
+  guessesLeftDiv.id = "guessesLeft"
+  guessesLeftDiv.class = "guessesLeft"
+  guessesLeftDiv.appendChild(guessesLeftOutput)
+  document.body.insertBefore(guessesLeftDiv, secretDiv)
 
-document.body.appendChild(textBox)
+  }
 
-//if user input == one of the letters in the word (iterate over it)
-guessChecker = function (guess, secretWordArray) {
-  for (var index=0; index < secretWordArray.length; index++) {
-    if (guess === secretWordArray[index]) {
-      var lettersToReveal = document.getElementsByClassName(secretWordArray[index]) //array of letters that match that letter
-      for (var i=0; i < lettersToReveal.length; i++) {
-        lettersToReveal[i].className = lettersToReveal[i].textContent
+
+
+  //end function StartGame
+
+
+  //Begin Function Guesses
+  function Guesses() {
+
+    guessChecker = function (guess, secretWordArray) {
+      var numberWrong = 0
+      for (var index=0; index < secretWordArray.length; index++) {
+        if (guess === secretWordArray[index]) {
+          var lettersToReveal = document.getElementsByClassName(secretWordArray[index]) //array of letters that match that letter
+          for (var i=0; i < lettersToReveal.length; i++) {
+            lettersToReveal[i].className = lettersToReveal[i].textContent
+          }
+        } else {
+          numberWrong++
+        }
+        if (numberWrong == secretWordArray.length) {
+          guessesLeft--
+          document.getElementById("guessesLeft").children[0].textContent = "Guesses Left: " + guessesLeft
+        }
+      }
+      winChecker()
+    }
+  //set secretH2.className to revealed
+
+      winChecker = function() {
+      if (guessesLeft != 0) {
+        var currentLettersArray = document.getElementsByClassName("letter") //array of letters
+        hidden(currentLettersArray, function(elem, klass) {
+         return (" " + elem.className + " " ).indexOf( " "+klass+" " ) > -1;
+    })
+      } else {
+        alert("you lose!")
       }
     }
-  }
-}
-//set secretH2.className to revealed
 
-textBox.addEventListener.call(textBox, "input", function() {
-    if (this.value.length > 1) {
-        this.value = this.value.substring(1, 2);
+    function hidden(currentLettersArray, fn) {
+      var numOfHidden = currentLettersArray.length
+      for (var index=0; index < currentLettersArray.length; index++){ //for each letter
+        var thisLetter = currentLettersArray[index].children[0]
+        var isHidden = fn(thisLetter, "hidden")
+        numOfHidden -= evaluate(isHidden, numOfHidden)
+      }
+      if (numOfHidden === 0) { //if none are hidden
+        alert("You win!")
+      } else {
+        console.log("keep going")
+      }
+    };
+
+    function evaluate(isHidden, numOfHidden) {
+      if (isHidden) {  //if its hidden
+        return 0
+      } else {
+        return 1
+      }
+    };
+
+  textBox.addEventListener.call(textBox, "input", function() {
+    if (this.value) {
+      var guess = this.value.toUpperCase()
+      guessChecker(guess, secretWordArray)
+     } else {
+      alert("I went to else for some reason")
     }
-})
+  })
+}
+//end Function Guesses
 
-textBox.addEventListener.call(textBox, "input", function() {
-  if (this.value) {
-    var guess = this.value.toUpperCase()
-    guessChecker(guess, secretWordArray)
-   } else {
-    alert("I went to else for some reason")
-  }
-})
-
-
-
+StartGame()
+Guesses()
 
 }
 
