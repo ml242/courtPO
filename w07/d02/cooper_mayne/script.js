dd5755a51f44fffc3ee80be248efcf9f3bf21121
Cooper Mayne <coopermayne@gmail.com>
2013-11-05 15:52:02 -0500

    paint working

--- w07/d02/cooper_mayne/script.js
+++ w07/d02/cooper_mayne/script.js
@@ -1,3 +1,5 @@
+var paintColorClass = 'black'
+
 var mouseDown = 0;
 document.body.onmousedown = function() { 
   ++mouseDown;
@@ -11,42 +13,28 @@ for (var i = 0; i < 4000; i++) {
 };
 
 $('.tile').on('mouseenter',function() {
-  $(this).addClass('black');
+  $(this).addClass(paintColorClass + '-hover');
 })
 
 $('.tile').on('mouseleave',function() {
-  $(this).removeClass('black');
+  $(this).removeClass(paintColorClass + '-hover');
 })
 
 $('.tile').on('click',function() {
-  $(this).addClass('blackp');
+  $(this).removeClass();
+  $(this).addClass('tile')
+  $(this).addClass(paintColorClass + '-painted');
 })
 
 $('.tile').on('mousemove',function(e) {
   if (mouseDown==1) {
-    $(this).addClass('blackp');
+    $(this).removeClass();
+    $(this).addClass('tile')
+    $(this).addClass(paintColorClass + '-painted');
   }
 })
 
-//var colorHistory = [[0,0,0]]
-//var colorHistoryIndex = 0
-
-//$("#colorButton").on("click", function() {
-  //rVar = parseInt($('input[name=r]')[0].value)
-  //gVar = parseInt($('input[name=g]')[0].value)
-  //bVar = parseInt($('input[name=b]')[0].value)
-  //var colorsRGB = [rVar,gVar,bVar];
-  //colorHistory.push(colorsRGB);
-  //rgbaString = "rgb("+ colorsRGB.join(',')+")";
-  //$('body').css("background-color", rgbaString);
-//})
-
-//$('#cycleColorButtons').on("click", function(){
-  //var ind = colorHistoryIndex%colorHistory.length;
-  //var colorsRGB = colorHistory[ind];
-
-  //rgbaString = "rgb("+ colorsRGB.join(',')+")";
-  //$('body').css("background-color", rgbaString);
-
-  //colorHistoryIndex++;
-//})
+$('#color-palette > *').on('click',function() {
+  // set the paint color class to the div's color
+  paintColorClass = $(this).attr('class')
+})
