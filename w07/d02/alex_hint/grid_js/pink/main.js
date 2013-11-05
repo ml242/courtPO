831329be6e926f168575c1dc9207104d5c5db6d7
DeProdigy <alex.hint.ah@gmail.com>
2013-11-05 13:34:47 -0500

    3_click_to_pink_click_to_unpink

--- w07/d02/alex_hint/grid_js/pink/main.js
+++ w07/d02/alex_hint/grid_js/pink/main.js
@@ -1,7 +1,22 @@
 // 1_click_to_pink_inline_css
 // Use JQuery so that when you click on "Click Me", it will make the page have a pink background. Use JQuery's .css method to do this first which changes it's inline style
-$("#page").click(function() {
-  $(this).css("background-color", "pink");
-});
+// $("#button").click(function() {
+//   $("#page").css("background-color", "pink");
+// });
+
 //2_click_to_pink_via_setting_class
 //Instead of changing inline styles, it is much better to add and remove class names to change functionality. Use jQuery's .addClass and .on function to do this.
+// $("#button").click(function() {
+//   $("#page").addClass("changeBackgroundPink");
+// });
+
+//3_click_to_pink_click_to_unpink
+//Now when you click on it again, make it so that the background is no longer pink
+$("#button").click(function(){
+  var $page = $("#page");
+  if ($page[0].className === "changeBackgroundPink") {
+    $("#page").removeClass("changeBackgroundPink");
+  } else {
+    $("#page").addClass("changeBackgroundPink");
+  }
+});
