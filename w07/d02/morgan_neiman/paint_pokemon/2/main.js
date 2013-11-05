78f018ee668cbca4ec81b3102fa2132efa7ef493
Morgan Neiman <quackhouse@me.com>
2013-11-05 14:43:58 -0500

    i dont' know anymore

--- w07/d02/morgan_neiman/paint_pokemon/2/main.js
+++ w07/d02/morgan_neiman/paint_pokemon/2/main.js
@@ -6,11 +6,9 @@ $( document ).ready(function() {
     height: 900
   });
 
-document.getElementById('container').addEventListener('click', function(e) {
-
-var layer = new Kinetic.Layer();
-var rectX = e.x;
-var rectY = e.y;
+  var layer = new Kinetic.Layer();
+  var rectX = 20;
+  var rectY = 20;
 // debugger;
 var box = new Kinetic.Rect({
   x: rectX,
@@ -31,14 +29,38 @@ box.on('mouseover touchstart', function() {
 box.on('mouseout touchend', function() {
   this.setFill('#00D2FF');
   layer.draw();
-})
+});
 
-console.log(rectX);
-console.log(e.x);
-console.log(rectY);
-console.log(e.y);
+function newBox(){
+  var rect = new Kinetic.Rect({
+    x: rectX,
+    y: rectY,
+    width: 100,
+    height: 100,
+    fill: '#00D2FF',
+    stroke: 'none',
+    strokeWidth: 4,
+    draggable: true
+  });
+
+  rect.on('mouseover touchstart', function() {
+    this.setFill('blue');
+    layer.draw();
+  });
+
+  rect.on('mouseout touchend', function() {
+    this.setFill('#00D2FF');
+    layer.draw();
+  });
+
+  layer.add(rect);
+  rect.on('dragstart', newBox);
+}
+
+box.on('dragstart', newBox);
+
 layer.add(box);
 stage.add(layer);
-});
+
 
 });
