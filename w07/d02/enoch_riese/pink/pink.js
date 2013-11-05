8370b79bb72b9d9d4b1cc85cde75bb35fb499886
Enoch Riese <enoch.riese@gmail.com>
2013-11-05 14:11:58 -0500

    project 1 step 4 complete

--- w07/d02/enoch_riese/pink/pink.js
+++ w07/d02/enoch_riese/pink/pink.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
-
-  onClick = function(e) {
-    if ($("body").hasClass("pink")) {
-      $("body").removeClass();
-    }
-    else {
-      $("body").addClass("pink");
-    }
+  COLORS = ["red", "blue", "green", "yellow", "pink", "purple"];
+  var onClick = function(e) {
+     var color = $("body").attr("class")
+     var nextColor = COLORS.indexOf(color) + 1
+     $("body").removeClass();
+     $("body").addClass(COLORS[nextColor % COLORS.length]);
+    // if ($("body").hasClass("pink")) {
+    //   $("body").removeClass();
+    // }
+    // else {
+    //   $("body").addClass("pink");
+    // }
   };
 
   $("#button").on("click", onClick);
