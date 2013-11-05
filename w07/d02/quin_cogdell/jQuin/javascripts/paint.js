bbe8fd0980b3f880e30358f46d8816ddf696ebfc
Quin Cogdell <quincogdell@gmail.com>
2013-11-05 17:18:42 -0500

    02_hover_to_green_refactor

--- w07/d02/quin_cogdell/jQuin/javascripts/paint.js
+++ w07/d02/quin_cogdell/jQuin/javascripts/paint.js
@@ -1,18 +1,19 @@
 $("document").ready(function() {
-  generateTiles(21, 64, 64);
+  generateTiles(32, 50, 50);
   })
-
-
 
 function generateTiles(numTiles, width, height) {
   for (i=0; i<numTiles; i++) {
    var tile =  $("<div/>")
       .addClass("tile")
       .css("width", width)
-      .css("height", height);
+      .css("height", height)
+      .mouseover(function() {
+        $(this).addClass("mousePaint")
+      })
+      .mouseout(function(){
+        $(this).removeClass("mousePaint")
+      });
   $("#tiles").append(tile);
   }
-
-
-
 }
