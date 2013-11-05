2cf1002991a6c79aaf7365db4eb30d7c6dd3cf2d
Edward Shin <Eshin0910@gmail.com>
2013-11-05 16:37:44 -0500

    paint file added with working tiles and green hover/out

---
+++ w07/d02/edward_shin/paint/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  console.log('ready bitch')
+
+  for (var i = 1; i < 199; i++) {
+    $("#tile-box")
+    .append($("<div>")
+      .attr("class", "tile"))
+  }
+
+
+  $("button").click(function() {
+    $("#tile-box")
+    .append($("<div>")
+      .attr("class", "tile"))
+  })
+
+  function hoverGreen(){
+    $(this).addClass("green");
+  }
+  function noGreen(){
+    $(this).removeClass("green");
+  }
+
+  $(".tile").on("mouseover", hoverGreen)
+  $(".tile").on("mouseout", noGreen)
+
+});
