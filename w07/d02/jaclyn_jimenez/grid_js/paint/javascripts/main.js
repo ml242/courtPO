9e922a73b7b8c987e1077d40380ba9e64af809c4
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-11-05 17:10:28 -0500

    holding down mouse continues painting until mouseup

--- w07/d02/jaclyn_jimenez/grid_js/paint/javascripts/main.js
+++ w07/d02/jaclyn_jimenez/grid_js/paint/javascripts/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  var clicked = 0
 
   function repeat(fn, times) {
     for (var i =0; i < times; i++)  {
@@ -6,21 +7,44 @@ $(document).ready(function() {
     }
   }
 
-  function generateTiles() {
-  //<div class = 'tile'></div>
+  function Tile() {
   var newTile = $("<div>")
     newTile.addClass('tile')
     $('body').append(newTile)
     newTile.on("mouseover", hoverRed )
     newTile.on("mouseout", removeColor )
+    newTile.on("mousedown", stayRed )
+    newTile.on("mouseup", stopRed )
+  }
+
+  function generateTiles() {
+  //<div class = 'tile'></div>
+  new Tile()
   }
 
   function hoverRed() {
-    $(this).addClass('red')
-  }
+    $(this).addClass('redHover')
+    if (clicked == 1) {
+      $(this).addClass('red')
+    }
+    }
+
   function removeColor() {
-    $(this).removeClass('red')
+    $(this).removeClass('redHover')
   }
+
+  function stayRed() {
+    $(this).addClass('red')
+    clicked = 1
+  }
+  function stopRed() {
+    clicked = 0
+  }
+
+  // function stayDownRed() {
+  //   $(this).addClass('red')
+  //   $('div').mouseover(function(){$(this).addClass('red')})
+  // }
 
   repeat(generateTiles, 272)
 
