f5eef96d45f938adaf7d5cb9ce15d01273e92085
Matthew Korporaal <matthew.korporaal@gmail.com>
2013-11-05 14:22:00 -0500

    added pt 1

--- w07/d02/matthew_korporaal/mygrid_js/javascripts/main.js
+++ w07/d02/matthew_korporaal/mygrid_js/javascripts/main.js
@@ -1,3 +1,19 @@
+// part 1
+// (function main() {
+//   var clicked = false;
+
+//   $('#magic-button').click(function(){
+//     if (clicked) {
+//      $('body').removeClass('pink');
+//      clicked = false;
+//     } else {
+//      $('body').addClass('pink');
+//      clicked = true;
+//     }
+//   });
+// })();
+
+// part 2
 (function main() {
   var clicked = false;
   COLORS = ["pink", "blue", "aqua", "yellow"];
@@ -5,7 +21,6 @@
 
   $('#magic-button').click(function(){
      $('body').removeClass(COLORS[currColor]);
-
      if (currColor == COLORS.length-1){
       currColor = 0;
      } else {
