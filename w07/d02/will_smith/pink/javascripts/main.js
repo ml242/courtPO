fccef64a648e39c53f1985ee17ebecbb1e847f04
Will Smith <will.smith@preplaysports.com>
2013-11-05 14:08:19 -0500

    3_click_to_pink_click_to_unpink

--- w07/d02/will_smith/pink/javascripts/main.js
+++ w07/d02/will_smith/pink/javascripts/main.js
@@ -1,6 +1,11 @@
 console.log("hi");
-$( document ).ready(function() {
-  $( "h1" ).on("click", function() {
-    $( "body" ).addClass("pink");
+$(document).ready(function() {
+  $("h1").on("click", function() {
+    var isPink = $("body").hasClass("pink");
+    if (isPink === true) {
+      $("body").removeClass("pink");
+    } else {
+      $("body").addClass("pink");
+    }
   });
 });
