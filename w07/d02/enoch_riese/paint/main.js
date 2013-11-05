2e3733f95c160c924ee604afbe3a44a81f7862ba
Enoch Riese <enoch.riese@gmail.com>
2013-11-05 14:43:27 -0500

    project 2 step 1 complete

--- w07/d02/enoch_riese/paint/main.js
+++ w07/d02/enoch_riese/paint/main.js
@@ -1,10 +1,19 @@
 $(document).ready( function() {
   console.log("hi");
 })
+var hoverGreen = function(e) {
+  $(this).addClass("green");
+}
+var unGreen = function(e) {
+  $(this).removeClass("green")
+}
 var makeTiles = function(tileNum) {
-    for (var i = 0 ; i < tileNum; i++) {
-      var divvy = $("<div>");
-      divvy.addClass("tile");
-      $("body").append(divvy);
-    };
-  }
+  for (var i = 0 ; i < tileNum; i++) {
+    var divvy = $("<div>");
+    divvy.addClass("tile");
+    divvy.on("mouseover", hoverGreen);
+    divvy.on("mouseout", unGreen);
+    $("body").append(divvy);
+  };
+}
+makeTiles(50);
