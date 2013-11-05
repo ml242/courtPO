1340526dd7e35d24a77cdf6ee73c19d2ff3debf9
Morgan Neiman <quackhouse@me.com>
2013-11-05 15:34:29 -0500

    tiles

--- w07/d02/morgan_neiman/paint_pokemon/2/main.js
+++ w07/d02/morgan_neiman/paint_pokemon/2/main.js
@@ -6,100 +6,126 @@ $( document ).ready(function() {
     height: 900
   });
 
+
   var layer = new Kinetic.Layer();
-  var rectX = 20;
-  var rectY = 20;
-// debugger;
-var box = new Kinetic.Rect({
-  x: rectX,
-  y: rectY,
-  width: 100,
-  height: 100,
-  fill: '#00D2FF',
-  stroke: 'none',
-  strokeWidth: 4,
-  draggable: true
-});
 
-box.on('mouseover touchstart', function() {
-  this.setFill('blue');
-  layer.draw();
-});
+  var tileY = 400;
+  var tileX = 0;
+  var maxX = 1200;
+  var maxY = 900;
 
-box.on('mouseout touchend', function() {
-  this.setFill('#00D2FF');
-  layer.draw();
-});
+  function generateTiles() {
+    for(var yCoord = tileY; yCoord < maxY; yCoord += 100){
+      for(var xCoord = tileX; xCoord < maxX; xCoord += 100){
+        var tile = new Kinetic.Rect({
+          x: xCoord,
+          y: yCoord,
+          width: 98,
+          height: 98,
+          fill: '#DCE8E7',
+          stroke: '#FFFFFF',
+          strokeWidth: 1
+        });
+        layer.add(tile);
+      }
+    }
+  }
 
-function newBox(){
-  var rect = new Kinetic.Rect({
-    x: rectX,
-    y: rectY,
-    width: 100,
-    height: 100,
-    fill: '#00D2FF',
-    stroke: 'none',
-    strokeWidth: 4,
-    draggable: true
-  });
+generateTiles();
 
-  rect.on('mouseover touchstart', function() {
-    this.setFill('blue');
-    layer.draw();
-  });
+//   var rectX = 20;
+//   var rectY = 20;
+// // debugger;
+// var box = new Kinetic.Rect({
+//   x: rectX,
+//   y: rectY,
+//   width: 100,
+//   height: 100,
+//   fill: '#00D2FF',
+//   stroke: 'none',
+//   strokeWidth: 4,
+//   draggable: true
+// });
 
-  rect.on('mouseout touchend', function() {
-    this.setFill('#00D2FF');
-    layer.draw();
-  });
+// box.on('mouseover touchstart', function() {
+//   this.setFill('blue');
+//   layer.draw();
+// });
 
-  layer.add(rect);
-  rect.on('dragstart', newBox);
-}
+// box.on('mouseout touchend', function() {
+//   this.setFill('#00D2FF');
+//   layer.draw();
+// });
 
-box.on('dragstart', newBox);
+// function newBox(){
+//   var rect = new Kinetic.Rect({
+//     x: rectX,
+//     y: rectY,
+//     width: 100,
+//     height: 100,
+//     fill: '#00D2FF',
+//     stroke: 'none',
+//     strokeWidth: 4,
+//     draggable: true
+//   });
 
-var circle = new Kinetic.Circle({
-  x: 300,
-  y: 100,
-  radius: 10,
-  fill: 'pink',
-  stroke: 'none',
-  strokeWidth: 4,
-  draggable: true
-});
+//   rect.on('mouseover touchstart', function() {
+//     this.setFill('blue');
+//     layer.draw();
+//   });
 
-function newCircle(e) {
-  var mousePos = stage.getPointerPosition();
-  var circ = new Kinetic.Circle({
-    x: mousePos.x,
-    y: mousePos.y,
-    radius: 10,
-    fill: 'pink',
-    stroke: 'none',
-    strokeWidth: 4,
-    draggable: true
-  });
-  circ.on('mousedown', drawing);
-  circ.on('mouseup', endDrawing);
-  layer.add(circ);
-}
+//   rect.on('mouseout touchend', function() {
+//     this.setFill('#00D2FF');
+//     layer.draw();
+//   });
 
-var timer;
+//   layer.add(rect);
+//   rect.on('dragstart', newBox);
+// }
 
-function drawing() {
-  timer = setInterval(newCircle, 100);
-}
+// box.on('dragstart', newBox);
 
-function endDrawing() {
-  clearInterval(timer);
-}
+// var circle = new Kinetic.Circle({
+//   x: 300,
+//   y: 100,
+//   radius: 10,
+//   fill: 'pink',
+//   stroke: 'none',
+//   strokeWidth: 4,
+//   draggable: true
+// });
 
-circle.on('mousedown', drawing);
-circle.on('mouseup', endDrawing);
+// function newCircle(e) {
+//   var mousePos = stage.getPointerPosition();
+//   var circ = new Kinetic.Circle({
+//     x: mousePos.x,
+//     y: mousePos.y,
+//     radius: 10,
+//     fill: 'pink',
+//     stroke: 'none',
+//     strokeWidth: 4,
+//     draggable: true
+//   });
+//   circ.on('mousedown', drawing);
+//   circ.on('mouseup', endDrawing);
+//   layer.add(circ);
+// }
 
-layer.add(circle);
-layer.add(box);
+// var timer;
+
+// function drawing() {
+//   timer = setInterval(newCircle, 10);
+// }
+
+// function endDrawing() {
+//   clearInterval(timer);
+// }
+
+// circle.on('mousedown', drawing);
+// circle.on('mouseup', endDrawing);
+
+// layer.add(circle);
+// layer.add(box);
 stage.add(layer);
 
 
