c0c9c5acbfaea9adf600a0897bd92006952de491
JC <jorge.e.colindres@gmail.com>
2013-11-05 16:57:25 -0500

    painting

--- w07/d02/jorge_colindres/grid/main.js
+++ w07/d02/jorge_colindres/grid/main.js
@@ -1,24 +1,57 @@
-function buildGrid(numTiles) {
-  for (var i = 0; i < numTiles; i++) {
-    var newTile = $('<div>');
-    newTile.addClass('tile');
-    $('body').append(newTile);
-  }
+function Paint(){
+  this.selectedColor = 'green';
 }
 
-$(function(){
-  buildGrid(50);
-  $('.tile').mouseover(function(){
+
+Paint.prototype.buildGrid = function(numTiles) {
+  var numRows = numTiles / 10;
+  for (var i = 0; i < numRows; i++) {
+    var newRow = $('<div>');
+    newRow.addClass('row');
+    $('body').append(newRow);
+
+    for (var j = 0; j < 10; j++) {
+      var newTile = $('<div>');
+      newTile.addClass('tile');
+      newRow.append(newTile);
+    }
+  }
+};
+Paint.prototype.addColorPicker = function(){
+  var self = this;
+  $('.palette').click(function(){
+    self.selectedColor = $(this).data('color');
+  });
+};
+
+Paint.prototype.addEventLists = function(){
+  var self = this;
+  $('.tile').mouseover(function(e){
     $(this).addClass('highlight');
   }).mouseout(function(){
     $(this).removeClass('highlight');
   });
-  $('.tile').mousemove(function(e){
+
+  $('.tile').on('mousemove', function(e){
     if (e.which === 1){
-      $(this).addClass('selected');
+      $(this).removeClass();
+      $(this).addClass('tile');
+      $(this).addClass(self.selectedColor);
     }
   });
-  $('.tile').click(function(){
-    $(this).addClass('selected');
+
+  $('.tile').on('click', function(){
+    $(this).removeClass();
+    $(this).addClass('tile');
+    $(this).addClass(self.selectedColor);
   });
+};
+
+
+$(function(){
+  var paintMS = new Paint();
+  paintMS.buildGrid(50);
+  paintMS.addColorPicker();
+  paintMS.addEventLists();
 });
+
