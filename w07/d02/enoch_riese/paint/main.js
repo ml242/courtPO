84aa9fb0b1176c518088252199180f2350886902
Enoch Riese <enoch.riese@gmail.com>
2013-11-06 00:10:47 -0500

    project 2 step 7 and 8 complete

--- w07/d02/enoch_riese/paint/main.js
+++ w07/d02/enoch_riese/paint/main.js
@@ -1,32 +1,53 @@
 $(document).ready( function() {
   console.log("hi");
 })
+COLORS = ["red", "blue", "green", "yellow", "pink", "purple"];
 var hoverGreen = function(e) {
-  $(this).addClass("green");
+  $(this).removeClass();
+  $(this).addClass("tile painted " + $(".selected").attr("id"));
 }
 var unGreen = function(e) {
-  $(this).removeClass("green")
+  $(this).removeClass($(".selected").attr("id"))
 }
 var stayGreen = function(e) {
   hoverGreen();
   $("div").off("mouseout");
 }
 var unClick = function(e) {
-  addEventListeners($(".tile").not(".green"));
+  tileAddEventListeners($(".tile").not(".painted"));
 }
-var addEventListeners = function(node) {
+var tileAddEventListeners = function(node) {
   node.on("mouseover", hoverGreen);
   node.on("mouseout", unGreen);
   node.on("mousedown", stayGreen);
   node.on("mouseup", unClick);
 }
 var makeTiles = function(tileNum) {
-  var tileStr = ""
+  var tileStr = "";
   for (var i = 0 ; i < tileNum; i++) {
     var divvy = "<div class='tile'></div>";
     tileStr += divvy;
   };
   $("#canvas").append(tileStr);
-  addEventListeners($(".tile"));
+  tileAddEventListeners($(".tile"));
 }
-makeTiles(10000);
+makeTiles(2500);
+paletteClick = function(e) {
+  $(".selected").removeClass("selected")
+  $(this).addClass("selected")
+}
+paletteAddEventListeners = function(node) {
+  node.on("click", paletteClick);
+}
+var makePalette = function(colors) {
+  paletteStr = "";
+  for (var i = 0; i < colors.length; i++) {
+    var divvy = "<div class='palette " + colors[i] +"' id='" + colors[i] + "'></div>";
+    paletteStr += divvy;
+  };
+  palette = $("<div/>").append(paletteStr)
+  palette.addClass("paletteContainer")
+  $("body").prepend(palette);
+  paletteAddEventListeners($(".palette"));
+}
+makePalette(COLORS);
