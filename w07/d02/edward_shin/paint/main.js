836f3ccad1170c1070a15efa5b8eb082cd0620dd
Edward Shin <Eshin0910@gmail.com>
2013-11-06 09:07:02 -0500

    color palette working

--- w07/d02/edward_shin/paint/main.js
+++ w07/d02/edward_shin/paint/main.js
@@ -1,48 +1,59 @@
 $(document).ready(function() {
   console.log('ready bitch')
-
-  for (var i = 1; i < 199; i++) {
-    $("#tile-box")
-    .append($("<div>")
-      .attr("class", "tile"))
-  }
-
-
+  //adding button
   $("button").click(function() {
-    $("#tile-box")
+    $("#wrapper")
     .append($("<div>")
       .attr("class", "tile"))
   })
 
+  //adding color palette
+  var colorsArray = ["pink", "blue", "green", "yellow", "purple"];
+  for (var i = 0; i < colorsArray.length; i++) {
+    $(".palette").append($("<div>")
+      .addClass("colorPalette "+ colorsArray[i] )
+    )}
+  //adding tiles
+  for (var i = 1; i < 1326; i++) {
+    $("#wrapper")
+    .append($("<div>")
+      .attr("class", "tile"))
+  }
+
+  //set variable color (default green)
+  var currentColor = "green"
+  //event listener functions
   function hoverGreen(){
-    $(this).addClass("green");
+    $(this).addClass(currentColor);
   }
   function noGreen(){
-    $(this).removeClass("green");
+    $(this).removeClass(currentColor);
   }
   function clickGreen() {
-    $(this).addClass("perm_green");
+    $(this).addClass("perm_"+currentColor);
   }
   function downGreen() {
-    $(this).addClass("perm_green");
+    $(this).addClass("perm_"+currentColor);
     $(".tile").on("mousemove", moveGreen);
   }
   function moveGreen(){
-    $(this).addClass("perm_green");
+    $(this).addClass("perm_"+currentColor);
   }
   function upGreen(){
-    $(this).addClass("perm_green");
+    $(this).addClass("perm_"+currentColor);
     $(".tile").off("mousemove", moveGreen);
   }
+  function chooseColor() {
+    var classList = $(this).attr("class").split(" ");
+    currentColor = classList[1];
+  }
 
-
+  //event listeners
   $(".tile").on("mouseover", hoverGreen);
   $(".tile").on("mouseout", noGreen);
   $(".tile").on("click", clickGreen);
   $(".tile").on("mousedown", downGreen);
   $(".tile").on("mouseup", upGreen);
-
-
-
+  $(".colorPalette").on("click", chooseColor);
 
 });
