07280dad4364c9d155365499b1ea1b964a6e4194
Will Smith <will.smith@preplaysports.com>
2013-11-05 20:32:55 -0500

    02_hover_to_green

--- w07/d02/will_smith/grid/javascripts/main.js
+++ w07/d02/will_smith/grid/javascripts/main.js
@@ -2,4 +2,11 @@ var Paint = Paint || {};
 
 $(function(){
   Paint.createTiles(1000, 64, 64);
+  $('div').hover(
+    function(){
+      $(this).addClass("green");
+    }, function() {
+      $(this).removeClass("green");
+    });
 });
+
