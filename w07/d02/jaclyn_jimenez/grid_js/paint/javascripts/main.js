95a60231890ee9695bd6bf05cd3fd60b30f1c99b
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-11-05 22:03:41 -0500

    finished paint project with different brushes

--- w07/d02/jaclyn_jimenez/grid_js/paint/javascripts/main.js
+++ w07/d02/jaclyn_jimenez/grid_js/paint/javascripts/main.js
@@ -1,53 +1,59 @@
 $(document).ready(function() {
   var clicked = 0
+  var color = 'red'
 
-  function repeat(fn, times) {
+  function repeat(times, fn) {
     for (var i =0; i < times; i++)  {
       fn()
     }
   }
-
   function Tile() {
-  var newTile = $("<div>")
-    newTile.addClass('tile')
-    $('body').append(newTile)
-    newTile.on("mouseover", hoverRed )
-    newTile.on("mouseout", removeColor )
-    newTile.on("mousedown", stayRed )
-    newTile.on("mouseup", stopRed )
+    this.tileNode = $("<div>")
+      .addClass('tile')
+      .on("mouseover", hoverColor )
+      .on("mouseout", removeColor )
+      .on("mousedown", stayColor )
+      .on("mouseup", stopColor )
+      .css('cursor', 'crosshair');
   }
-
-  function generateTiles() {
-  //<div class = 'tile'></div>
-  new Tile()
+  function Row() {
+    this.rowNode = $("<div>")
+      .addClass('row')
   }
-
-  function hoverRed() {
-    $(this).addClass('redHover')
+  function generateRows() {
+    var newRow = new Row()
+    repeat(30, function(){
+      var newTile = new Tile()
+      newRow.rowNode.append(newTile.tileNode)
+    })
+  $('body').append(newRow.rowNode)
+  }
+  function hoverColor() {
+    $(this).addClass(color+'Hover')
     if (clicked == 1) {
-      $(this).addClass('red')
+      $(this).addClass(color)
     }
-    }
-
-  function removeColor() {
-    $(this).removeClass('redHover')
   }
-
-  function stayRed() {
-    $(this).addClass('red')
+  function removeColor() {
+    $(this).removeClass(color+'Hover')
+  }
+  function stayColor() {
+    $(this).addClass(color)
     clicked = 1
   }
-  function stopRed() {
+  function stopColor() {
     clicked = 0
   }
+  function brush(){
+    console.log("got it! "+this.id)
+    color = this.id
+  }
+  var redBrush = $('#red').on("click", brush )
+  var yellowBrush = $('#yellow').on("click", brush )
+  var greenBrush = $('#green').on("click", brush )
+  var blackBrush = $('#black').on("click", brush )
 
-  // function stayDownRed() {
-  //   $(this).addClass('red')
-  //   $('div').mouseover(function(){$(this).addClass('red')})
-  // }
-
-  repeat(generateTiles, 272)
-
+  repeat(26, generateRows)
 })
 
 
