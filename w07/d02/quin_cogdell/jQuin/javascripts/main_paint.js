32bfc7e8280e08d5a71d7ead174cb846cb728721
Quin Cogdell <quincogdell@gmail.com>
2013-11-06 07:30:15 -0500

    04_click_and_hold_to_paint_tiles

--- w07/d02/quin_cogdell/jQuin/javascripts/main_paint.js
+++ w07/d02/quin_cogdell/jQuin/javascripts/main_paint.js
@@ -1,8 +1,7 @@
 var Paint = Paint || {};
 
 $("document").ready( function() {
-  Paint.generateTiles(32, 50, 50);
-  Paint.addEventListeners();
+  Paint.generateTiles(100, 50, 50);
 })
 
 
--- w07/d02/quin_cogdell/jQuin/javascripts/paint.js
+++ w07/d02/quin_cogdell/jQuin/javascripts/paint.js
@@ -2,22 +2,50 @@ var Paint = Paint || {};
 
 Paint.generateTiles = function(numTiles, width, height) {
   for (i=0; i<numTiles; i++) {
-   var tile =  $("<div/>")
+   var $tile =  $("<div/>")
       .addClass("tile")
       .css("width", width)
-      .css("height", height)
-  $("#tiles").append(tile);
+      .css("height", height);
+
+// TODO last tile does not have event listeners
+    Paint.addEventListeners($tile);
+    $("#tiles").append($tile);
   }
 }
 
-Paint.addEventListeners = function() {
-  $(".tile").on("mouseover", function(e){
-    var tile = $(e.target);
-    tile.addClass("mousePaint");
-  })
+Paint.addEventListeners = function($tile) {
+
+  $tile.on("mousemove", function(e){
+    //debugger
+    console.log(e.timestamp)
+    $tile.addClass("mouseOverPaint");
+
+    var LEFT_MOUSE_BUTTON_DOWN = 1;
+
+    if (e.which == LEFT_MOUSE_BUTTON_DOWN) {
+      //debugger
+      $tile.addClass("mouseClickPaint")
+    }
+  });
+
+  // $(".tile").on("mouseover", function(e){
+  //   var tile = $(e.target);
+  //   tile.addClass("mouseOverPaint");
+  // })
   $(".tile").on("mouseout", function(e) {
     var tile = $(e.target);
-    tile.removeClass("mousePaint");
+    tile.removeClass("mouseOverPaint");
   })
 
+  $tile.on("click", function(e) {
+    var tile = $(e.target);
+    tile.addClass("mouseClickPaint")
+  })
+  $tile.on("mousedown", function(e) {
+    console.log(e);
+    // var tile = $(e.target);
+    // tile.addClass("mouseClickPaint")
+  })
 }
+
+
