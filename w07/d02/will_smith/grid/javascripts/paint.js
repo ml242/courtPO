c00f52affc4083a7174a77bdd1e5c5d161166c80
Will Smith <will.smith@preplaysports.com>
2013-11-05 20:37:11 -0500

    refactored

--- w07/d02/will_smith/grid/javascripts/paint.js
+++ w07/d02/will_smith/grid/javascripts/paint.js
@@ -1,9 +1,18 @@
 var Paint = Paint || {};
 Paint.createTiles = function(numTiles, width, height) {
-    for (var i = 1; i <= numTiles; i++) {
-      var div = $("<div>", {class: "tile"})
-                  .css("width", width)
-                  .css("height", height);
-             $('body').append(div);
-         }
+  for (var i = 1; i <= numTiles; i++) {
+    var div = $("<div>", {class: "tile"})
+    .css("width", width)
+    .css("height", height);
+    $('body').append(div);
+  }
+};
+
+Paint.addEventListeners = function(){
+  $('div').hover(
+    function(){
+      $(this).addClass("green");
+    }, function() {
+      $(this).removeClass("green");
+    });
 };
