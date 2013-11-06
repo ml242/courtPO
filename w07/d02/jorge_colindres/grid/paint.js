434d303661b4143e2fe94d435b04e681784ec635
JC <jorge.e.colindres@gmail.com>
2013-11-05 19:25:48 -0500

    drawing poke maps

--- w07/d02/jorge_colindres/grid/paint.js
+++ w07/d02/jorge_colindres/grid/paint.js
@@ -22,7 +22,7 @@ Paint.prototype.buildGrid = function(numTiles) {
 
 Paint.prototype.addColorPicker = function(){
   var self = this;
-  var colors = ['green', 'red', 'blue'];
+  var colors = ['grass','red_flowers','weed','weed_4x','weed_small','week_2x','box','fence_1','fence_2','fence_3','fence_4','fence_5','fence_6','fence_7','fence_8','fence_9','mountain_1','mountain_2','mountain_3','mountain_4','mountain_5','mountain_6','mountain_7','mountain_8','mountain_9','mountain_entrance','rock_1','rock_2','sign','tree_1','tree_2','urn','water_1','water_2','water_3','water_4','water_5','water_6','water_7','water_8','water_9'];
   var colorPicker = $('.color-picker');
   for (var i = 0; i < colors.length; i++) {
     var newColor = $('<div>');
