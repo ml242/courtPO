ddbfb2c1e7f91c8db158a5fefd24cc6f04e3aa77
Matthew Korporaal <matthew.korporaal@gmail.com>
2013-11-05 22:25:25 -0500

    v4 add click and color

---
+++ w07/d02/matthew_korporaal/paint_app/paint_v4/javascripts/main.js
@@ -0,0 +1,39 @@
+var Paint = Paint || {};
+
+$(document).ready(function() {
+  Paint.createSurface(30, 30, 25, 25);
+});
+
+
+// part 1
+// (function main() {
+//   var clicked = false;
+
+//   $('#magic-button').click(function(){
+//     if (clicked) {
+//      $('body').removeClass('pink');
+//      clicked = false;
+//     } else {
+//      $('body').addClass('pink');
+//      clicked = true;
+//     }
+//   });
+// })();
+
+// part 2
+// (function main() {
+//   var clicked = false;
+//   COLORS = ["pink", "blue", "aqua", "yellow"];
+//   var currColor = 0;
+
+//   $('#magic-button').click(function(){
+//      $('body').removeClass(COLORS[currColor]);
+//      if (currColor == COLORS.length-1){
+//       currColor = 0;
+//      } else {
+//       currColor++;
+//      }
+//      $('body').addClass(COLORS[currColor]);
+
+//   });
+// })();
