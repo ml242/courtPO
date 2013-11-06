67cc68a3995d02ebf408338c2c405ff9e4710fef
Katherine Santiago <santiago.kat@gmail.com>
2013-11-05 21:40:11 -0500

    added the click, mouseover, mouseup function

--- w07/d02/katherine_santiago/pokemon_app/pokemon.js
+++ w07/d02/katherine_santiago/pokemon_app/pokemon.js
@@ -12,4 +12,38 @@ $( document ).ready(function() {
     createTile();
   }
 
+  // mouseover a tile and it adds class hover-turquoise
+  // the class adds background color to the tile
+  $(".tile").on("mouseover", function() {
+    $(this).addClass("hover-turquoise");
+  });
+
+  // mouse away from a tile (mouseout) and the hover class
+  // is removed
+  $(".tile").on("mouseout", function() {
+    $(this).removeClass("hover-turquoise");
+  });
+
+  // click on a tile and it adds a paint (permanent class)
+  $(".tile").on("click", function() {
+    $(this).addClass("paint-turquoise");
+
+    $(".tile").on("mouseover", function() {
+      $(this).addClass("paint-turquoise");
+
+    $(".tile").on("mouseup", function() {
+      $(".tile").unbind("mouseover");
+
+    });
+    });
+
+  });
+
+  // while i hold down the left mousebutton
+  $(".tile").mousedown(function() {
+    $(this).addClass("paint-turquoise");
+  // continue painting until mouseup
+  })
+
+
 });
