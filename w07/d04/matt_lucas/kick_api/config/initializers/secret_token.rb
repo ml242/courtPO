f9c5b3b7d254114d22e64051d7b8a476ab27e985
Matt Lucas <mattlucas@gmail.com>
2013-11-07 20:09:47 -0500

    getting installed

---
+++ w07/d04/matt_lucas/kick_api/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KickApi::Application.config.secret_token = '75c4db202ee9708206a82816a9d0f3d0dee31d7d6e82312c31b9cacabc72fadfa9bbe797e7b051c19127d002fd8b461acfac1e48fde2b59c03600a596675adbe'
