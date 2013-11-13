339f6d41cc74f7e8b78605024482b2051cdc765c
Matt Lucas <mattlucas@gmail.com>
2013-11-13 16:03:12 -0500

    twitter progress

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,9 @@
+# Twitter.configure do |config|
+#   config.consumer_key = YOUR_CONSUMER_KEY
+#   config.consumer_secret = YOUR_CONSUMER_SECRET
+# end
+
+# client = Twitter::Streaming::Client.new do |config|
+#   config.consumer_key        = "YOUR_CONSUMER_KEY"
+#   config.consumer_secret     = "YOUR_CONSUMER_SECRET"
+# end
