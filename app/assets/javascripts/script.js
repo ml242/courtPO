ecd2203a68eb6a7b3accc78c76370c06f9466601
Matt Lucas <mattlucas@gmail.com>
2013-11-14 12:40:13 -0500

    screwed up the dom

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,14 +1,56 @@
 
 $(function(){
+
   $('#case-button').on('click', function(e){
    var opinion = $('#opinion-input').val();
 
    $.ajax({
-  type: "POST",
-  url: url,
-  data: data,
-  success: success,
-  dataType: dataType
-});
+    type: "POST",
+    url: url,
+    data: data,
+    success: success,
+    dataType: dataType
+  });
+
+ });
+  backgroundResizeFun();
+  $(window).resize(function(){
+    backgroundResizeFun();
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var backgroundResizeFun=function(){
+  // console.log('resize');
+   $('#wrapperz').height($(window).height());
+  // $('#bkg-image').css({
+  //   width:$(window).width()+'px',
+  //   height:$(window).height()+'px'
+  // });
+} ;
+
+
+
+
+
+
+
+
