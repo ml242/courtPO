9ae088bd7aba732eeeac58089458b69dec079b14
Tasha25 <greennatasha@aol.com>
2013-11-14 23:04:52 -0500

    Opinion added with html

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -2,19 +2,20 @@ class SolutionsController < ApplicationController
   # before_filter :load_case
 
   def create
-
+    binding.pry
     @user = current_user
     @case = Case.find(params[:case_id])
-    @solution = Solution.new(params[:solution])
 
+    @solution = Solution.new( :user_id => current_user.id, :case_id => @case.id, :opinion => params[:opinion_input])
    #this goes to the show and helps to print out what you want to see form the database.
     if @solution.save
-      redirect_to cases_path
+      render json: @solution
     else
       @users = User.all
       render :new
     end
   end
+
   def show
     render json: params[:id]
   end
