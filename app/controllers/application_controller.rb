7c1f7204179651a6167407b6e39338dbb4736b77
Matt Lucas <mattlucas@gmail.com>
2013-11-14 19:53:52 -0500

    updated session status and links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
 private
-  helper_method :current_user, :logged_in_twitter
 
   def current_user
-    #if session[:user_id]
-    if params[:id]
-      if @current_user.nil?
-        @current_user = User.find(params[:id])
-      end
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
     end
-
-    return @current_user
   end
 
-  def logged_in_twitter
-    return session[:user_id]
-  end
+  helper_method :current_user
+
+  # def logged_in_twitter
+  #   return session[:user_id]
+  # end
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,6 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-
     respond_to do |format|
       format.html
       format.json { render json: @users }
